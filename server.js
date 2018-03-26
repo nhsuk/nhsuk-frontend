078d3b3761a8d603de570b748e2b5b272f199b02
Adam Chrimes <iChrimes@live.co.uk>
2018-03-26 09:48:47 +0100

    Heroku deployment

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var path = require('path');
 var logger = require('morgan');
 var compression = require('compression');
 var methodOverride = require('method-override');
-var session = require('express-session');
-var flash = require('express-flash');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var dotenv = require('dotenv');
@@ -31,8 +29,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(methodOverride('_method'));
-app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
-app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', HomeController.index);
