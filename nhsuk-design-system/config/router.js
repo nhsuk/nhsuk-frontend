14fe5b2178f433314601b5833bc62ce1e82a1d21
Alex Colbeck <alexcolbeck@gmail.com>
2018-05-30 09:51:15 +0100

    New breadcrumb partial and renaming pages

--- nhsuk-design-system/config/router.js
+++ nhsuk-design-system/config/router.js
@@ -33,124 +33,104 @@ router.get('/styles/header-footer', (req, res) => {
   res.render('styles/header-footer', {});
 });
 
-router.get('/examples', (req, res) => {
-  res.render('examples/index', {});
+router.get('/styles/breadcrumbs', (req, res) => {
+  res.render('styles/breadcrumbs', {});
 });
 
-router.get('/examples/chickenpox', (req, res) => {
-  res.render('examples/chickenpox', {});
+router.get('/examples', (req, res) => {
+  res.render('examples/index', {});
 });
 
 router.get('/examples/page', (req, res) => {
   res.render('examples/page', {});
 });
 
-router.get('/examples/page-1', (req, res) => {
-  res.render('examples/page-1', {});
+router.get('/examples/test-condition-blisters-v1', (req, res) => {
+  res.render('examples/test-condition-blisters-v1', {});
 });
 
-router.get('/examples/page-2', (req, res) => {
-  res.render('examples/page-2', {});
+router.get('/examples/test-condition-chickenpox', (req, res) => {
+  res.render('examples/test-condition-chickenpox', {});
 });
 
-router.get('/examples/page-3', (req, res) => {
-  res.render('examples/page-3', {});
+router.get('/examples/test-condition-chlamydia', (req, res) => {
+  res.render('examples/test-condition-chlamydia', {});
 });
 
-router.get('/examples/page-4', (req, res) => {
-  res.render('examples/page-4', {});
+router.get('/examples/test-condition-depression', (req, res) => {
+  res.render('examples/test-condition-depression', {});
 });
 
-router.get('/examples/page-5', (req, res) => {
-  res.render('examples/page-5', {});
-});
-
-router.get('/examples/test-medicine', (req, res) => {
-  res.render('examples/test-medicine', {});
-});
-
-router.get('/examples/test-medicine-v2', (req, res) => {
-  res.render('examples/test-medicine-v2', {});
-});
-
-router.get('/examples/test-medicine-v3', (req, res) => {
-  res.render('examples/test-medicine-v3', {});
-});
-
-router.get('/styles/breadcrumbs', (req, res) => {
-  res.render('styles/breadcrumbs', {});
-});
-
-router.get('/examples/test-gout', (req, res) => {
-  res.render('examples/test-gout', {});
-});
-
-router.get('/examples/test-sciatica', (req, res) => {
-  res.render('examples/test-sciatica', {});
-});
-
-router.get('/examples/medicines', (req, res) => {
-  res.render('examples/medicines/index', {});
-});
-
-router.get('/examples/medicines/propranolol', (req, res) => {
-  res.render('examples/medicines/propranolol/index', {});
-});
-
-router.get('/examples/medicines/propranolol/page-1', (req, res) => {
-  res.render('examples/medicines/propranolol/page-1', {});
-});
-
-router.get('/examples/medicines/propranolol/page-2', (req, res) => {
-  res.render('examples/medicines/propranolol/page-2', {});
-});
-
-router.get('/examples/medicines/propranolol/page-3', (req, res) => {
-  res.render('examples/medicines/propranolol/page-3', {});
-});
-
-router.get('/examples/header-cookie', (req, res) => {
-  res.render('examples/header-cookie', {});
-});
-
-router.get('/examples/test-depression', (req, res) => {
-  res.render('examples/test-depression', {});
-});
-
-router.get('/examples/medicines', (req, res) => {
-  res.render('examples/medicines/index', {});
-});
-
-router.get('/examples/medicines/propranolol', (req, res) => {
-  res.render('examples/medicines/propranolol/index', {});
-});
-
-router.get('/examples/medicines/propranolol/page-1', (req, res) => {
-  res.render('examples/medicines/propranolol/page-1', {});
-});
-
-router.get('/examples/medicines/propranolol/page-2', (req, res) => {
-  res.render('examples/medicines/propranolol/page-2', {});
-});
-
-router.get('/examples/medicines/propranolol/page-3', (req, res) => {
-  res.render('examples/medicines/propranolol/page-3', {});
-});
-
-router.get('/examples/test-condition-vertigo', (req, res) => {
-  res.render('examples/test-condition-vertigo', {});
-});
-
-router.get('/examples/test-condition-slipped-disc', (req, res) => {
-  res.render('examples/test-condition-slipped-disc', {});
+router.get('/examples/test-condition-gout', (req, res) => {
+  res.render('examples/test-condition-gout', {});
 });
 
 router.get('/examples/test-condition-heel-pain-v1', (req, res) => {
   res.render('examples/test-condition-heel-pain-v1', {});
 });
 
-router.get('/examples/test-condition-blisters-v1', (req, res) => {
-  res.render('examples/test-condition-blisters-v1', {});
+router.get('/examples/test-condition-overactive-thyroid', (req, res) => {
+  res.render('examples/test-condition-overactive-thyroid', {});
+});
+
+router.get('/examples/test-condition-sciatica', (req, res) => {
+  res.render('examples/test-condition-sciatica', {});
+});
+
+router.get('/examples/test-condition-slipped-disc', (req, res) => {
+  res.render('examples/test-condition-slipped-disc', {});
+});
+
+router.get('/examples/test-condition-vertigo', (req, res) => {
+  res.render('examples/test-condition-vertigo', {});
+});
+
+router.get('/examples/test-guide-pregnancy', (req, res) => {
+  res.render('examples/test-guide-pregnancy', {});
+});
+
+router.get('/examples/header-cookie', (req, res) => {
+  res.render('examples/header-cookie', {});
+});
+
+router.get('/examples/test-medicine-amoxicillin', (req, res) => {
+  res.render('examples/test-medicine-amoxicillin', {});
+});
+
+router.get('/examples/test-medicine-amoxicillin-v2', (req, res) => {
+  res.render('examples/test-medicine-amoxicillin-v2', {});
+});
+
+router.get('/examples/test-medicine-amoxicillin-v3', (req, res) => {
+  res.render('examples/test-medicine-amoxicillin-v3', {});
+});
+
+router.get('/examples/medicines/index', (req, res) => {
+  res.render('examples/medicines/index', {});
+});
+
+router.get('/examples/medicines/propranolol', (req, res) => {
+  res.render('examples/medicines/propranolol/index', {});
+});
+
+router.get('/examples/medicines/propranolol/page-1', (req, res) => {
+  res.render('examples/medicines/propranolol/page-1', {});
+});
+
+router.get('/examples/medicines/propranolol/page-2', (req, res) => {
+  res.render('examples/medicines/propranolol/page-2', {});
+});
+
+router.get('/examples/medicines/propranolol/page-3', (req, res) => {
+  res.render('examples/medicines/propranolol/page-3', {});
+});
+
+router.get('/examples/test-medicine-buscopan', (req, res) => {
+  res.render('examples/test-medicine-buscopan', {});
+});
+
+router.get('/examples/test-news-52-diet', (req, res) => {
+  res.render('examples/test-news-52-diet', {});
 });
 
 module.exports = router;
