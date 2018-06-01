9f65383da5db1a79bf41fc88f3d473471ba5da31
AdamChrimes <iChrimes@live.co.uk>
2018-06-01 09:24:31 +0100

    :eyes: remove smooth scrolling

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,7 @@
 /* eslint-disable */
 (function() {
-  // Your custom JavaScript goes here
-
   // Change background colour (for user research testing purposes)
-
-  var hexCode = getParameterByName('background'); 
+  var hexCode = getParameterByName('background');
   var bodyElement = document.body;
 
   if(hexCode) {
@@ -21,43 +18,3 @@
     return decodeURIComponent(results[2].replace(/\+/g, " "));
   }
 })();
-
-
-// Smooth page scrolling
-
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-	// On-page links
-	if (
-	  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-	  && 
-	  location.hostname == this.hostname
-	) {
-	  // Figure out element to scroll to
-	  var target = $(this.hash);
-	  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-	  // Does a scroll target exist?
-	  if (target.length) {
-		// Only prevent default if animation is actually gonna happen
-		event.preventDefault();
-		$('html, body').animate({
-		  scrollTop: target.offset().top
-		}, 500, function() {
-		  // Callback after animation
-		  // Must change focus!
-		  var $target = $(target);
-		  $target.focus();
-		  if ($target.is(":focus")) { // Checking if the target was focused
-			return false;
-		  } else {
-			$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-			$target.focus(); // Set focus again
-		  };
-		});
-	  }
-	}
-  });
