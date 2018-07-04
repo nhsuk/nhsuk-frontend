158eb71f50edd84ec2b945c9120e9f8c4dc865fa
Michael Cheung <michaelcheung@nhs.net>
2018-07-04 16:09:29 +0100

    :link: Add skip link to main content

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -17,4 +17,16 @@
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
   }
+
+
+  // skip link focus fix
+  $( document ).ready(function() {
+    $(".c-skiplink__link").click(function(event){
+      var skipTo="#"+this.href.split('#')[1];
+      $(skipTo).attr('tabindex', -1).on('blur focusout', function () {
+        $(this).removeAttr('tabindex');
+      }).focus();
+    });
+  });
+
 })();
