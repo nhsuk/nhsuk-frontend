985f18849c7cb88c9b6e4de712ab6c8d16c0ed9c
AdamChrimes <iChrimes@live.co.uk>
2018-10-23 10:43:15 +0100

    [refs #65] Remove time element and scss comments
    Remove the time element as there is no additional benefit and
    we would need to output the date in 2 different formats every time.
    Also remove unnecessary css comments.

--- packages/components/review-date/template.test.js
+++ packages/components/review-date/template.test.js
@@ -2,6 +2,5 @@ tests = [
   'Review date element must the class nhsuk-c-review-date',
   'Review date content must contain a last review date',
   'Review date content must contain a next review date',
-  'Review date, dates must be contained in the HTML5 <time></time> element',
   'Review date, dates must be formatted as 1 January 2016'
 ];
