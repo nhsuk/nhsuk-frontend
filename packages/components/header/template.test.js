6b16478264e8c037d5a3063d6d7c463d12a582b2
AdamChrimes <iChrimes@live.co.uk>
2018-10-30 11:02:06 +0000

    [refs #57] Psuedocode tests for the header
    Added some psuedocode tests for header accessibility, to be
    added to a automated testing framework at a later date.

---
+++ packages/components/header/template.test.js
@@ -0,0 +1,11 @@
+tests = [
+  'Header must have a role="banner" attribute',
+  'Header logo link must have a aria-label="NHS homepage" attribute',
+  'Header menu button must have aria-controls="c-nav-primary" aria-label="Open menu" attributes',
+  'Header search button must have aria-controls="search" aria-label="Open search" attributes',
+  'Header search form element must have method="get" role="search" attributes',
+  'Header search input field must have autocomplete="off" attributes',
+  'Header search button must have hidden span text value Search',
+  'Header close search button must have hidden span text value Close search',
+  'Header navigation must have role="navigation" aria-label="Primary navigation" aria-labelledby="navigation-label" attributes'
+];
