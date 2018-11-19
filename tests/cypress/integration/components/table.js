a068bdbefcef2281b62dd3dbde9088848e96a07e
Surya Pulakhandam <surya.pulakhandam1@nhs.net>
2018-11-19 12:54:40 +0000

    added tests to table component

---
+++ tests/cypress/integration/components/table.js
@@ -0,0 +1,22 @@
+describe('Table component tests', function () {
+  beforeEach(function () {
+    cy.viewport('macbook-15');
+  });
+
+  it('Navigating to Table Component', function () {
+    cy.visit(Cypress.config().baseUrl + '/tables.html')
+  });
+
+  it('Checking for the table caption class', function () {
+    cy.get('.nhsuk-table').children().should('have.class', 'nhsuk-table__caption')
+  });
+
+  it('Checking for the table heading class', function () {
+    cy.get('.nhsuk-table').children().should('have.class', 'nhsuk-table__head')
+  });
+
+  it('Checking for the table body class', function () {
+    cy.get('.nhsuk-table').children().should('have.class', 'nhsuk-table__body')
+  });
+  // TODO: aria label and the role
+});
