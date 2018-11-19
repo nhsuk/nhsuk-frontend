5d1b7e6088580ba356e2dcce062be94fe5c92e29
Surya Pulakhandam <surya.pulakhandam1@nhs.net>
2018-11-19 14:34:42 +0000

    updated tests for details component

--- tests/cypress/integration/components/details.js
+++ tests/cypress/integration/components/details.js
@@ -1,58 +1,30 @@
-describe('setting screen res', function () {
+// TODO: need to add aria expander status and aria label
 
+describe('Details component tests', function () {
   beforeEach(function () {
     cy.viewport('macbook-15');
-
   });
 
-  it('Navigating to details Component', function (done) {
-    // this event will automatically be unbound when this
-    // test ends because it's attached to 'cy'
-    cy.on('uncaught:exception', (err, runnable) => {
-      // using mocha's async done callback to finish
-      // this test so we prove that an uncaught exception
-      // was thrown
-      done();
-      // return false to prevent the error from
-      // failing this test
-      return false
-    });
-
-    // assume this causes an error
-    cy
-      .visit(Cypress.config().baseUrl + '/details')
-
+  it('Navigating to details Component', function () {
+    cy.visit(Cypress.config().baseUrl + '/details.html')
   });
 
-
-  // checking for class name - nhsuk-details__summary-text
   it('Checking class name', function () {
-    cy
-      .get('.nhsuk-details__summary').children().should('have.class','nhsuk-details__summary-text')
-
+    cy.get('.nhsuk-details__summary').children().should('have.class','nhsuk-details__summary-text')
   });
 
-  // checking for Aria controls
   it('Checking Aria controls', function () {
-    cy
-      .get('.nhsuk-details__summary').should('have.attr','aria-controls','details-content-0')
-
+    cy.get('.nhsuk-details__summary').should('have.attr','aria-controls','details-content-0')
   });
 
-
-  // checking for expander default status
   it('Checking expander default status', function () {
-    cy
-      .get('.nhsuk-details__summary').should('have.attr','aria-expanded', 'false')
-
+    cy.get('.nhsuk-details__summary').should('have.attr','aria-expanded', 'false')
   });
 
-  // checking for expander status after expanding
   it('Checking expander click status', function () {
     cy
       .get('.nhsuk-details__summary').click()
       .get('.nhsuk-details__summary').should('have.attr','aria-expanded', 'true')
-
   });
 
   });
