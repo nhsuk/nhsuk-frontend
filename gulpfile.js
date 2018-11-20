3eceae6e0ffd288e77cb7a95fee95e212f6b9d58
Mike Monteith <mike@mikemonteith.com>
2018-11-20 09:08:10 +0000

    Watch site/ directory for changes

--- gulpfile.js
+++ gulpfile.js
@@ -132,7 +132,7 @@ function createZip() {
  * Recompile CSS, JS and docs when there are any changes
  */
 var watch = function() {
-  gulp.watch(['packages/**/*', 'docs/**/*'], gulp.series(['build', 'docs:build']));
+  gulp.watch(['packages/**/*', 'docs/**/*', 'site/**/*'], gulp.series(['build', 'docs:build']));
 }
 
 
