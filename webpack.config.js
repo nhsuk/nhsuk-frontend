e6b931e7648e8a8b9af790f2fe81de47e0ae7607
AdamChrimes <iChrimes@live.co.uk>
2018-11-28 11:14:50 +0000

    [refs #96] Use gulp to build our Javascript with webpack
    Create a gulp task to compile our JavaScript using the webpack
    gulp plugin. Deleting the old build folder and using the main dist
    folder for our built JavaScript.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
-
-const path = require('path');
-
 module.exports = {
-  entry: './packages/nhsuk.js',
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'nhsuk.bundle.js'
-  }
+    filename: 'nhsuk.bundle.js',
+  },
+  target: 'web',
 };
