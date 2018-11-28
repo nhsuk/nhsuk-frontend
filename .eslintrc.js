e316fc2b67b1f5441209b122082025ce325a2683
AdamChrimes <iChrimes@live.co.uk>
2018-11-28 14:13:50 +0000

    [refs #98] Refactor feedback banner JS to an ES6 module
    Refactor the feedback banner JavaScript so that it can be imported
    as a module, using ES6 syntax.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-  "extends": "nhsuk"
+  "extends": "nhsuk",
+  "env": {
+    "browser": true,
+  }
 };
--- packages/components/feedback-banner/feedback-banner.js
+++ packages/components/feedback-banner/feedback-banner.js
@@ -1,27 +1,46 @@
-// Feedback banner
+/**
+ * Feedback banner
+ *
+ * Javascript to show and hide the feedback banner, showing
+ * the banner can be delayed by a number of seconds.
+ *
+ * The default delay before showing the feedback banner is
+ * 3000ms (3 seconds), this can be changed.
+ *
+ * Usage:
+ * feedbackBanner.init(3000);
+ */
 
 const banner = document.querySelector('#nhsuk-feedback-banner');
 const bannerCloseButton = document.querySelector('#nhsuk-feedback-banner-close');
 
 function showBanner() {
-  if (typeof(banner) != 'undefined' && banner != null) {
-    setTimeout(function () {
-      banner.style.display = "block";
-    }, 3000);
+  if (banner) {
+    banner.style.display = 'block';
   }
 }
 
 function hideBanner() {
-  if (bannerCloseButton) {
-    bannerCloseButton.addEventListener("click", function(){
-      if (typeof(banner) != 'undefined' && banner != null) {
-        banner.style.display = "none";
-      }
-    });
+  if (banner) {
+    banner.style.display = 'none';
   }
 }
 
-export function init() {
-  showBanner();
-  hideBanner();
+function handleBannerDisplay(delay) {
+  setTimeout(() => {
+    showBanner();
+  }, delay);
 }
+
+function handleBannerClose() {
+  if (bannerCloseButton) {
+    bannerCloseButton.addEventListener('click', hideBanner);
+  }
+}
+
+function feedbackBanner(delay) {
+  handleBannerDisplay(delay);
+  handleBannerClose();
+}
+
+export default feedbackBanner;
