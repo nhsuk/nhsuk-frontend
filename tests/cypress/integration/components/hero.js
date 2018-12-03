3b1eb6534f1f7946eddab02f6a49fb33252c185f
suryapulakhandam <surya.pulakhandam1@nhs.net>
2018-12-03 12:20:11 +0000

    [refs #229] Implementing a testing framework
    Updated the tests for hero.js with Hero with Image and content to cover the tests for checking the class name

--- tests/cypress/integration/components/hero.js
+++ tests/cypress/integration/components/hero.js
@@ -18,3 +18,31 @@ describe('Hero Image Component', function () {
 
 });
 
+
+describe('Hero with Image and content Component', function () {
+
+  beforeEach(function () {
+    cy.viewport('macbook-15');
+  });
+
+  it('Navigating to hero Component', function () {
+    cy.visit(Cypress.config().baseUrl + '/hero-image-content.html')
+  });
+
+  it('Checking for a class name - nhsuk-c-hero', function () {
+    cy.get('.nhsuk-hero__overlay').children().should('have.class', 'nhsuk-width-container nhsuk-hero--border')
+  });
+
+  it('Checking for a class - nhsuk-!-margin-bottom-3', function () {
+    cy.get('.nhsuk-hero-content').children().should('have.class', 'nhsuk-!-margin-bottom-3','We’re here for you')
+  });
+
+  it('Checking for a class - nhsuk-!-margin-bottom-3', function () {
+    cy.get('.nhsuk-hero-content').children().should('have.class', 'nhsuk-body-l nhsuk-!-margin-bottom-0','nhsuk-body-l nhsuk-!-margin-bottom-0')
+  });
+
+  it('Checking for a class - nhsuk-hero nhsuk-hero--image nhsuk-hero--image-description', function () {
+    cy.get('.nhsuk-hero.nhsuk-hero--image.nhsuk-hero--image-description')
+  });
+
+});
