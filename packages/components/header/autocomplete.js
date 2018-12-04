59e56db594a9bc6b4de8f8c23aeae4ed6684ff80
Sab <sabeeh.ul-hassan1@nhs.net>
2018-12-04 12:14:13 +0000

    [refs #96] Cleanup linting and formatting of the JS files

--- packages/components/header/autocomplete.js
+++ packages/components/header/autocomplete.js
@@ -1,95 +1,91 @@
 import accessibleAutocomplete from 'accessible-autocomplete'
 
 function getFunnelbackQueryUrl(query) {
-  const FUNNELBACK_QUERY_PATH = "https://nhs.funnelback.co.uk/s/suggest.json"
-  const FUNNELBACK_MAX_RESULTS = 10
-  return `${FUNNELBACK_QUERY_PATH}?collection=nhs-meta&partial_query=${query}&sort=0&fmt=json++&profile=&show=${FUNNELBACK_MAX_RESULTS}`
+    const FUNNELBACK_QUERY_PATH = "https://nhs.funnelback.co.uk/s/suggest.json"
+    const FUNNELBACK_MAX_RESULTS = 10
+    return `${FUNNELBACK_QUERY_PATH}?collection=nhs-meta&partial_query=${query}&sort=0&fmt=json++&profile=&show=${FUNNELBACK_MAX_RESULTS}`
 }
 
 function getFunnelbackResults(query, populateResults) {
-  const url = getFunnelbackQueryUrl(query)
-  const xhr = new XMLHttpRequest()
-  xhr.open('GET', url)
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText)
-      const results = data.map(item => {
-        return item.disp
-      })
-      populateResults(results)
-    } else {
-      console.warn(xhr)
-      //TODO: nice error messaging here
+    const url = getFunnelbackQueryUrl(query)
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText)
+            const results = data.map(item => {
+                return item.disp
+            })
+            populateResults(results)
+        } else {
+            console.warn(xhr)
+                //TODO: nice error messaging here
+        }
     }
-  }
-  xhr.send()
+    xhr.send()
 }
-
 
 function autocomplete(config) {
 
-  const defaultId = 'search-field'
-  const id = (config && config.id) ? config.id : defaultId
+    const defaultId = 'search-field'
+    const id = (config && config.id) ? config.id : defaultId
 
-  const fallbackInputElement = document.getElementById(id)
+    const fallbackInputElement = document.getElementById(id)
 
-  const defaultConfig = {
-    element: document.querySelector('#autocomplete-container'),
-    id: id,
-    minLength: 2,
-    placeholder: fallbackInputElement.placeholder,
-    //displayMenu: 'overlay', this need enable but currently breaks.
-    confirmOnBlur: false,
-    onConfirm: (SelectiedContent) => {
-    window.open("https://www.nhs.uk/search/?q=" + SelectiedContent, "_self")
-    },
-    source: getFunnelbackResults,
-    templates: {
-   // inputValue: inputValueTemplate,
-    suggestion: suggestionTemplate
-   }
-    
-  }
+    const defaultConfig = {
+        element: document.querySelector('#autocomplete-container'),
+        id: id,
+        minLength: 2,
+        //cssNamespace:
+        placeholder: fallbackInputElement.placeholder,
+        //displayMenu: 'overlay', this need enable but currently breaks.
+        confirmOnBlur: false,
+        onConfirm: (SelectiedContent) => {
+            window.open("https://www.nhs.uk/search/?q=" + SelectiedContent, "_self")
+        },
+        source: getFunnelbackResults,
+        templates: {
+            // inputValue: inputValueTemplate,
+            suggestion: suggestionTemplate
+        }
 
-function inputValueTemplate(result){
+    }
 
-function treff(){
-  window.open(result) // just here so we can inspect the dropdown
-}
+    function inputValueTemplate(result) {
 
-}
-  function suggestionTemplate (result) {
-  var truncateLength = 12;
-  var TypedText = document.getElementById(id).value 
-  var dots = result.length > truncateLength ? '...' : '';
-  result = result.substring(0, truncateLength)+dots;
-   if(!result){
-     return "";
-   }
-   var expr = TypedText;
-  
-   expr = expr.replace(/\s+/, "|",TypedText);
-   var regex = new RegExp(expr,"gi"); //g = global, match all instances & i = case insensitive 
-  result = '<a href="">' + result + '</a>' //link example - just here for formatting
-   return result.replace(regex, function($1){
-       return '<b>'+ $1 +'</b>';
-  });
+        function treff() {
+            window.open(result) // just here so we can inspect the dropdown
+        }
 
-}
-  const accessibleAutocompleteConfig = {
-    ...defaultConfig,
-    ...config,
-  }
-  function stringTruncate(str, length){
-    var dots = str.length > length ? '...' : '';
-    return str.substring(0, length)+dots;
-  };
- 
+    }
 
-  document.getElementById(id).remove()
-  accessibleAutocomplete(accessibleAutocompleteConfig)
+    function suggestionTemplate(result) {
+
+        var truncateLength = 36; //Same value as previous one
+        var dots = result.length > truncateLength ? '...' : '';
+        result = result.substring(0, truncateLength) + dots;
+
+        if (!result) {
+            return "";
+        }
+
+        var TypedText = document.getElementById(id).value
+        var expr = TypedText;
+        expr = expr.replace(/\s+/, "|", TypedText);
+        var regex = new RegExp(expr, "gi"); //g = global, match all instances & i = case insensitive 
+        result = '<a href="">' + result + '</a>' //link example - just here for formatting
+        return result.replace(regex, function($1) {
+            return '<b>' + $1 + '</b>';
+        });
+
+    }
+    const accessibleAutocompleteConfig = {
+        ...defaultConfig,
+        ...config,
+    }
+    document.getElementById(id).remove()
+    accessibleAutocomplete(accessibleAutocompleteConfig)
 
 }
- 
 
 export default autocomplete
