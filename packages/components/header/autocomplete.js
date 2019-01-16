5b97d8f314048267f8512c77911ea4c02126a23b
Michael Cheung <michaelcheung@nhs.net>
2019-01-16 11:35:23 +0000

    [refs #320] Fix bug if no search in header, plus make it more efficient
    Do checks for search divs and elements to run the autocomplete code,
    else ignore it it.

--- packages/components/header/autocomplete.js
+++ packages/components/header/autocomplete.js
@@ -43,12 +43,16 @@ function autocomplete(config) {
   const id = (config && config.id) ? config.id : defaultId;
   const fallbackInputElement = document.getElementById(id);
 
+  if (!fallbackInputElement) {
+    return;
+  }
+
   function suggestionTemplate(result) {
     const truncateLength = 36;
     const dots = result.length > truncateLength ? '...' : '';
     const resultTruncated = result.substring(0, truncateLength) + dots;
     const svgIcon = '<svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>';
-    const resultsHref = '<a href="https://www.nhs.uk/search?collection=nhs-meta&query=' + result + '">' + result + '</a>';
+    const resultsHref = '<a href="https://www.nhs.uk/search?collection=nhs-meta&query=' + result + '">' + resultTruncated + '</a>';
 
     return svgIcon + resultsHref;
   }
@@ -82,31 +86,36 @@ function autocomplete(config) {
 
 }
 
-window.addEventListener("load", function(event) {
+if (document.getElementById('wrap-search')) {
 
-  positionsAndWidths();
+  // only add event listener if 'wrap-search' exists to act on
+  window.addEventListener("load", function(event) {
 
-  // To deal with window resizing, need to reset positioning of search results dropdown
-  // Use setTimeout on resize so as not to kill CPU
-  // https://developer.mozilla.org/en-US/docs/Web/Events/resize
-  window.addEventListener("resize", resizeThrottler, false);
-
-  let resizeTimeout;
-  function resizeThrottler() {
-    // ignore resize events as long as an actualResizeHandler execution is in the queue
-    if ( !resizeTimeout ) {
-      resizeTimeout = setTimeout(function() {
-        resizeTimeout = null;
-        actualResizeHandler();
-       // The actualResizeHandler will execute at a rate of 15fps
-       }, 66);
-    }
-  }
-
-  function actualResizeHandler() {
     positionsAndWidths();
-  }
 
-});
+    // To deal with window resizing, need to reset positioning of search results dropdown
+    // Use setTimeout on resize so as not to kill CPU
+    // https://developer.mozilla.org/en-US/docs/Web/Events/resize
+    window.addEventListener("resize", resizeThrottler, false);
+
+    let resizeTimeout;
+    function resizeThrottler() {
+      // ignore resize events as long as an actualResizeHandler execution is in the queue
+      if ( !resizeTimeout ) {
+        resizeTimeout = setTimeout(function() {
+          resizeTimeout = null;
+          actualResizeHandler();
+         // The actualResizeHandler will execute at a rate of 15fps
+         }, 66);
+      }
+    }
+
+    function actualResizeHandler() {
+      positionsAndWidths();
+    }
+
+  });
+
+}
 
 export default autocomplete;
