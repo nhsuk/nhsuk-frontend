aca8d19df9d7351b1c293c753bb579b2a74696b6
Sab <sabeeh.ul-hassan1@nhs.net>
2019-01-16 12:39:26 +0000

    Add experimental bolding for search results

--- packages/components/header/autocomplete.js
+++ packages/components/header/autocomplete.js
@@ -44,11 +44,14 @@ function autocomplete(config) {
   const fallbackInputElement = document.getElementById(id);
 
   function suggestionTemplate(result) {
+    const TypedText = document.getElementById(id).value;
     const truncateLength = 36;
     const dots = result.length > truncateLength ? '...' : '';
     const resultTruncated = result.substring(0, truncateLength) + dots;
+    const regx = new RegExp(TypedText, 'g');
+    const boldedResult = resultTruncated.replace(regx, '<b>'+TypedText+'</b>')
     const svgIcon = '<svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>';
-    const resultsHref = '<a href="https://www.nhs.uk/search?collection=nhs-meta&query=' + result + '">' + result + '</a>';
+    const resultsHref = '<a href="https://www.nhs.uk/search?collection=nhs-meta&query=' + result + '">' + boldedResult + '</a>';
 
     return svgIcon + resultsHref;
   }
