e09e22891a708a78ef7356a96d2f46a7779c4a92
Adam Fenwick <afenwickdev@gmail.com>
2019-08-23 12:57:32 +0100

    Removing promo and moving promo into card

--- packages/components/card/card.js
+++ packages/components/card/card.js
@@ -1,8 +1,8 @@
 function nhsuk_card () {
-  document.querySelectorAll('.clickable-card').forEach((promo) => { /* [1] */
-    promo.classList.add('nhsuk-promo__link-wrapper'); /* [2] */
-    promo.addEventListener('click', () => { /* [3] */
-      promo.querySelector('a').click();
+  document.querySelectorAll('.clickable-card').forEach((card) => { /* [1] */
+    card.classList.add('nhsuk-card__link-wrapper'); /* [2] */
+    card.addEventListener('click', () => { /* [3] */
+      card.querySelector('a').click();
     });
   });
 }
