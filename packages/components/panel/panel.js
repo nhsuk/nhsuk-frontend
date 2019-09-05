3c3845ab28bdd2796f2a1d52cb62126ad6396789
Adam Fenwick <afenwickdev@gmail.com>
2019-09-05 14:32:12 +0100

    Renaming card back to original panel

---
+++ packages/components/panel/panel.js
@@ -0,0 +1,10 @@
+function nhsuk_panel () {
+  document.querySelectorAll('.clickable-panel').forEach((panel) => { /* [1] */
+    panel.classList.add('nhsuk-panel__link-wrapper'); /* [2] */
+    panel.addEventListener('click', () => { /* [3] */
+      panel.querySelector('a').click();
+    });
+  });
+}
+
+export default nhsuk_panel;
