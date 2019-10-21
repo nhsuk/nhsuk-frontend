1e10fe6c73583179b9a8aa5edd4896e033eda9dc
Tom Doughty <thomashdoughty@gmail.com>
2019-10-21 13:24:59 +0100

    Add unit tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'packages/**/*.js',
+  ],
+  verbose: true,
+};
--- packages/nhsuk.js
+++ packages/nhsuk.js
@@ -1,14 +1,16 @@
 // Components
-import nhsuk_header from './components/header/header';
-import nhsuk_skipLink from './components/skip-link/skip-link';
-import autocomplete from './components/header/autocomplete';
+import FeedbackBanner from './components/feedback-banner/feedback-banner';
+import Header from './components/header/header';
+import SkipLink from './components/skip-link/skip-link';
+import AutoComplete from './components/header/autocomplete';
 
 // HTML5 polyfills
 import './components/details/details.polyfill';
 
 // Initialize components
-document.addEventListener('DOMContentLoaded', function() {
-  nhsuk_header();
-  nhsuk_skipLink();
-  autocomplete();
-})
+document.addEventListener('DOMContentLoaded', () => {
+  FeedbackBanner(3000);
+  Header();
+  SkipLink();
+  AutoComplete();
+});
