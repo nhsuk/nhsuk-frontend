3753f99ecf8ed84f7900c5b7ca0c29eafa808966
Tom Doughty <thomashdoughty@gmail.com>
2019-10-22 08:28:54 +0100

    Initialise details as component not polyfill

--- packages/nhsuk.js
+++ packages/nhsuk.js
@@ -3,12 +3,11 @@ import FeedbackBanner from './components/feedback-banner/feedback-banner';
 import Header from './components/header/header';
 import SkipLink from './components/skip-link/skip-link';
 import AutoComplete from './components/header/autocomplete';
-
-// HTML5 polyfills
-import './components/details/details.polyfill';
+import Details from './components/details/details';
 
 // Initialize components
 document.addEventListener('DOMContentLoaded', () => {
+  Details();
   FeedbackBanner(3000);
   Header();
   SkipLink();
