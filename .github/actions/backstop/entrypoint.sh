8279e67b136939eda7c25490b97594cb6bb0bec2
Tom Doughty <thomashdoughty@gmail.com>
2019-11-04 21:56:58 +0000

    Run custom docker command

--- .github/actions/backstop/entrypoint.sh
+++ .github/actions/backstop/entrypoint.sh
@@ -7,4 +7,4 @@ setup="NODE_ENV=development npm install --no-package-lock &&"
 
 echo "## Running Action"
 args=$@
-sh -c "$setup ((npm run start) & npm run backstop:test)"
+sh -c "$setup ((npm run start) & docker run --rm -it --network='host' --mount type=bind,source='/home/travis/build/nhsuk/nhsuk-frontend',target=/src backstopjs/backstopjs:latest test --config=./tests/backstop/backstop.js --moby --testhost=http://127.0.0.1:3000)"
