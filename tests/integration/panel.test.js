3f3d5018a784bc04c2875f98c44e2b75260f9df0
Adam Fenwick <afenwickdev@gmail.com>
2019-11-18 16:45:22 +0000

    Updating unit tests for panel

---
+++ tests/integration/panel.test.js
@@ -0,0 +1,24 @@
+import Panel from '../../packages/components/panel/panel';
+
+const panelHtml = '<div id="nhsuk-panel" class="nhsuk-panel clickable-panel">' +
+                  '  <h3><a href="https://www.nhs.uk">Save a life: give blood</a></h3>' +
+                  '  <p>Please register today. Donating blood is easy, and saves lives.</p>' +
+  '</div>';
+
+let panel;
+const panelWrapper = 'nhsuk-panel__link-wraper'
+
+const initTest = (html = '') => {
+  document.body.innerHTML = html;
+  panel = document.querySelector('panel');
+  Panel();
+};
+
+describe('NHS.UK panel', () => {
+  describe('should add wrapper class to panel', () => {
+    it('if clickable-panel class exists', () => {
+      initTest(panelHtml);
+      expect(panel.classList).toContain(panelWrapper);
+    });
+  });
+});
