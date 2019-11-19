a9bdd919b6709e14b7e9dbed24d0fea30bc2a298
Adam Fenwick <afenwickdev@gmail.com>
2019-11-19 10:46:37 +0000

    Updating JS and tests for panel

--- packages/components/panel/panel.js
+++ packages/components/panel/panel.js
@@ -1,8 +1,10 @@
 export default () => {
   document.querySelectorAll('.clickable-panel').forEach((card) => { /* [1] */
-    card.classList.add('nhsuk-panel__link-wrapper'); /* [2] */
-    card.addEventListener('click', () => { /* [3] */
-      card.querySelector('a').click();
-    });
+    if (card.querySelector('a') !== null) {
+      card.classList.add('nhsuk-panel__link-wrapper'); /* [2] */
+      card.addEventListener('click', () => { /* [3] */
+        card.querySelector('a').click();
+      });
+    }
   });
 };
--- tests/integration/panel.test.js
+++ tests/integration/panel.test.js
@@ -1,16 +1,16 @@
 import Panel from '../../packages/components/panel/panel';
 
-const panelHtml = '<div id="nhsuk-panel" class="nhsuk-panel clickable-panel">' +
-                  '  <h3><a href="https://www.nhs.uk">Save a life: give blood</a></h3>' +
-                  '  <p>Please register today. Donating blood is easy, and saves lives.</p>' +
-  '</div>';
+const panelHtml = '<div id="nhsuk-panel" class="nhsuk-panel clickable-panel">'
+                  + '  <h3><a href="https://www.nhs.uk">Save a life: give blood</a></h3>'
+                  + '  <p>Please register today. Donating blood is easy, and saves lives.</p>'
+                  + '</div>';
 
 let panel;
-const panelWrapper = 'nhsuk-panel__link-wraper'
+const panelWrapper = 'nhsuk-panel__link-wrapper';
 
 const initTest = (html = '') => {
   document.body.innerHTML = html;
-  panel = document.querySelector('panel');
+  panel = document.querySelector('.nhsuk-panel');
   Panel();
 };
 
