2482d3e47e05b226f2e3bf7b3883e8963e805ac6
Paul Flynn <pgflynn@gmail.com>
2020-08-26 11:29:49 +0100

    Kick off changing api refernce

--- packages/components/header/headerAutoComplete.js
+++ packages/components/header/headerAutoComplete.js
@@ -9,9 +9,15 @@ const suggestion = (result) => {
   const truncateLength = 36;
   const dots = result.length > truncateLength ? '...' : '';
   const resultTruncated = result.substring(0, truncateLength) + dots;
+  // return `
+  //   <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>
+  //   <a href="https://www.nhs.uk/search?collection=nhs-meta&query=${result}">
+  //     ${resultTruncated}
+  //   </a>
+  // `;
   return `
     <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>
-    <a href="https://www.nhs.uk/search?collection=nhs-meta&query=${result}">
+    <a href="https://nhsuk-site-search-dev-uks.azurewebsites.net/search/?q=${result}">
       ${resultTruncated}
     </a>
   `;
@@ -24,9 +30,12 @@ const suggestion = (result) => {
 */
 const source = (query, populateResults) => {
   // Build URL for search endpoint
-  const rootUrl = 'https://nhs.funnelback.co.uk/s/suggest.json';
-  const maxResults = 10;
-  const fullUrl = `${rootUrl}?collection=nhs-meta&partial_query=${query}&sort=0&fmt=json++&profile=&show=${maxResults}`;
+  // const rootUrl = 'https://nhs.funnelback.co.uk/s/suggest.json';
+  // const maxResults = 10;
+  // const fullUrl = `${rootUrl}?collection=nhs-meta&partial_query=${query}&sort=0&fmt=json++&profile=&show=${maxResults}`;
+  // const maxResults = 10;
+  const rootUrl = 'https://nhsuk-apim-dev-uks.azure-api.net/site-search/Autocomplete';
+  const fullUrl = `${rootUrl}?q=${query}&api-version=1`;
 
   // Async request for results based on query param
   const xhr = new XMLHttpRequest();
