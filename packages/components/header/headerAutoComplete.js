f8e1ec5c52e33c5d0af7db40b2e405a4387d5d68
Tom Doughty <thomashdoughty@gmail.com>
2020-08-26 12:06:44 +0100

    Set up to use new api or funnelback

--- packages/components/header/headerAutoComplete.js
+++ packages/components/header/headerAutoComplete.js
@@ -1,5 +1,12 @@
 import AutoComplete from './autoCompleteConfig';
 
+// Standard search variables
+// const searchApiUrl = 'https://www.nhs.uk/s/suggest.json';
+// const searchPageUrl = 'https://www.nhs.uk/search/?q=';
+// IA test search variables
+const searchApiUrl = 'https://nhsuk-site-search-dev-uks.azurewebsites.net/site-search/Autocomplete';
+const searchPageUrl = 'https://nhsuk-site-search-dev-uks.azurewebsites.net/search/?q=';
+
 /**
  * Function to build truncated result with svg for search autocomplete
  * @param {string} result String containing individual result from autocomplete source function
@@ -9,15 +16,9 @@ const suggestion = (result) => {
   const truncateLength = 36;
   const dots = result.length > truncateLength ? '...' : '';
   const resultTruncated = result.substring(0, truncateLength) + dots;
-  // return `
-  //   <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>
-  //   <a href="https://www.nhs.uk/search?collection=nhs-meta&query=${result}">
-  //     ${resultTruncated}
-  //   </a>
-  // `;
   return `
     <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>
-    <a href="https://nhsuk-site-search-dev-uks.azurewebsites.net/search/?q=${result}">
+    <a href="${searchPageUrl}${result}">
       ${resultTruncated}
     </a>
   `;
@@ -30,14 +31,8 @@ const suggestion = (result) => {
 */
 const source = (query, populateResults) => {
   // Build URL for search endpoint
-  // const rootUrl = 'https://nhs.funnelback.co.uk/s/suggest.json';
-  // const maxResults = 10;
-  // const fullUrl =
-  // `${rootUrl}?collection=nhs-meta&partial_query=
-  // ${query}&sort=0&fmt=json++&profile=&show=${maxResults}`;
-  // const maxResults = 10;
-  const rootUrl = 'https://nhsuk-apim-dev-uks.azure-api.net/site-search/Autocomplete';
-  const fullUrl = `${rootUrl}?q=${query}&api-version=1`;
+  const maxResults = 10;
+  const fullUrl = `${searchApiUrl}?collection=nhs-meta&partial_query=${query}&sort=0&fmt=json++&profile=&show=${maxResults}&q=${query}&api-version=1`;
 
   // Async request for results based on query param
   const xhr = new XMLHttpRequest();
@@ -46,7 +41,8 @@ const source = (query, populateResults) => {
     if (xhr.status === 200) {
       // Array of display values from json
       const results = JSON.parse(xhr.responseText)
-        .map(({ disp }) => disp);
+        // Handle new search API or Funnelback
+        .map((result) => result.query || result.disp);
 
       // Fire callback from autoComplete plugin
       populateResults(results);
