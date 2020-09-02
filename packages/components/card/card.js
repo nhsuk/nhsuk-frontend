749ed9d843dba9cf8c07679bb431bc1e1d93c612
davidhunter08 <thebeakking@gmail.com>
2020-09-02 12:28:16 +0100

    add card component, remove promo and panel

---
+++ packages/components/card/card.js
@@ -0,0 +1,10 @@
+// Loops through dom and finds all elements with nhsuk-card--clickable class
+document.querySelectorAll('.nhsuk-card--clickable').forEach((panel) => {
+  // Check if panel has a link within it
+  if (panel.querySelector('a') !== null) {
+    // Clicks the link within the heading to navigate to desired page
+    panel.addEventListener('click', () => {
+      panel.querySelector('a').click();
+    });
+  }
+});
