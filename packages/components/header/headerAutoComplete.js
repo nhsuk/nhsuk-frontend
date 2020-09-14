6ad796e12b2efb6708ae177317e7dd899b328033
Tom Doughty <thomashdoughty@gmail.com>
2020-09-14 21:22:56 +0100

    Allow URLs to be set from HTML

--- packages/components/header/headerAutoComplete.js
+++ packages/components/header/headerAutoComplete.js
@@ -1,12 +1,8 @@
 import AutoComplete from './autoCompleteConfig';
 
-// Standard search variables
-// const searchApiUrl = 'https://www.nhs.uk/s/suggest.json';
-// const searchPageUrl = 'https://www.nhs.uk/search/?q=';
-// IA test search variables
-const searchApiUrl = 'https://nhsuk-site-search-dev-uks.azurewebsites.net/site-search/Autocomplete';
-const searchPageUrl = 'https://nhsuk-site-search-dev-uks.azurewebsites.net/search/?q=';
-
+// Search variables which can be set from HTML
+const searchApiUrl = window.NHSUK_SETTINGS.SUGGESTIONS_TEST_HOST || 'https://www.nhs.uk/s/suggest.json';
+const searchPageUrl = window.NHSUK_SETTINGS.SEARCH_TEST_HOST || 'https://www.nhs.uk/search/';
 /**
  * Function to build truncated result with svg for search autocomplete
  * @param {string} result String containing individual result from autocomplete source function
@@ -18,7 +14,7 @@ const suggestion = (result) => {
   const resultTruncated = result.substring(0, truncateLength) + dots;
   return `
     <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>
-    <a href="${searchPageUrl}${result}">
+    <a href="${searchPageUrl}?q=${result}">
       ${resultTruncated}
     </a>
   `;
