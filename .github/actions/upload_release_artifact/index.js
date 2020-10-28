a0159cc0566a54293c6eb65e8c40d44dbc10df0a
Tom Doughty <thomashdoughty@gmail.com>
2020-10-28 08:12:14 +0000

    Update to octokit 4.0

--- .github/actions/upload_release_artifact/index.js
+++ .github/actions/upload_release_artifact/index.js
@@ -1,15 +1,15 @@
 const core = require('@actions/core');
-const { GitHub } = require('@actions/github');
+const github = require('@actions/github');
 const fs = require('fs');
 const { version } = require('./package.json');
 
 (async () => {
   try {
+    // Get event object from release creation, handy for understanding what else you can do
     const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH));
     console.log(event);
 
-    // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
     const assetPath = `./dist/nhsuk-frontend-${version}.zip`;
     const assetName = `nhsuk-frontend-${version}.zip`;
     
@@ -22,7 +22,7 @@ const { version } = require('./package.json');
     // Upload a release asset
     // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
     // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset
-    await github.repos.uploadReleaseAsset({
+    await octokit.repos.uploadReleaseAsset({
       url: event.release.upload_url,
       headers,
       name: assetName,
