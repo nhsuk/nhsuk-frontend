221f8df8068515b552c37d4321f37ee6440699ff
Tom Doughty <thomashdoughty@gmail.com>
2020-11-18 10:37:41 +0000

    Add onConfirm to header autocomplete

--- packages/components/header/headerAutoComplete.js
+++ packages/components/header/headerAutoComplete.js
@@ -18,9 +18,7 @@ const suggestion = (result) => {
   const resultTruncated = result.substring(0, truncateLength) + dots;
   return `
     <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>
-    <a href="${searchPageUrl}?q=${result}">
-      ${resultTruncated}
-    </a>
+    ${resultTruncated}
   `;
 };
 
@@ -52,11 +50,21 @@ const source = (query, populateResults) => {
   xhr.send();
 };
 
+/**
+ * Callback function to redirect to NHS Search page when an
+ * option is selected either with click or enter.
+ * @param {string} result Query to pass to search page URL
+*/
+const onConfirm = (result) => {
+  window.location.href = `${searchPageUrl}?q=${result}`;
+};
+
 export default () => {
   AutoComplete({
     containerId: 'autocomplete-container',
     formId: 'search',
     inputId: 'search-field',
+    onConfirm,
     source,
     templates: {
       suggestion,
