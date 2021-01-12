bb0c0dc28efdef8e8842a53f4ef4866c5b3a358a
Tom Doughty <thomashdoughty@gmail.com>
2021-01-12 11:26:33 +0000

    remove IE8-10 JavaScript polyfills and utils
    As our list of supported browsers has changed we are now able to use
    more JavaScript APIs and properties without polyfilling or writing
    our own solutions.

--- packages/common.js
+++ packages/common.js
@@ -1,72 +1,4 @@
 /**
- * Get an array of an HTML elements classes
- * @param {HTMLElement} element
- * @returns {string[]} string array of class names
-*/
-export const getClasses = (element) => {
-  // Return without error if element or class are missing
-  if (!element) return [];
-  return element.className.split(' ').filter(Boolean);
-};
-
-/**
- * Does a class exist on an element
- * @param {HTMLElement} element
- * @returns {Boolean} true if element has class
-*/
-export const hasClass = (element, className) => {
-  // Return without error if element or class are missing
-  if (!element || !className) return false;
-  return getClasses(element).includes(className);
-};
-
-/**
- * Remove a class on a HTML element
- * @param {HTMLElement} element
- * @param {string} className
-*/
-export const removeClass = (element, className) => {
-  // Return without error if element or class are missing
-  if (!element || !className) return;
-  if (hasClass(element, className)) {
-    // Array of all existing classes
-    const existingClasses = getClasses(element, className);
-    // String of existing classes minus the class to remove
-    const newClasses = existingClasses.filter((existingClass) => existingClass !== className).join(' ');
-    // Set class attribute to the new classes
-    element.setAttribute('class', newClasses);
-  }
-};
-
-/**
- * Add a class on a HTML element
- * @param {HTMLElement} element
- * @param {string} className
-*/
-export const addClass = (element, className) => {
-  // Return without error if element or class are missing
-  if (!element || !className) return;
-  if (!hasClass(element, className)) {
-    // Set class attribute to the new classes
-    element.setAttribute('class', `${element.className} ${className}`.trim());
-  }
-};
-
-/**
- * Toggle a class on a HTML element
- * @param {HTMLElement} element
- * @param {string} className
-*/
-export const toggleClass = (element, className) => {
-  if (!element || !className) return;
-  if (hasClass(element, className)) {
-    removeClass(element, className);
-  } else {
-    addClass(element, className);
-  }
-};
-
-/**
  * Toggle a boolean attribute on a HTML element
  * @param {HTMLElement} element
  * @param {string} attr
@@ -93,7 +25,7 @@ export const toggleConditionalInput = (input, className) => {
     // Get the conditional element from the input data-aria-controls attribute
     const conditionalElement = document.getElementById(conditionalId);
     if (conditionalElement) {
-      toggleClass(conditionalElement, className);
+      conditionalElement.classList.toggle(className);
       toggleAttribute(input, 'aria-expanded');
     }
   }
--- packages/components/checkboxes/checkboxes.js
+++ packages/components/checkboxes/checkboxes.js
@@ -1,20 +1,25 @@
 import { toggleConditionalInput } from '../../common';
 
-/*
-* NHS.UK checkbox buttons.
-*
-* Conditionally show content when a checkbox button is checked
+/**
+ * Conditionally show content when a checkbox button is checked
+ * Test at http://0.0.0.0:3000/components/checkboxes/conditional.html
 */
 
 export default () => {
-  // All checkbox inputs inside a conditional form group
+  // Checkbox input DOMElements inside a conditional form group
   const checkboxInputs = document.querySelectorAll('.nhsuk-checkboxes--conditional .nhsuk-checkboxes__input');
 
-  // Add event listener to every checkbox button
-  Array.prototype.slice.call(checkboxInputs).forEach((checkboxButton) => {
-    checkboxButton.addEventListener('change', (e) => {
-      // Toggle conditional content based on checked state
-      toggleConditionalInput(e.target, 'nhsuk-checkboxes__conditional--hidden');
-    });
+  /**
+   * Toggle classes and attributes
+   * @param {Object} event click event object
+  */
+  const handleClick = (event) => {
+    // Toggle conditional content based on checked state
+    toggleConditionalInput(event.target, 'nhsuk-checkboxes__conditional--hidden');
+  };
+
+  // Attach handleClick as click to checkboxInputs
+  checkboxInputs.forEach((checkboxButton) => {
+    checkboxButton.addEventListener('change', handleClick);
   });
 };
