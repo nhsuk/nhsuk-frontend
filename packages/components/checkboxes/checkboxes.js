4f91d117a10c31ad66e024e52331be51b8136591
Andy Mantell <134642+andymantell@users.noreply.github.com>
2021-07-23 13:34:06 +0100

    Add "None of these" JavaScript behaviour to checkboxes
    See original at https://github.com/alphagov/govuk-frontend/pull/2151

--- packages/components/checkboxes/checkboxes.js
+++ packages/components/checkboxes/checkboxes.js
@@ -5,6 +5,52 @@ import { toggleConditionalInput } from '../../common';
  * Test at http://0.0.0.0:3000/components/checkboxes/conditional.html
 */
 
+/**
+ * Uncheck other checkboxes
+ *
+ * Find any other checkbox inputs with the same name value, and uncheck them.
+ * This is useful for when a “None of these" checkbox is checked.
+ */
+ const unCheckAllInputsExcept = function (input) {
+  const allInputsWithSameName = document.querySelectorAll('input[type="checkbox"][name="' + input.name + '"]')
+
+  allInputsWithSameName.forEach((inputWithSameName) => {
+    const hasSameFormOwner = input.form === inputWithSameName.form
+    if (hasSameFormOwner && inputWithSameName !== input) {
+      inputWithSameName.checked = false
+    }
+  })
+
+  syncAllConditionalReveals(input)
+}
+
+/**
+ * Uncheck exclusive inputs
+ *
+ * Find any checkbox inputs with the same name value and the 'exclusive' behaviour,
+ * and uncheck them. This helps prevent someone checking both a regular checkbox and a
+ * "None of these" checkbox in the same fieldset.
+ */
+const unCheckExclusiveInputs = function (input) {
+  const allInputsWithSameNameAndExclusiveBehaviour = document.querySelectorAll(
+    'input[data-behaviour="exclusive"][type="checkbox"][name="' + input.name + '"]'
+  )
+
+  allInputsWithSameNameAndExclusiveBehaviour.forEach((exclusiveInput) => {
+    const hasSameFormOwner = input.form === exclusiveInput.form
+    if (hasSameFormOwner) {
+      exclusiveInput.checked = false
+    }
+  })
+
+  syncAllConditionalReveals(input)
+}
+
+const syncAllConditionalReveals = function(input) {
+  const allInputsWithSameName = document.querySelectorAll('input[type="checkbox"][name="' + input.name + '"]');
+  Array.from(allInputsWithSameName).map((input) => toggleConditionalInput(input, 'nhsuk-checkboxes__conditional--hidden'));
+}
+
 export default () => {
   // Checkbox input DOMElements inside a conditional form group
   const checkboxInputs = document.querySelectorAll('.nhsuk-checkboxes--conditional .nhsuk-checkboxes__input');
@@ -16,6 +62,17 @@ export default () => {
   const handleClick = (event) => {
     // Toggle conditional content based on checked state
     toggleConditionalInput(event.target, 'nhsuk-checkboxes__conditional--hidden');
+
+    if (!event.target.checked) {
+      return
+    }
+
+    // Handle 'exclusive' checkbox behaviour (ie "None of these")
+    if (event.target.getAttribute('data-behaviour') === 'exclusive') {
+      unCheckAllInputsExcept(event.target)
+    } else {
+      unCheckExclusiveInputs(event.target)
+    }
   };
 
   // Attach handleClick as click to checkboxInputs
