d9c6bf370058aa08ba2090541bd1a3562cd31f6d
w@rren <w@rren.dev>
2022-07-22 18:16:44 +0100

    Copied in suggested formatting config
    - with a cheeky change to tab

--- tests/linters/.eslintrc.js
+++ tests/linters/.eslintrc.js
@@ -1,9 +1,32 @@
 module.exports = {
-  "extends": [
-    "nhsuk"
-  ],
-  "env": {
-    "browser": true,
-    "jest": true
-  }
+  extends: ["airbnb-base/legacy", "plugin:prettier/recommended"],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false,
+  },
+  env: {
+    browser: true,
+    node: true,
+    jquery: true,
+    es6: true,
+    jest: true,
+  },
+  rules: {
+    "no-console": 0,
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "resolve|reject|err",
+      },
+    ],
+    "no-plusplus": 0,
+    "no-underscore-dangle": 0,
+    "no-param-reassign": 0,
+  },
 };
