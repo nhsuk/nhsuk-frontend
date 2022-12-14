7e896cd2af5161b3bae4217c99370a12e45777d7
Andy Mantell <134642+andymantell@users.noreply.github.com>
2022-12-14 14:29:11 +0000

    Tests for the button space bar activation and double click prevention

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  browserContext: "incognito",
+  browserPerWorker: true,
+
+  /**
+   * Workaround for jest-environment-puppeteer 'uncaughtException'
+   * see error handling in ./config/jest/environment/puppeteer.mjs
+   */
+  exitOnPageError: false,
+
+  /**
+   * @type {import('puppeteer').PuppeteerLaunchOptions}
+   */
+  launch: {
+    args: [
+      /**
+       * Workaround for 'No usable sandbox! Update your kernel' error
+       * see more https://github.com/Googlechrome/puppeteer/issues/290
+       */
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+
+      /**
+       * Prevent empty Chromium startup window
+       * Tests use their own `browser.newPage()` instead
+       */
+      "--no-startup-window",
+    ],
+    waitForInitialPage: false,
+  },
+};
