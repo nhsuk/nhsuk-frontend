f3ac2eb910d5f46d90a8846516c30c23a2174e6e
diana-nita <diana.nita1@nhs.net>
2023-11-03 10:56:56 +0000

    updated tests

--- tests/integration/jsdom/header.test.js
+++ tests/integration/jsdom/header.test.js
@@ -19,6 +19,7 @@ describe('Header class', () => {
             <div class="nhsuk-header__menu-toggle"></div>
             `
     })
+
     it('Should create navigation elements in the DOM', async () => {
         // Call the Header initialization function
         await Header();
@@ -98,17 +99,67 @@ describe('Header class', () => {
         expect(mobileMenuList.classList).toContain('nhsuk-header__drop-down--hidden');
     });
 
-    it.only('Should update navigation', async () => {
+    it('Should not update navigation when the available space is enough for all elements', async () => {
         const mobileMenuToggleButton = document.querySelector('.nhsuk-header__menu-toggle');
         const mobileMenuContainer = document.querySelector('.nhsuk-mobile-menu-container');
+        const navigationElement = document.querySelector('.nhsuk-navigation');
+        const navigationList = document.querySelector('.nhsuk-header__navigation-list');
+        let mobileMenuList = document.querySelector('.nhsuk-mobile-menu-container ul');
 
+        // Spy on offsetWidth property for navigation element
+        const navigationOffsetWidthSpy = jest.spyOn(HTMLElement.prototype, 'offsetWidth', 'get');
+        // Mock offsetWidth for navigation element
+        navigationOffsetWidthSpy.mockImplementation(function () {
+            if (this === navigationElement) {
+                return 1000; // Mock navigation element offsetWidth
+            }
+            return 50; // Mock children offsetWidth
+        });
+
+        await Header();
+
+        // breakpoints will be [50,100,150,200,250,300,350,400]
+        // the available space - navigation offsetWidth - will be greater than the last element from the breakpoints array
+        // meaning we don't need the mobile menu to get any items from the navigation
         expect(mobileMenuToggleButton.classList.contains('nhsuk-header__menu-toggle--visible')).toBe(false);
         expect(mobileMenuContainer.classList.contains('nhsuk-mobile-menu-container--visible')).toBe(false);
+       
+        mobileMenuList = document.querySelector('.nhsuk-mobile-menu-container ul');
+        expect(mobileMenuList.children).toHaveLength(0);
+        expect(navigationList.children).toHaveLength(8);
 
-        Header();
-
-        expect(mobileMenuToggleButton.classList.contains('nhsuk-header__menu-toggle--visible')).toEqual(true);
-        expect(mobileMenuContainer.classList.contains('nhsuk-mobile-menu-container--visible')).toEqual(true);
+        navigationOffsetWidthSpy.mockRestore();
     });
 
+    it('Should update navigation when the available space is not enough for all elements', async () => {
+        const mobileMenuToggleButton = document.querySelector('.nhsuk-header__menu-toggle');
+        const mobileMenuContainer = document.querySelector('.nhsuk-mobile-menu-container');
+        const navigationElement = document.querySelector('.nhsuk-navigation');
+        const navigationList = document.querySelector('.nhsuk-header__navigation-list');
+        let mobileMenuList = document.querySelector('.nhsuk-mobile-menu-container ul');
+
+        // Spy on offsetWidth property for navigation element
+        const navigationOffsetWidthSpy = jest.spyOn(HTMLElement.prototype, 'offsetWidth', 'get');
+        // Mock offsetWidth for navigation element
+        navigationOffsetWidthSpy.mockImplementation(function () {
+            if (this === navigationElement) {
+                return 700; // Mock navigation element offsetWidth
+            }
+            return 100; // Mock children offsetWidth
+        });
+
+        await Header();
+
+        // breakpoints will be [100,200,300,400,500,600,700,800]
+        // the available space - navigation offsetWidth - will be smaller than the last element from the breakpoints array
+        // meaning we need the mobile menu to get 1 item from the navigation
+        expect(mobileMenuToggleButton.classList.contains('nhsuk-header__menu-toggle--visible')).toBe(true);
+        expect(mobileMenuContainer.classList.contains('nhsuk-mobile-menu-container--visible')).toBe(true);
+       
+        mobileMenuList = document.querySelector('.nhsuk-mobile-menu-container ul');
+        expect(mobileMenuList.children).toHaveLength(1);
+        expect(navigationList.children).toHaveLength(7);
+
+        navigationOffsetWidthSpy.mockRestore();
+    });
 })
