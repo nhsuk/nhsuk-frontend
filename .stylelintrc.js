68cc940f3018ba3dc921002eae71b3212a3106ab
Colin Rotherham <work@colinr.com>
2025-02-11 08:25:47 +0000

    Update Stylelint packages + config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,21 @@
 module.exports = {
-  extends: ['stylelint-config-prettier'],
+  ignoreFiles: [
+    '**/dist/**',
+    '**/vendor/**',
+
+    // Ignore CSS-in-JS (including dotfiles)
+    '**/?(.)*.{cjs,js,mjs}'
+  ],
+  overrides: [
+    {
+      customSyntax: 'postcss-markdown',
+      files: ['**/*.md']
+    },
+    {
+      customSyntax: 'postcss-scss',
+      files: ['**/*.scss']
+    }
+  ],
   plugins: ['stylelint-order', 'stylelint-scss'],
   rules: require('./tests/linters/stylelint-rules')
 }
