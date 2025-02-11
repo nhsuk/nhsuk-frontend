b582328081685c79294343a15a2460a85c6b8953
Colin Rotherham <work@colinr.com>
2025-02-11 08:37:10 +0000

    Add documentation to remaining rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -28,9 +28,51 @@ module.exports = {
     },
     {
       customSyntax: 'postcss-scss',
-      files: ['**/*.scss']
+      files: ['**/*.scss'],
+      rules: {
+        // Disallow hex colours
+        // https://stylelint.io/user-guide/rules/color-no-hex/
+        'color-no-hex': true,
+
+        // Properties and values that are disallowed
+        // https://stylelint.io/user-guide/rules/declaration-property-value-disallowed-list/
+        'declaration-property-value-disallowed-list': {
+          transition: ['all'],
+          '/^border/': ['none']
+        },
+
+        // Disallow @mixin before @extends
+        // https://github.com/hudochenkov/stylelint-order/blob/master/rules/order/README.md#extended-at-rule-objects
+        'order/order': [
+          {
+            type: 'at-rule',
+            name: 'extend'
+          },
+          {
+            type: 'at-rule',
+            name: 'mixin'
+          }
+        ],
+
+        // Allow underscores (but not numbers) in mixin naming pattern
+        // https://github.com/stylelint-scss/stylelint-scss/tree/master/src/rules/at-mixin-pattern
+        'scss/at-mixin-pattern': [
+          /^([a-z-_])*$/,
+          {
+            message: 'Mixin names may only contain [a-z-_] characters'
+          }
+        ],
+
+        // Allow underscores (but not numbers) in %placeholder naming pattern
+        // https://github.com/stylelint-scss/stylelint-scss/tree/master/src/rules/percent-placeholder-pattern
+        'scss/percent-placeholder-pattern': [
+          /^([a-z-_])*$/,
+          {
+            message: 'Placeholders may only contain [a-z-_] characters'
+          }
+        ]
+      }
     }
   ],
-  plugins: ['stylelint-order'],
-  rules: require('./tests/linters/stylelint-rules')
+  plugins: ['stylelint-order']
 }
