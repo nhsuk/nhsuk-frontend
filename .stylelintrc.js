03353d27af1bb739c50a401b1ff711a8a6bb926e
Colin Rotherham <work@colinr.com>
2025-02-11 08:34:08 +0000

    Fix or ignore Stylelint issues in `*.md`

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -12,6 +12,20 @@ module.exports = {
       files: ['**/*.md']
     },
     {
+      customSyntax: 'postcss-markdown',
+      files: ['**/coding-standards.md', '**/README.md'],
+      rules: {
+        // Allow markdown `*.md` CSS bad examples
+        'block-no-empty': null,
+        'color-no-hex': null,
+        'selector-max-id': null,
+        'selector-no-qualifying-type': null,
+
+        // Allow markdown `*.md` Sass bad examples
+        'scss/dollar-variable-pattern': null
+      }
+    },
+    {
       customSyntax: 'postcss-scss',
       files: ['**/*.scss']
     }
