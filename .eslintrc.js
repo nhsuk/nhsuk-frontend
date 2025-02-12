49807fba321ccc944db1fcbd9220a3a17cf90ef6
Colin Rotherham <work@colinr.com>
2025-02-12 16:45:36 +0000

    Update ESLint packages + config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
   extends: ['airbnb-base', 'prettier'],
   parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false
-  },
-  env: {
-    browser: true,
-    node: true,
-    jquery: true,
-    es6: true,
-    jest: true
-  },
+  ignorePatterns: [
+    '**/coverage/**',
+    '**/dist/**',
+
+    // Enable dotfile linting
+    '!.*',
+    'node_modules',
+    'node_modules/.*'
+  ],
   rules: {
     'no-console': 0,
     'no-unused-vars': [
@@ -22,5 +21,24 @@ module.exports = {
     'no-plusplus': 0,
     'no-underscore-dangle': 0,
     'no-param-reassign': 0
-  }
+  },
+  overrides: [
+    {
+      files: ['packages/**/*.{cjs,js,mjs}', '**/*.test.{cjs,js,mjs}'],
+      env: {
+        browser: true
+      },
+      parserOptions: {
+        sourceType: 'module'
+      }
+    },
+    {
+      files: ['**/engine_scripts/**/*.{cjs,js,mjs}', '**/*.test.{cjs,js,mjs}'],
+      env: {
+        browser: true,
+        jest: true
+      }
+    }
+  ],
+  root: true
 }
