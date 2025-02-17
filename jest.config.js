be631c440e9dc0a95b746f621026f0668fa6a64f
Colin Rotherham <work@colinr.com>
2025-02-17 20:42:24 +0000

    Align Puppeteer config with GOV.UK Frontend

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
+const jestPuppeteerConfig = require('./jest-puppeteer.config.js')
+
+// Detect when browser has been launched headless
+const { headless = true } = jestPuppeteerConfig.launch
+
 module.exports = {
-  collectCoverage: true,
   collectCoverageFrom: ['packages/**/*.js'],
-  globals: {
-    window: true
-  },
-  verbose: true,
-  testTimeout: 15000,
+
+  // Reduce CPU usage during project test runs
+  maxWorkers: headless
+    ? '50%' // Matches Jest default (50%) via `--watch`
+    : 1, // Use only 1x browser window when headless
+
   projects: [
     {
       displayName: 'JSDom',
@@ -19,5 +24,11 @@ module.exports = {
       testEnvironment: 'jest-environment-puppeteer',
       testMatch: ['<rootDir>/tests/integration/puppeteer/**/*.test.js']
     }
-  ]
+  ],
+
+  // Enable GitHub Actions reporter UI
+  reporters: ['default', 'github-actions'],
+
+  // Browser test increased timeout (5s to 15s)
+  testTimeout: 15000
 }
