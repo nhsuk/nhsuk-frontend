40b397107b1c50019b9768302124ebcefe331b1c
Colin Rotherham <work@colinr.com>
2025-03-05 18:26:14 +0000

    Add helper `setFocus()` from GOV.UK Frontend

--- packages/common.js
+++ packages/common.js
@@ -35,3 +35,51 @@ export const toggleConditionalInput = (input, className) => {
     }
   }
 }
+
+/**
+ * Move focus to element
+ *
+ * Sets tabindex to -1 to make the element programmatically focusable,
+ * but removes it on blur as the element doesn't need to be focused again.
+ *
+ * Original code taken from GDS (Government Digital Service)
+ * {@link https://github.com/alphagov/govuk-frontend}
+ *
+ * @template {HTMLElement} FocusElement
+ * @param {FocusElement} $element - HTML element
+ * @param {object} [options] - Handler options
+ * @param {function(this: FocusElement): void} [options.onBeforeFocus] - Callback before focus
+ * @param {function(this: FocusElement): void} [options.onBlur] - Callback on blur
+ */
+export function setFocus($element, options = {}) {
+  const isFocusable = $element.getAttribute('tabindex')
+
+  if (!isFocusable) {
+    $element.setAttribute('tabindex', '-1')
+  }
+
+  /**
+   * Handle element focus
+   */
+  function onFocus() {
+    $element.addEventListener('blur', onBlur, { once: true })
+  }
+
+  /**
+   * Handle element blur
+   */
+  function onBlur() {
+    options.onBlur?.call($element)
+
+    if (!isFocusable) {
+      $element.removeAttribute('tabindex')
+    }
+  }
+
+  // Add listener to reset element on blur, after focus
+  $element.addEventListener('focus', onFocus, { once: true })
+
+  // Focus element
+  options.onBeforeFocus?.call($element)
+  $element.focus()
+}
