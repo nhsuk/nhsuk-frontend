8f087e5a8830d99403e83c92a07f4b1f2838410e
Colin Rotherham <work@colinr.com>
2025-04-11 17:13:17 +0100

    Add JSDoc

--- tests/backstop/engine_scripts/playwright/onReadyResize.js
+++ tests/backstop/engine_scripts/playwright/onReadyResize.js
@@ -1,8 +1,17 @@
 const onReady = require('./onReady')
 
+/**
+ * @param {Page} page
+ * @param {Scenario} scenario
+ */
 module.exports = async function (page, scenario) {
   await onReady(page, scenario)
 
   await page.evaluate(() => window.dispatchEvent(new Event('resize')))
   await page.waitForTimeout(200)
 }
+
+/**
+ * @import { Scenario } from 'backstopjs'
+ * @import { Page } from 'playwright-core'
+ */
