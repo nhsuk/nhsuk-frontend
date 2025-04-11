c8b073b165c2aaa2d96886a5db44407b62abb5f1
Colin Rotherham <work@colinr.com>
2025-04-11 17:29:39 +0100

    Lower screenshot delay to 200ms

--- tests/backstop/engine_scripts/playwright/onReadyResize.js
+++ tests/backstop/engine_scripts/playwright/onReadyResize.js
@@ -2,5 +2,5 @@ module.exports = async (page, scenario) => {
   await require('./onReady')(page, scenario);
 
   await page.evaluate(() => window.dispatchEvent(new Event('resize')));
-  await page.waitForTimeout(300);
+  await page.waitForTimeout(200);
 };
