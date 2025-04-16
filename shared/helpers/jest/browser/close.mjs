a879c1cf377aceb245ba1f963821111c70d3f2fd
Colin Rotherham <work@colinr.com>
2025-04-16 15:57:54 +0100

    Various JSDoc fixes

--- shared/helpers/jest/browser/close.mjs
+++ shared/helpers/jest/browser/close.mjs
@@ -3,9 +3,13 @@ import teardown from 'jest-environment-puppeteer/teardown'
 /**
  * Close browser
  *
- * @param {import('jest').Config} jestConfig - Jest config
+ * @param {Config} jestConfig - Jest config
  */
 export default (jestConfig) => {
   // Close browser, stop server (also in watch mode)
   return teardown({ ...jestConfig, watch: false })
 }
+
+/**
+ * @import { Config } from 'jest'
+ */
