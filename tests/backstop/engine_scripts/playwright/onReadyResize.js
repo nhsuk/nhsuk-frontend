31d63f39d8334fc6c86a84ce903b5c76527e3934
Colin Rotherham <work@colinr.com>
2025-04-22 17:07:54 +0100

    Fix ESLint and Prettier issues

--- tests/backstop/engine_scripts/playwright/onReadyResize.js
+++ tests/backstop/engine_scripts/playwright/onReadyResize.js
@@ -1,6 +1,8 @@
-module.exports = async (page, scenario) => {
-  await require('./onReady')(page, scenario);
+const onReady = require('./onReady')
 
-  await page.evaluate(() => window.dispatchEvent(new Event('resize')));
-  await page.waitForTimeout(200);
-};
+module.exports = async function (page, scenario) {
+  await onReady(page, scenario)
+
+  await page.evaluate(() => window.dispatchEvent(new Event('resize')))
+  await page.waitForTimeout(200)
+}
