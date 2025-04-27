91a16e11674ad7d0bbc4dbdd9ffbc1432034c591
Frankie Roberto <frankie@frankieroberto.com>
2025-04-27 12:42:59 +0100

    Add macro options for Pagination

--- packages/nhsuk-frontend/src/nhsuk/components/pagination/macro-options.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/pagination/macro-options.mjs
@@ -13,25 +13,124 @@ export const params = {
     required: false,
     description: 'The ID of the pagination container.'
   },
-  previousUrl: {
-    type: 'string',
+  previous: {
+    type: 'object',
     required: true,
-    description: 'The value of the previous link href attribute.'
+    description: 'The value of the previous link href attribute.',
+    params: {
+      href: {
+        type: 'string',
+        required: true,
+        description: 'The value of the previous link href attribute.'
+      },
+      text: {
+        type: 'string',
+        required: false,
+        description:
+          "Text to use within the previous link, defaults to 'Previous page', with 'page' being visually hidden. If `html` is provided, the `text` argument will be ignored."
+      },
+      html: {
+        type: 'string',
+        required: false,
+        description:
+          'If `text` is set, this is not required. HTML to use within the previous link. If `html` is provided, the `text` argument will be ignored.'
+      },
+      labelText: {
+        type: 'string',
+        required: false,
+        description:
+          'The name of the previous page to include within the previous link text'
+      },
+      attributes: {
+        type: 'object',
+        required: false,
+        description:
+          'HTML attributes (for example data attributes) to add to the previous link.'
+      }
+    }
   },
-  previousPage: {
-    type: 'string',
+  next: {
+    type: 'object',
     required: true,
-    description: 'The text of the previous link.'
+    description: 'The value of the next link href attribute.',
+    params: {
+      href: {
+        type: 'string',
+        required: true,
+        description: 'The value of the next link href attribute.'
+      },
+      text: {
+        type: 'string',
+        required: false,
+        description:
+          "Text to use within the next link, defaults to 'Next page', with 'page' being visually hidden. If `html` is provided, the `text` argument will be ignored."
+      },
+      html: {
+        type: 'string',
+        required: false,
+        description:
+          'If `text` is set, this is not required. HTML to use within the next link. If `html` is provided, the `text` argument will be ignored.'
+      },
+      labelText: {
+        type: 'string',
+        required: false,
+        description:
+          'The name of the next page to include within the previous link text'
+      },
+      attributes: {
+        type: 'object',
+        required: false,
+        description:
+          'HTML attributes (for example data attributes) to add to the next link.'
+      }
+    }
   },
-  nextUrl: {
-    type: 'string',
+  items: {
+    type: 'array',
     required: true,
-    description: 'The value of the next link href attribute.'
-  },
-  nextPage: {
-    type: 'string',
-    required: true,
-    description: 'The text of the next link.'
+    description: 'Array of items within the pagination component.',
+    params: {
+      number: {
+        type: 'string',
+        required: false,
+        description: 'The pagination item text – usually a page number.'
+      },
+      href: {
+        type: 'string',
+        required: false,
+        description:
+          'The value of the item link `href`. Required unless the item is an ellipsis.'
+      },
+      visuallyHiddenText: {
+        type: 'string',
+        required: false,
+        description:
+          "The visually hidden label for the pagination item, which will be set as an `aria-label` and announced by screen readers on the pagination item link. Should include page number. Defaults to, for example 'Page 1'."
+      },
+      current: {
+        type: 'boolean',
+        required: false,
+        description:
+          'Set to `true` to indicate the current page the user is on.'
+      },
+      ellipsis: {
+        type: 'boolean',
+        required: false,
+        description:
+          'Set to `true` to specify an ellipsis, indicated skipped pages in a numbered sequence.'
+      },
+      classes: {
+        type: 'string',
+        required: false,
+        description: 'Classes to add to the list item.'
+      },
+      attributes: {
+        type: 'object',
+        required: false,
+        description:
+          'The HTML attributes (for example, data attributes) to add to the item link.'
+      }
+    }
   },
   classes: {
     type: 'string',
