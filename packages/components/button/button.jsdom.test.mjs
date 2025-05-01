4739ed2d8937ca320f040b562b168471fc0a51cf
Colin Rotherham <work@colinr.com>
2025-05-01 12:13:37 +0100

    Add tests for all components

--- packages/components/button/button.jsdom.test.mjs
+++ packages/components/button/button.jsdom.test.mjs
@@ -1,11 +1,59 @@
+import { getByRole } from '@testing-library/dom'
+
 import initButton from './button.js'
 
-describe('mis-instantiation', () => {
-  it('does not prevent further JavaScript from running', () => {
-    expect(() => {
-      // `undefined` simulates the element being missing,
-      // from an unchecked `document.querySelector` for example
-      initButton(undefined)
-    }).not.toThrow()
+describe('Button', () => {
+  /** @type {HTMLButtonElement} */
+  let button
+
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <button class="nhsuk-button" data-module="nhsuk-button" type="submit">
+        Save and continue
+      </button>
+    `
+
+    button = getByRole(document.body, 'button')
+
+    jest.spyOn(button, 'addEventListener')
+  })
+
+  describe('Exports', () => {
+    it('should export init function', () => {
+      expect(initButton).toBeInstanceOf(Function)
+    })
+  })
+
+  describe('Initialisation', () => {
+    it('should add event listeners', () => {
+      initButton()
+
+      expect(button.addEventListener).toHaveBeenNthCalledWith(
+        1,
+        'keydown',
+        expect.any(Function)
+      )
+
+      expect(button.addEventListener).toHaveBeenNthCalledWith(
+        2,
+        'click',
+        expect.any(Function)
+      )
+    })
+
+    it('should not throw with missing button', () => {
+      button.remove()
+      expect(() => initButton()).not.toThrow()
+    })
+
+    it('should not throw with empty body', () => {
+      document.body.innerHTML = ''
+      expect(() => initButton()).not.toThrow()
+    })
+
+    it('should not throw with empty scope', () => {
+      const scope = document.createElement('div')
+      expect(() => initButton({ scope })).not.toThrow()
+    })
   })
 })
