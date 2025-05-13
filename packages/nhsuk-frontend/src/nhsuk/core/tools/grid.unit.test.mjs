cd612364d449e0b398f9d6e5795b9f44764e353d
Colin Rotherham <work@colinr.com>
2025-05-13 10:00:14 +0100

    Apply grid classes from tablet (not desktop)

--- packages/nhsuk-frontend/src/nhsuk/core/tools/grid.unit.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/core/tools/grid.unit.test.mjs
@@ -115,7 +115,7 @@ describe('Grid system', () => {
           width: 100%;
           padding: 0 16px;
         }
-        @media (min-width: 48.0625em) {
+        @media (min-width: 40.0625em) {
           .nhsuk-grid-column-full {
             width: 100%;
             float: left;
@@ -143,7 +143,7 @@ describe('Grid system', () => {
           width: 100%;
           padding: 0 16px;
         }
-        @media (min-width: 48.0625em) {
+        @media (min-width: 40.0625em) {
           .nhsuk-grid-column-two-thirds {
             width: 66.6666666667%;
             float: left;
@@ -168,7 +168,6 @@ describe('Grid system', () => {
       expect(results.css).toBe(outdent`
         .nhsuk-grid-column-one-quarter-at-desktop {
           box-sizing: border-box;
-          width: 100%;
           padding: 0 16px;
         }
         @media (min-width: 48.0625em) {
@@ -227,7 +226,7 @@ describe('Grid system', () => {
           width: 100%;
           padding: 0 16px;
         }
-        @media (min-width: 48.0625em) {
+        @media (min-width: 40.0625em) {
           .nhsuk-grid-column-one-half-right {
             width: 50%;
             float: right;
@@ -253,7 +252,7 @@ describe('Grid system', () => {
           width: 100%;
           padding: 0 16px;
         }
-        @media (min-width: 48.0625em) {
+        @media (min-width: 40.0625em) {
           .nhsuk-grid-column-full {
             width: 100%;
             float: left;
@@ -279,7 +278,7 @@ describe('Grid system', () => {
           width: 100%;
           padding: 0 16px;
         }
-        @media (min-width: 48.0625em) {
+        @media (min-width: 40.0625em) {
           .large-column-three-quarters {
             width: 75%;
             float: left;
