201724725044ff87d45a42a5e09f675940a6b017
Colin Rotherham <work@colinr.com>
2025-05-23 07:48:25 +0100

    Use class pattern for skip link

--- packages/components/skip-link/skip-link.js
+++ packages/components/skip-link/skip-link.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-extraneous-class */
+
 const { setFocus } = require('../../common')
 
 /*
@@ -6,40 +8,46 @@ const { setFocus } = require('../../common')
  * When using VoiceOver on iOS, focus remains on the skip link anchor
  * when elected so the next focusable element is not at the jumped to area.
  */
+class SkipLink {
+  constructor($module) {
+    if (!$module || !($module instanceof HTMLAnchorElement)) {
+      return this
+    }
+
+    this.$module = $module
+
+    const linkedElementId = this.$module.hash.split('#').pop()
+    const $linkedElement = linkedElementId
+      ? document.getElementById(linkedElementId)
+      : null
+
+    // Check for linked element
+    if (!$linkedElement) {
+      return
+    }
+
+    /**
+     * Focus the linked element on click
+     *
+     * Adds a helper CSS class to hide native focus styles,
+     * but removes it on blur to restore native focus styles
+     */
+    this.$module.addEventListener('click', () =>
+      setFocus($linkedElement, {
+        onBeforeFocus() {
+          $linkedElement.classList.add('nhsuk-skip-link-focused-element')
+        },
+        onBlur() {
+          $linkedElement.classList.remove('nhsuk-skip-link-focused-element')
+        }
+      })
+    )
+  }
+}
 
 module.exports = (options = {}) => {
   const $scope = options.scope || document
   const $module = $scope.querySelector('.nhsuk-skip-link')
 
-  // Check for skip link
-  if (!$module || !($module instanceof HTMLAnchorElement)) {
-    return
-  }
-
-  const linkedElementId = $module.hash.split('#').pop()
-  const $linkedElement = linkedElementId
-    ? document.getElementById(linkedElementId)
-    : null
-
-  // Check for linked element
-  if (!$linkedElement) {
-    return
-  }
-
-  /**
-   * Focus the linked element on click
-   *
-   * Adds a helper CSS class to hide native focus styles,
-   * but removes it on blur to restore native focus styles
-   */
-  $module.addEventListener('click', () =>
-    setFocus($linkedElement, {
-      onBeforeFocus() {
-        $linkedElement.classList.add('nhsuk-skip-link-focused-element')
-      },
-      onBlur() {
-        $linkedElement.classList.remove('nhsuk-skip-link-focused-element')
-      }
-    })
-  )
+  new SkipLink($module)
 }
