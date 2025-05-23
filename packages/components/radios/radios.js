8e6eb84b486e5a451f87a58cbe2dac5eae2cd0dd
Colin Rotherham <work@colinr.com>
2025-05-23 07:48:18 +0100

    Use class pattern for radios

--- packages/components/radios/radios.js
+++ packages/components/radios/radios.js
@@ -1,42 +1,65 @@
 const { toggleConditionalInput } = require('../../common')
 
 /**
+ * Radios component
+ *
  * Conditionally show content when a radio button is checked
  * Test at http://localhost:3000/nhsuk-frontend/components/radios/conditional.html
  */
+class Radios {
+  constructor($module) {
+    if (!$module) {
+      return this
+    }
 
-module.exports = ({ scope = document } = {}) => {
-  // Radio input HTMLElements inside a conditional form group
-  const radioInputs = scope.querySelectorAll(
-    '.nhsuk-radios--conditional .nhsuk-radios__input'
-  )
+    this.$module = $module
+
+    const $inputs = this.$module.querySelectorAll('.nhsuk-radios__input')
+    if (!$inputs.length) {
+      return this
+    }
+
+    this.$inputs = $inputs
+
+    // When the page is restored after navigating 'back' in some browsers the
+    // state of form controls is not restored until *after* the DOMContentLoaded
+    // event is fired, so we need to sync after the pageshow event in browsers
+    // that support it.
+    if ('onpageshow' in window) {
+      window.addEventListener('pageshow', () =>
+        this.syncAllConditionalReveals()
+      )
+    } else {
+      window.addEventListener('DOMContentLoaded', () =>
+        this.syncAllConditionalReveals()
+      )
+    }
+
+    // Although we've set up handlers to sync state on the pageshow or
+    // DOMContentLoaded event, init could be called after those events have fired,
+    // for example if they are added to the page dynamically, so sync now too.
+
+    // Attach event handler to radioInputs
+    this.$inputs.forEach((radioButton) => {
+      radioButton.addEventListener('change', () =>
+        this.syncAllConditionalReveals()
+      )
+    })
+  }
 
   /**
    * Update all conditional reveals to match checked state
    */
-  const syncAllConditionalReveals = () => {
-    radioInputs.forEach((input) =>
+  syncAllConditionalReveals() {
+    this.$inputs.forEach((input) =>
       toggleConditionalInput(input, 'nhsuk-radios__conditional--hidden')
     )
   }
+}
 
-  // When the page is restored after navigating 'back' in some browsers the
-  // state of form controls is not restored until *after* the DOMContentLoaded
-  // event is fired, so we need to sync after the pageshow event in browsers
-  // that support it.
-  if ('onpageshow' in window) {
-    window.addEventListener('pageshow', syncAllConditionalReveals)
-  } else {
-    window.addEventListener('DOMContentLoaded', syncAllConditionalReveals)
-  }
+module.exports = (options = {}) => {
+  const $scope = options.scope || document
+  const $module = $scope.querySelector('.nhsuk-radios--conditional')
 
-  // Although we've set up handlers to sync state on the pageshow or
-  // DOMContentLoaded event, init could be called after those events have fired,
-  // for example if they are added to the page dynamically, so sync now too.
-  syncAllConditionalReveals()
-
-  // Attach event handler to radioInputs
-  radioInputs.forEach((radioButton) => {
-    radioButton.addEventListener('change', syncAllConditionalReveals)
-  })
+  new Radios($module)
 }
