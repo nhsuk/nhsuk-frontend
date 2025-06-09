32dd783c70de53f33b7ff1857375fa4f1bba4304
Frankie Roberto <frankie@frankieroberto.com>
2025-06-09 20:26:31 +0100

    Add numbered page pagination variant

--- backstop.config.js
+++ backstop.config.js
@@ -1051,6 +1051,15 @@ module.exports = {
       ]
     },
     {
+      label: 'Pagination numbered',
+      url: `${BASE_URL}/components/pagination/numbered.html`,
+      viewports: [
+        viewports.get('mobile'),
+        viewports.get('tablet'),
+        viewports.get('desktop')
+      ]
+    },
+    {
       label: 'Panel',
       url: `${BASE_URL}/components/panel/index.html`,
       viewports: [
