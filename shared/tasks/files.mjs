b6fdcb5fd4aa0a4cc54db72b2d96b67d9af3be3b
Colin Rotherham <work@colinr.com>
2025-06-10 12:47:52 +0100

    Add file copy task

---
+++ shared/tasks/files.mjs
@@ -0,0 +1,45 @@
+import { join, parse } from 'path'
+
+import gulp from 'gulp'
+import rename from 'gulp-rename'
+
+/**
+ * Copy asset
+ *
+ * @param {string} assetPath
+ * @param {AssetOptions} entry
+ */
+export function copy(assetPath, { srcPath, destPath, output = {} }) {
+  let stream = gulp.src(join(srcPath, assetPath), {
+    sourcemaps: true
+  })
+
+  // Rename file (optional)
+  if (output.file) {
+    const { name, ext, dir } = parse(output.file)
+
+    stream = stream.pipe(
+      rename({
+        dirname: dir,
+        basename: name,
+        extname: ext
+      })
+    )
+  }
+
+  return stream.pipe(
+    gulp.dest(destPath, {
+      encoding: false,
+      sourcemaps: '.'
+    })
+  )
+}
+
+/**
+ * Asset options
+ *
+ * @typedef {object} AssetOptions
+ * @property {string} srcPath - Source directory
+ * @property {string} destPath - Destination directory
+ * @property {{ file?: string }} [output] - Output options
+ */
--- shared/tasks/index.mjs
+++ shared/tasks/index.mjs
@@ -1,6 +1,7 @@
 /**
  * Shared tasks
  */
+export * as files from './files.mjs'
 export * as scripts from './scripts.mjs'
 export * as styles from './styles.mjs'
 export * as task from './task.mjs'
