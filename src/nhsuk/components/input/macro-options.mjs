2c37cd654e83a9866e7149fe9d55eb0e691a4885
Colin Rotherham <work@colinr.com>
2025-06-16 18:09:49 +0100

    Use shorter library method names

--- src/nhsuk/components/input/macro-options.mjs
+++ src/nhsuk/components/input/macro-options.mjs
@@ -1,4 +1,4 @@
-import { getComponentMacroOptions } from '@nhsuk/frontend-lib/components.mjs'
+import { components } from '@nhsuk/frontend-lib'
 
 export const name = 'Text input'
 
@@ -113,7 +113,7 @@ export const params = {
   }
 }
 
-export const options = getComponentMacroOptions(params)
+export const options = components.getMacroOptions(params)
 
 /**
  * @import { MacroParam } from '@nhsuk/frontend-lib/components.mjs'
