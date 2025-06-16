cf5588d8b15472787dc5ccaadb07ff7a84981509
Colin Rotherham <work@colinr.com>
2025-06-16 08:20:23 +0100

    Migrate macro options to ES modules

---
+++ src/nhsuk/components/panel/macro-options.mjs
@@ -0,0 +1,64 @@
+import { getComponentMacroOptions } from '@nhsuk/frontend-lib/components.mjs'
+
+export const name = 'Panel'
+
+/**
+ * Nunjucks macro option params
+ *
+ * @satisfies {{ [param: string]: MacroParam }}
+ */
+export const params = {
+  titleText: {
+    type: 'string',
+    required: true,
+    description:
+      'If `titleHtml` is set, this is not required. Text to use within the panel. If `titleHtml` is provided, the `titleText` option will be ignored.'
+  },
+  titleHtml: {
+    type: 'string',
+    required: false,
+    description:
+      'If `titleText` is set, this is not required. HTML to use within the panel. If `titleHtml` is provided, the `titleText` option will be ignored.'
+  },
+  headingLevel: {
+    type: 'integer',
+    required: false,
+    description: 'Heading level, from `1` to `6`. Defaults to `1`.'
+  },
+  text: {
+    type: 'string',
+    required: false,
+    description:
+      'If `html` is set, this is not required. Text to use within the panel content. If `html` is provided, the `text` option will be ignored.'
+  },
+  html: {
+    type: 'string',
+    required: false,
+    description:
+      'If `text` is set, this is not required. Text to use within the panel content. If `text` is provided, the `html` option will be ignored.'
+  },
+  caller: {
+    type: 'nunjucks-block',
+    required: false,
+    description:
+      'Not strictly a parameter but a Nunjucks code convention. Using a `call` block enables you to call a macro with all the text inside the tag. This is helpful if you want to pass a lot of content into a macro. To use it, you will need to wrap the entire panel component in a `call` block.'
+  },
+  classes: {
+    type: 'string',
+    required: false,
+    description:
+      'Optional additional classes to add to the hint `div` tag. Separate each class with a space.'
+  },
+  attributes: {
+    type: 'object',
+    required: false,
+    description:
+      'Any extra HTML attributes (for example data attributes) to add to the input component.'
+  }
+}
+
+export const options = getComponentMacroOptions(params)
+
+/**
+ * @import { MacroParam } from '@nhsuk/frontend-lib/components.mjs'
+ */
---
+++ src/nhsuk/components/tables/macro-options.mjs
@@ -0,0 +1,136 @@
+import { getComponentMacroOptions } from '@nhsuk/frontend-lib/components.mjs'
+
+export const name = 'Tables'
+
+/**
+ * Nunjucks macro option params
+ *
+ * @satisfies {{ [param: string]: MacroParam }}
+ */
+export const params = {
+  rows: {
+    type: 'array',
+    required: true,
+    description: 'Array of table rows and cells.',
+    params: {
+      text: {
+        type: 'string',
+        required: true,
+        description:
+          'If `html` is set, this is not required. Text for cells in table rows. If `html` is provided, the `text` argument will be ignored.'
+      },
+      html: {
+        type: 'string',
+        required: true,
+        description:
+          'If `text` is set, this is not required. HTML for cells in table rows. If `html` is provided, the `text` argument will be ignored.'
+      },
+      header: {
+        type: 'string',
+        required: false,
+        description: 'Header text for cells in responsive table rows only.'
+      },
+      format: {
+        type: 'string',
+        required: false,
+        description:
+          "Specify format of a cell. Currently we only use `'numeric'`."
+      },
+      colspan: {
+        type: 'integer',
+        required: false,
+        description: 'Specify how many columns a cell extends.'
+      },
+      rowspan: {
+        type: 'integer',
+        required: false,
+        description: 'Specify how many rows a cell extends.'
+      }
+    }
+  },
+  head: {
+    type: 'array',
+    required: false,
+    description: 'Array of table head cells.',
+    params: {
+      text: {
+        type: 'string',
+        required: false,
+        description:
+          'If `html` is set, this is not required. Text for table head cells. If `html` is provided, the `text` argument will be ignored.'
+      },
+      html: {
+        type: 'string',
+        required: false,
+        description:
+          'If `text` is set, this is not required. HTML for table head cells. If `html` is provided, the `text` argument will be ignored.'
+      },
+      format: {
+        type: 'string',
+        required: false,
+        description:
+          "Specify format of a cell. Currently we only use `'numeric'`."
+      },
+      colspan: {
+        type: 'integer',
+        required: false,
+        description: 'Specify how many columns a cell extends.'
+      },
+      rowspan: {
+        type: 'integer',
+        required: false,
+        description: 'Specify how many rows a cell extends.'
+      }
+    }
+  },
+  heading: {
+    type: 'string',
+    required: false,
+    description:
+      'Heading/label of the panel if the panel argument is set to true.'
+  },
+  headingLevel: {
+    type: 'integer',
+    required: false,
+    description: 'Optional heading level for the heading. Defaults to `3`.'
+  },
+  caption: {
+    type: 'string',
+    required: false,
+    description: 'Caption text.'
+  },
+  captionClasses: {
+    type: 'string',
+    required: false,
+    description:
+      "Classes for caption text size. Classes to add to the table caption, for example `'nhsuk-table__caption--l'`."
+  },
+  firstCellIsHeader: {
+    type: 'boolean',
+    required: false,
+    description:
+      'If set to true, first cell in table row will be a TH instead of a TD.'
+  },
+  responsive: {
+    type: 'boolean',
+    required: false,
+    description: 'If set to true, responsive table classes will be applied.'
+  },
+  tableClasses: {
+    type: 'string',
+    required: false,
+    description: 'Classes to add to the table container.'
+  },
+  attributes: {
+    type: 'object',
+    required: false,
+    description:
+      'HTML attributes (for example data attributes) to add to the table container.'
+  }
+}
+
+export const options = getComponentMacroOptions(params)
+
+/**
+ * @import { MacroParam } from '@nhsuk/frontend-lib/components.mjs'
+ */
