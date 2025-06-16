ec155513a4826311f2fc5257431a5de7dc00919e
Colin Rotherham <work@colinr.com>
2025-06-16 08:23:49 +0100

    Add directory listing helpers

---
+++ shared/lib/files.mjs
@@ -0,0 +1,40 @@
+import { basename, join } from 'path'
+
+import { glob } from 'glob'
+
+/**
+ * Directory listing for path
+ *
+ * @param {string} directoryPath - Minimatch pattern to directory
+ * @param {GlobOptionsWithFileTypesUnset} [options] - Glob options
+ * @returns File paths
+ */
+export async function getListing(directoryPath, options = {}) {
+  const listing = await glob(directoryPath, {
+    nodir: true,
+    ...options
+  })
+
+  return listing.sort()
+}
+
+/**
+ * Directory listing (directories only)
+ *
+ * @param {string} directoryPath - Minimatch pattern to directory
+ * @param {GlobOptionsWithFileTypesUnset} [options] - Glob options
+ * @returns Directory names
+ */
+export async function getDirectories(directoryPath, options = {}) {
+  const listing = await getListing(join(directoryPath, '*/'), {
+    nodir: false,
+    ...options
+  })
+
+  // Use directory names only
+  return listing.map((directoryPath) => basename(directoryPath)).sort()
+}
+
+/**
+ * @import { GlobOptionsWithFileTypesUnset } from 'glob'
+ */
--- shared/lib/index.mjs
+++ shared/lib/index.mjs
@@ -2,3 +2,4 @@
  * Shared library
  */
 export * as components from './components.mjs'
+export * as files from './files.mjs'
