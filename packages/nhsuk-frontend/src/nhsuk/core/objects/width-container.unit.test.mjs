4c2a8338cd2f539d144bff0db0b6f0bed528f2e1
Colin Rotherham <work@colinr.com>
2025-06-18 07:53:15 +0100

    Allow creating custom width containers

---
+++ packages/nhsuk-frontend/src/nhsuk/core/objects/width-container.unit.test.mjs
@@ -0,0 +1,46 @@
+import { outdent } from 'outdent'
+import { compileStringAsync } from 'sass-embedded'
+
+describe('Page width container', () => {
+  const sassBootstrap = `
+    @use "core/objects/width-container" as *;
+  `
+
+  describe('@mixin nhsuk-width-container', () => {
+    it('allows different widths to be specified using $width', async () => {
+      const sass = `
+        ${sassBootstrap}
+
+        .app-width-container--wide {
+          @include nhsuk-width-container(1200px);
+        }
+      `
+
+      const results = compileStringAsync(sass, {
+        loadPaths: ['packages/nhsuk-frontend/src/nhsuk']
+      })
+
+      await expect(results).resolves.toMatchObject({
+        css: expect.stringContaining(outdent`
+          .app-width-container--wide {
+            max-width: 1200px;
+            margin-right: 16px;
+            margin-left: 16px;
+          }
+          @media (min-width: 48.0625em) {
+            .app-width-container--wide {
+              margin-right: 32px;
+              margin-left: 32px;
+            }
+          }
+          @media (min-width: 1264px) {
+            .app-width-container--wide {
+              margin-right: auto;
+              margin-left: auto;
+            }
+          }
+        `)
+      })
+    })
+  })
+})
