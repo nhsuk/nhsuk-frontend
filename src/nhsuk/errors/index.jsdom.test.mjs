9d72e3a7f375c7e830fc3d1f903e5bc1ad5b6e3c
Colin Rotherham <work@colinr.com>
2025-07-03 16:34:24 +0100

    Add support for `ElementError` from GOV.UK Frontend

--- src/nhsuk/errors/index.jsdom.test.mjs
+++ src/nhsuk/errors/index.jsdom.test.mjs
@@ -1,4 +1,6 @@
-import { NHSUKFrontendError } from './index.mjs'
+import { SkipLink } from '../index.mjs'
+
+import { ElementError, NHSUKFrontendError } from './index.mjs'
 
 describe('Errors', () => {
   describe('NHSUKFrontendError', () => {
@@ -8,6 +10,54 @@ describe('Errors', () => {
       }
 
       expect(new CustomError().name).toBe('CustomName')
+    })
+  })
+
+  describe('ElementError', () => {
+    it('is an instance of NHSUKFrontendError', () => {
+      const error = new ElementError({
+        component: SkipLink,
+        identifier: 'variableName'
+      })
+
+      expect(error).toBeInstanceOf(NHSUKFrontendError)
+    })
+
+    it('has its own name set', () => {
+      const error = new ElementError({
+        component: SkipLink,
+        identifier: 'variableName'
+      })
+
+      expect(error).toHaveProperty('name', 'ElementError')
+    })
+
+    it('formats the message when the element is not found', () => {
+      const error = new ElementError({
+        component: SkipLink,
+        identifier: 'variableName'
+      })
+
+      expect(error).toHaveProperty(
+        'message',
+        'SkipLink: variableName not found'
+      )
+    })
+
+    it('formats the message when the element is not the right type', () => {
+      const $element = document.createElement('div')
+
+      const error = new ElementError({
+        element: $element,
+        component: SkipLink,
+        identifier: 'variableName',
+        expectedType: 'HTMLAnchorElement'
+      })
+
+      expect(error).toHaveProperty(
+        'message',
+        'SkipLink: variableName is not of type HTMLAnchorElement'
+      )
     })
   })
 })
