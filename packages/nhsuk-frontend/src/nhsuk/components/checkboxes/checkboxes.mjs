5ab6057f99ff17ef681369fe43e35e1ac24bb2ae
Colin Rotherham <work@colinr.com>
2025-07-07 00:24:16 +0100

    Skip unnecessary `pageshow` feature detection for IE8–10

--- packages/nhsuk-frontend/src/nhsuk/components/checkboxes/checkboxes.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/checkboxes/checkboxes.mjs
@@ -28,17 +28,8 @@ export class Checkboxes extends Component {
 
     // When the page is restored after navigating 'back' in some browsers the
     // state of form controls is not restored until *after* the DOMContentLoaded
-    // event is fired, so we need to sync after the pageshow event in browsers
-    // that support it.
-    if ('onpageshow' in window) {
-      window.addEventListener('pageshow', () =>
-        this.syncAllConditionalReveals()
-      )
-    } else {
-      window.addEventListener('DOMContentLoaded', () =>
-        this.syncAllConditionalReveals()
-      )
-    }
+    // event is fired, so we need to sync after the pageshow event.
+    window.addEventListener('pageshow', () => this.syncAllConditionalReveals())
 
     // Although we've set up handlers to sync state on the pageshow or
     // DOMContentLoaded event, init could be called after those events have fired,
