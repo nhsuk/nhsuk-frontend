d7ce962a4246595823f066be5dee985423b9206b
Colin Rotherham <work@colinr.com>
2025-07-10 18:24:59 +0100

    Ensure version numbers use `NODE_ENV` in development
    For builds it’s important to identify `version: 'development'` from release builds with version numbers

--- shared/config/version.mjs
+++ shared/config/version.mjs
@@ -3,7 +3,13 @@ import pkg from 'nhsuk-frontend/package.json' with { type: 'json' }
 // Node.js environment with default
 // https://nodejs.org/en/learn/getting-started/nodejs-the-difference-between-development-and-production
 const { NODE_ENV = 'development' } = process.env
+
 /**
  * NHS.UK frontend release version
+ *
+ * The version export identifies development builds using NODE_ENV by default
+ * unlike test and production builds which use the release pkg.version number
  */
-export const version = pkg.version
+export const version = ['test', 'production'].includes(NODE_ENV)
+  ? pkg.version // Use release version
+  : NODE_ENV // or default to build type
