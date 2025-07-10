235eb8063f228bb35a921726cf845871c4793b00
Colin Rotherham <work@colinr.com>
2025-07-10 18:24:06 +0100

    Set default `NODE_ENV` values

--- shared/config/version.mjs
+++ shared/config/version.mjs
@@ -1,5 +1,8 @@
 import pkg from 'nhsuk-frontend/package.json' with { type: 'json' }
 
+// Node.js environment with default
+// https://nodejs.org/en/learn/getting-started/nodejs-the-difference-between-development-and-production
+const { NODE_ENV = 'development' } = process.env
 /**
  * NHS.UK frontend release version
  */
