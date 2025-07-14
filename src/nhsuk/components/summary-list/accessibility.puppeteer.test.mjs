4f0591a5bffed8502a306b742de0512e4acdf125
Colin Rotherham <work@colinr.com>
2025-07-14 10:52:20 +0100

    Add Axe accessibility tests via Puppeteer
    Copies https://github.com/alphagov/govuk-frontend/pull/3522 from GOV.UK Frontend

---
+++ src/nhsuk/components/summary-list/accessibility.puppeteer.test.mjs
@@ -0,0 +1,21 @@
+import { axe, goToComponent } from '@nhsuk/frontend-helpers/puppeteer.mjs'
+
+import { examples } from './macro-options.mjs'
+
+describe('Summary list', () => {
+  /** @type {Page} */
+  let page
+
+  describe('Component examples', () => {
+    it('passes accessibility tests', async () => {
+      for (const example in examples) {
+        page = await goToComponent(browser, 'summary-list', { example })
+        await expect(axe(page)).resolves.toHaveNoViolations()
+      }
+    }, 120000)
+  })
+})
+
+/**
+ * @import { Page } from 'puppeteer'
+ */
