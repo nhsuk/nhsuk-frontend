f9affc3f92d3049dea4a221881d0e2f2a4953f1a
Ed Horsford <mail@edwardhorsford.com>
2025-07-15 12:54:37 +0100

    Add test from govuk - still needs fixing

---
+++ src/nhsuk/components/notification-banner/notification-banner.puppeteer.test.js
@@ -0,0 +1,288 @@
+/* eslint-disable no-new */
+
+const { render } = require('@nhsuk-frontend/helpers/puppeteer')
+const { getExamples } = require('@nhsuk-frontend/lib/components')
+
+describe('Notification banner', () => {
+  let examples
+
+  beforeAll(async () => {
+    examples = await getExamples('notification-banner')
+  })
+
+  describe('when type is set to "success"', () => {
+    it('has the correct tabindex attribute to be focused with JavaScript', async () => {
+      await render(
+        page,
+        'notification-banner',
+        examples['with type as success']
+      )
+
+      const tabindex = await page.$eval('.nhsuk-notification-banner', (el) =>
+        el.getAttribute('tabindex')
+      )
+
+      expect(tabindex).toBe('-1')
+    })
+
+    it('is automatically focused when the page loads', async () => {
+      await render(
+        page,
+        'notification-banner',
+        examples['with type as success']
+      )
+
+      const activeElement = await page.evaluate(() =>
+        document.activeElement.getAttribute('data-module')
+      )
+
+      expect(activeElement).toBe('nhsuk-notification-banner')
+    })
+
+    it('removes the tabindex attribute on blur', async () => {
+      await render(
+        page,
+        'notification-banner',
+        examples['with type as success']
+      )
+
+      await page.$eval(
+        '.nhsuk-notification-banner',
+        (el) => el instanceof window.HTMLElement && el.blur()
+      )
+
+      const tabindex = await page.$eval('.nhsuk-notification-banner', (el) =>
+        el.getAttribute('tabindex')
+      )
+      expect(tabindex).toBeNull()
+    })
+
+    describe('and auto-focus is disabled using data attributes', () => {
+      beforeAll(async () => {
+        await render(
+          page,
+          'notification-banner',
+          examples['auto-focus disabled, with type as success']
+        )
+      })
+
+      it('does not have a tabindex attribute', async () => {
+        const tabindex = await page.$eval('.nhsuk-notification-banner', (el) =>
+          el.getAttribute('tabindex')
+        )
+
+        expect(tabindex).toBeNull()
+      })
+
+      it('does not focus the notification banner', async () => {
+        const activeElement = await page.evaluate(() =>
+          document.activeElement.getAttribute('data-module')
+        )
+
+        expect(activeElement).not.toBe('nhsuk-notification-banner')
+      })
+    })
+
+    describe('and auto-focus is disabled using JavaScript configuration', () => {
+      beforeAll(async () => {
+        await render(
+          page,
+          'notification-banner',
+          examples['with type as success'],
+          {
+            config: {
+              disableAutoFocus: true
+            }
+          }
+        )
+      })
+
+      it('does not have a tabindex attribute', async () => {
+        const tabindex = await page.$eval('.nhsuk-notification-banner', (el) =>
+          el.getAttribute('tabindex')
+        )
+
+        expect(tabindex).toBeNull()
+      })
+
+      it('does not focus the notification banner', async () => {
+        const activeElement = await page.evaluate(() =>
+          document.activeElement.getAttribute('data-module')
+        )
+
+        expect(activeElement).not.toBe('nhsuk-notification-banner')
+      })
+    })
+
+    describe('and auto-focus is disabled using options passed to initAll', () => {
+      beforeAll(async () => {
+        await render(
+          page,
+          'notification-banner',
+          examples['with type as success'],
+          {
+            config: {
+              disableAutoFocus: true
+            }
+          }
+        )
+      })
+
+      it('does not have a tabindex attribute', async () => {
+        const tabindex = await page.$eval('.nhsuk-notification-banner', (el) =>
+          el.getAttribute('tabindex')
+        )
+
+        expect(tabindex).toBeNull()
+      })
+
+      it('does not focus the notification banner', async () => {
+        const activeElement = await page.evaluate(() =>
+          document.activeElement.getAttribute('data-module')
+        )
+
+        expect(activeElement).not.toBe('nhsuk-notification-banner')
+      })
+    })
+
+    describe('and autofocus is disabled in JS but enabled in data attributes', () => {
+      beforeAll(async () => {
+        await render(
+          page,
+          'notification-banner',
+          examples['auto-focus explicitly enabled, with type as success'],
+          {
+            config: {
+              disableAutoFocus: true
+            }
+          }
+        )
+      })
+
+      it('has the correct tabindex attribute to be focused with JavaScript', async () => {
+        const tabindex = await page.$eval('.nhsuk-notification-banner', (el) =>
+          el.getAttribute('tabindex')
+        )
+
+        expect(tabindex).toBe('-1')
+      })
+
+      it('is automatically focused when the page loads', async () => {
+        const activeElement = await page.evaluate(() =>
+          document.activeElement.getAttribute('data-module')
+        )
+
+        expect(activeElement).toBe('nhsuk-notification-banner')
+      })
+    })
+
+    describe('and role is overridden to "region"', () => {
+      beforeAll(async () => {
+        await render(
+          page,
+          'notification-banner',
+          examples['role=alert overridden to role=region, with type as success']
+        )
+      })
+
+      it('does not have a tabindex attribute', async () => {
+        const tabindex = await page.$eval('.nhsuk-notification-banner', (el) =>
+          el.getAttribute('tabindex')
+        )
+
+        expect(tabindex).toBeNull()
+      })
+
+      it('does not focus the notification banner', async () => {
+        const activeElement = await page.evaluate(() =>
+          document.activeElement.getAttribute('data-module')
+        )
+
+        expect(activeElement).not.toBe('nhsuk-notification-banner')
+      })
+    })
+
+    describe('and a custom tabindex is set', () => {
+      beforeAll(async () => {
+        await render(page, 'notification-banner', examples['custom tabindex'])
+      })
+
+      it('does not remove the tabindex attribute on blur', async () => {
+        await page.$eval(
+          '.nhsuk-notification-banner',
+          (el) => el instanceof window.HTMLElement && el.blur()
+        )
+
+        const tabindex = await page.$eval('.nhsuk-notification-banner', (el) =>
+          el.getAttribute('tabindex')
+        )
+        expect(tabindex).toBe('2')
+      })
+    })
+  })
+
+  describe('errors at instantiation', () => {
+    it('can throw a SupportError if appropriate', async () => {
+      await expect(
+        render(page, 'notification-banner', examples.default, {
+          beforeInitialisation() {
+            document.body.classList.remove('nhsuk-frontend-supported')
+          }
+        })
+      ).rejects.toMatchObject({
+        cause: {
+          name: 'SupportError',
+          message:
+            'GOV.UK Frontend initialised without `<body class="nhsuk-frontend-supported">` from template `<script>` snippet'
+        }
+      })
+    })
+
+    it('throws when initialised twice', async () => {
+      await expect(
+        render(page, 'notification-banner', examples.default, {
+          async afterInitialisation($root) {
+            const { NotificationBanner } = await import('nhsuk-frontend')
+            new NotificationBanner($root)
+          }
+        })
+      ).rejects.toMatchObject({
+        name: 'InitError',
+        message:
+          'nhsuk-notification-banner: Root element (`$root`) already initialised'
+      })
+    })
+
+    it('throws when $root is not set', async () => {
+      await expect(
+        render(page, 'notification-banner', examples.default, {
+          beforeInitialisation($root) {
+            $root.remove()
+          }
+        })
+      ).rejects.toMatchObject({
+        cause: {
+          name: 'ElementError',
+          message: 'nhsuk-notification-banner: Root element (`$root`) not found'
+        }
+      })
+    })
+
+    it('throws when receiving the wrong type for $root', async () => {
+      await expect(
+        render(page, 'notification-banner', examples.default, {
+          beforeInitialisation($root) {
+            // Replace with an `<svg>` element which is not an `HTMLElement` in the DOM (but an `SVGElement`)
+            $root.outerHTML = `<svg data-module="nhsuk-notification-banner"></svg>`
+          }
+        })
+      ).rejects.toMatchObject({
+        cause: {
+          name: 'ElementError',
+          message:
+            'nhsuk-notification-banner: Root element (`$root`) is not of type HTMLElement'
+        }
+      })
+    })
+  })
+})
