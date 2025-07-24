1719022f85ca46df3f5a0bf5171c28a43ffec257
Colin Rotherham <work@colinr.com>
2025-07-24 08:55:05 +0100

    Add configuration utilities from GOV.UK Frontend

--- packages/nhsuk-frontend/src/nhsuk/common/index.mjs
+++ packages/nhsuk-frontend/src/nhsuk/common/index.mjs
@@ -130,6 +130,17 @@ export function isSupported($scope = document.body) {
 }
 
 /**
+ * Check for an object
+ *
+ * @template {Partial<Record<keyof ObjectType, unknown>>} [ObjectType=ObjectNested]
+ * @param {unknown | ObjectType} option - Option to check
+ * @returns {option is ObjectType} Whether the option is an object
+ */
+export function isObject(option) {
+  return !!option && typeof option === 'object' && !Array.isArray(option)
+}
+
+/**
  * Format error message
  *
  * @param {ComponentConstructor} Component - Component that threw the error
@@ -144,4 +155,5 @@ export * from './nhsuk-frontend-version.mjs'
 
 /**
  * @import { ComponentConstructor } from '../component.mjs'
+ * @import { ObjectNested } from './configuration/normalise-dataset.mjs'
  */
