a9607e20c85194d882505269c0af5dd2cfb69baa
Colin Rotherham <work@colinr.com>
2025-08-14 15:08:57 +0100

    Ensure `nhsuk-tint` and `nhsuk-shade` output hexadecimal values
    This ensures the colours don't have float values for their R,G and B channels, which Safari <12 is not able to interpret correctly.

--- packages/nhsuk-frontend/src/nhsuk/core/helpers/colour.unit.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/core/helpers/colour.unit.test.mjs
@@ -83,4 +83,85 @@ describe('Colour helpers', () => {
       await expect(results).rejects.toThrow('Unknown colour `hooloovoo`')
     })
   })
+
+  describe('@function nhsuk-colour-compatible', () => {
+    it.each([
+      {
+        input: 'rgb(0, 127, 59)',
+        output: '#007f3b'
+      },
+      {
+        input: 'rgb(0.1, 127.2, 59.3)',
+        output: '#007f3b'
+      }
+    ])("outputs '$input' as value '$output'", async ({ input, output }) => {
+      const sass = outdent`
+        ${sassModules}
+
+        .foo {
+          color: nhsuk-colour-compatible(${input});
+        }
+      `
+
+      const results = compileStringAsync(sass, {
+        loadPaths: ['packages/nhsuk-frontend/src/nhsuk']
+      })
+
+      await expect(results).resolves.toMatchObject({
+        css: outdent`
+          .foo {
+            color: ${output};
+          }
+        `
+      })
+    })
+  })
+
+  describe('@function nhsuk-shade', () => {
+    it('outputs hexadecimal values', async () => {
+      const sass = outdent`
+        ${sassModules}
+
+        .foo {
+          color: nhsuk-shade(rgb(171, 205, 239), 17%);
+        }
+      `
+
+      const results = compileStringAsync(sass, {
+        loadPaths: ['packages/nhsuk-frontend/src/nhsuk']
+      })
+
+      await expect(results).resolves.toMatchObject({
+        css: outdent`
+          .foo {
+            color: #8eaac6;
+          }
+        `
+      })
+    })
+  })
+
+  describe('@function nhsuk-tint', () => {
+    it('outputs hexadecimal values', async () => {
+      const sass = outdent`
+        ${sassModules}
+
+        .foo {
+          color: nhsuk-tint(rgb(18, 52, 86), 17%);
+        }
+      `
+
+      const results = compileStringAsync(sass, {
+        loadPaths: ['packages/nhsuk-frontend/src/nhsuk']
+      })
+
+      await expect(results).resolves.toMatchObject({
+        css: outdent`
+          .foo {
+            color: #3a5773;
+          }
+        `
+      })
+    })
+  })
 })
