476e483eda5a1067f2e36974e7047b1a382f854c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-09-02 00:06:51 +0100

    Add file upload component

---
+++ packages/nhsuk-frontend/src/nhsuk/components/file-upload/accessibility.puppeteer.test.mjs
@@ -0,0 +1,21 @@
+import { axe, goToComponent } from '@nhsuk/frontend-helpers/puppeteer.mjs'
+
+import { examples } from './macro-options.mjs'
+
+describe('File upload', () => {
+  /** @type {Page} */
+  let page
+
+  describe('Component examples', () => {
+    it('passes accessibility tests', async () => {
+      for (const example in examples) {
+        page = await goToComponent(browser, 'file-upload', { example })
+        await expect(axe(page)).resolves.toHaveNoViolations()
+      }
+    }, 120000)
+  })
+})
+
+/**
+ * @import { Page } from 'puppeteer'
+ */
