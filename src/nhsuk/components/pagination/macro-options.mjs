f1d09ba8eeb6dcc3f2a5bfb30475b0596f7a2660
Frankie Roberto <frankie@frankieroberto.com>
2025-09-05 22:34:45 +0100

    Update to make numbered pagination backwards-compatible

--- src/nhsuk/components/pagination/macro-options.mjs
+++ src/nhsuk/components/pagination/macro-options.mjs
@@ -13,124 +13,25 @@ export const params = {
     required: false,
     description: 'The ID of the pagination container.'
   },
-  previous: {
-    type: 'object',
+  previousUrl: {
+    type: 'string',
     required: true,
-    description: 'The value of the previous link href attribute.',
-    params: {
-      href: {
-        type: 'string',
-        required: true,
-        description: 'The value of the previous link href attribute.'
-      },
-      text: {
-        type: 'string',
-        required: false,
-        description:
-          "Text to use within the previous link, defaults to 'Previous page', with 'page' being visually hidden. If `html` is provided, the `text` argument will be ignored."
-      },
-      html: {
-        type: 'string',
-        required: false,
-        description:
-          'If `text` is set, this is not required. HTML to use within the previous link. If `html` is provided, the `text` argument will be ignored.'
-      },
-      labelText: {
-        type: 'string',
-        required: false,
-        description:
-          'The name of the previous page to include within the previous link text'
-      },
-      attributes: {
-        type: 'object',
-        required: false,
-        description:
-          'HTML attributes (for example data attributes) to add to the previous link.'
-      }
-    }
+    description: 'The value of the previous link href attribute.'
   },
-  next: {
-    type: 'object',
+  previousPage: {
+    type: 'string',
     required: true,
-    description: 'The value of the next link href attribute.',
-    params: {
-      href: {
-        type: 'string',
-        required: true,
-        description: 'The value of the next link href attribute.'
-      },
-      text: {
-        type: 'string',
-        required: false,
-        description:
-          "Text to use within the next link, defaults to 'Next page', with 'page' being visually hidden. If `html` is provided, the `text` argument will be ignored."
-      },
-      html: {
-        type: 'string',
-        required: false,
-        description:
-          'If `text` is set, this is not required. HTML to use within the next link. If `html` is provided, the `text` argument will be ignored.'
-      },
-      labelText: {
-        type: 'string',
-        required: false,
-        description:
-          'The name of the next page to include within the previous link text'
-      },
-      attributes: {
-        type: 'object',
-        required: false,
-        description:
-          'HTML attributes (for example data attributes) to add to the next link.'
-      }
-    }
+    description: 'The text of the previous link.'
   },
-  items: {
-    type: 'array',
+  nextUrl: {
+    type: 'string',
     required: true,
-    description: 'Array of items within the pagination component.',
-    params: {
-      number: {
-        type: 'string',
-        required: false,
-        description: 'The pagination item text – usually a page number.'
-      },
-      href: {
-        type: 'string',
-        required: false,
-        description:
-          'The value of the item link `href`. Required unless the item is an ellipsis.'
-      },
-      visuallyHiddenText: {
-        type: 'string',
-        required: false,
-        description:
-          "The visually hidden label for the pagination item, which will be set as an `aria-label` and announced by screen readers on the pagination item link. Should include page number. Defaults to, for example 'Page 1'."
-      },
-      current: {
-        type: 'boolean',
-        required: false,
-        description:
-          'Set to `true` to indicate the current page the user is on.'
-      },
-      ellipsis: {
-        type: 'boolean',
-        required: false,
-        description:
-          'Set to `true` to specify an ellipsis, indicated skipped pages in a numbered sequence.'
-      },
-      classes: {
-        type: 'string',
-        required: false,
-        description: 'Classes to add to the list item.'
-      },
-      attributes: {
-        type: 'object',
-        required: false,
-        description:
-          'The HTML attributes (for example, data attributes) to add to the item link.'
-      }
-    }
+    description: 'The value of the next link href attribute.'
+  },
+  nextPage: {
+    type: 'string',
+    required: true,
+    description: 'The text of the next link.'
   },
   classes: {
     type: 'string',
@@ -153,65 +54,48 @@ export const params = {
 export const examples = {
   default: {
     context: {
-      previous: {
-        href: '#/section/treatments',
-        text: 'Previous',
-        labelText: 'Treatments'
-      },
-      next: {
-        href: '#/section/symptoms',
-        text: 'Next',
-        labelText: 'Symptoms'
-      }
+      previousUrl: '/section/treatments',
+      previousPage: 'Treatments',
+      nextUrl: '/section/symptoms',
+      nextPage: 'Symptoms'
     },
     screenshot: {
-      states: ['focus', 'hover', 'active'],
-      selector: '.nhsuk-pagination__prev a',
-      viewports: ['mobile', 'tablet', 'desktop']
+      viewports: ['mobile', 'tablet', 'desktop', 'large-desktop']
     }
   },
   numbered: {
     context: {
-      previous: {
-        href: '#'
-      },
-      next: {
-        href: '#'
-      },
+      previousUrl: '#',
+      nextUrl: '#',
       items: [
         {
           number: 1,
-          href: '#/page/1'
+          href: '#'
         },
         {
           ellipsis: true
         },
         {
           number: 6,
-          href: '#/page/6'
+          href: '#'
         },
         {
           number: 7,
-          href: '#/page/7',
+          href: '#',
           current: true
         },
         {
           number: 8,
-          href: '#/page/8'
+          href: '#'
         },
         {
           ellipsis: true
         },
         {
           number: 10,
-          href: '#/page/10'
+          href: '#'
         }
       ]
-    },
-    screenshot: {
-      states: ['focus', 'hover', 'active'],
-      selector: '.nhsuk-pagination__prev a',
-      viewports: ['mobile', 'tablet', 'desktop']
     }
   }
 }
