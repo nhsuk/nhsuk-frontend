50efea50818df9208633a01f96a968299ae740cf
anandamaryon1 <159916913+anandamaryon1@users.noreply.github.com>
2025-09-25 13:50:46 +0100

    radios - align macro options descriptions with GOV

--- packages/nhsuk-frontend/src/nhsuk/components/radios/macro-options.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/radios/macro-options.mjs
@@ -17,77 +17,67 @@ export const params = {
   fieldset: {
     type: 'object',
     required: false,
-    description: 'Options for the fieldset component (for example legend).',
+    description: 'The fieldset used by the radios component.',
     isComponent: true
   },
   hint: {
     type: 'object',
     required: false,
-    description: 'Options for the hint component (for example text).',
+    description: 'Can be used to add a hint to the radios component.',
     isComponent: true
   },
   errorMessage: {
     type: 'object',
     required: false,
-    description:
-      'Options for the error message component. The error message component will not display if you use a falsy value for `errorMessage`, for example `false` or `null`.',
+    description: 'Can be used to add an error message to the radios component. The error message component will not display if you use a falsy value for `errorMessage`, for example `false` or `null`.',
     isComponent: true
   },
   formGroup: {
     type: 'object',
     required: false,
-    description:
-      'Additional options for the form group containing the radios component.',
+    description: 'Additional options for the form group containing the radios component.',
     params: {
       classes: {
         type: 'string',
         required: false,
-        description:
-          'Classes to add to the form group (for example to show error state for the whole group).'
+        description: 'Classes to add to the form group (for example to show error state for the whole group).'
       },
       attributes: {
         type: 'object',
         required: false,
-        description:
-          'HTML attributes (for example data attributes) to add to the form group.'
+        description: 'HTML attributes (for example data attributes) to add to the form group.'
       },
       beforeInputs: {
         type: 'object',
         required: false,
-        description:
-          'Content to add before all radio items within the radios component.',
+        description: 'Content to add before all radio items within the radios component.',
         params: {
           text: {
             type: 'string',
             required: true,
-            description:
-              'Text to add before all radio items. If `html` is provided, the `text` option will be ignored.'
+            description: 'Text to add before all radio items. If `html` is provided, the `text` option will be ignored.'
           },
           html: {
             type: 'string',
             required: true,
-            description:
-              'HTML to add before all radio items. If `html` is provided, the `text` option will be ignored.'
+            description: 'HTML to add before all radio items. If `html` is provided, the `text` option will be ignored.'
           }
         }
       },
       afterInputs: {
         type: 'object',
         required: false,
-        description:
-          'Content to add after all radio items within the radios component.',
+        description: 'Content to add after all radio items within the radios component.',
         params: {
           text: {
             type: 'string',
             required: true,
-            description:
-              'Text to add after all radio items. If `html` is provided, the `text` option will be ignored.'
+            description: 'Text to add after all radio items. If `html` is provided, the `text` option will be ignored.'
           },
           html: {
             type: 'string',
             required: true,
-            description:
-              'HTML to add after all radio items. If `html` is provided, the `text` option will be ignored.'
+            description: 'HTML to add after all radio items. If `html` is provided, the `text` option will be ignored.'
           }
         }
       }
@@ -96,96 +86,100 @@ export const params = {
   idPrefix: {
     type: 'string',
     required: false,
-    description:
-      'String to prefix id for each radio item if no id is specified on each item. If `idPrefix` is not passed, fallback to using the name attribute instead.'
+    description: 'Optional prefix. This is used to prefix the `id` attribute for each radio input, hint and error message, separated by `-`. Defaults to the `name` option value.'
   },
   name: {
     type: 'string',
     required: true,
-    description: 'Name attribute for each radio item.'
-  },
-  value: {
-    type: 'string',
-    required: false,
-    description:
-      'The value for the radio which should be checked when the page loads. Use this as an alternative to setting the `checked` option on each individual item.'
+    description: 'Name attribute for the radio items.'
   },
   items: {
     type: 'array',
     required: true,
-    description: 'Array of radio items objects.',
+    description: 'The radio items within the radios component.',
     params: {
       text: {
         type: 'string',
         required: true,
-        description:
-          'If `html` is set, this is not required. Text to use within each radio item label. If `html` is provided, the `text` argument will be ignored.'
+        description: 'If `html` is set, this is not required. Text to use within each radio item label. If `html` is provided, the `text` option will be ignored.'
       },
       html: {
         type: 'string',
         required: true,
-        description:
-          'If `text` is set, this is not required. HTML to use within each radio item label. If `html` is provided, the `text` argument will be ignored.'
+        description: 'If `text` is set, this is not required. HTML to use within each radio item label. If `html` is provided, the `text` option will be ignored.'
       },
       id: {
         type: 'string',
         required: false,
-        description:
-          'Specific id attribute for the radio item. If omitted, then `idPrefix` string will be applied.'
-      },
-      label: {
-        type: 'object',
-        required: false,
-        description: 'Options for the label component.',
-        isComponent: true
+        description: 'Specific ID attribute for the radio item. If omitted, then `idPrefix` string will be applied.'
       },
       value: {
         type: 'string',
         required: true,
         description: 'Value for the radio input.'
       },
+      label: {
+        type: 'object',
+        required: false,
+        description: 'Subset of options for the label used by each radio item within the radios component.',
+        isComponent: true,
+        params: {
+          classes: {
+            type: 'string',
+            required: false,
+            description: 'Classes to add to the label tag.'
+          },
+          attributes: {
+            type: 'object',
+            required: false,
+            description: 'HTML attributes (for example data attributes) to add to the label tag.'
+          }
+        }
+      },
       hint: {
         type: 'object',
         required: false,
-        description: 'Provide hint to each radio item.',
+        description: 'Can be used to add a hint to each radio item within the radios component.',
         isComponent: true
       },
       divider: {
         type: 'string',
         required: false,
-        description:
-          "Divider text to separate radio items, for example the text `'or'`."
+        description: 'Divider text to separate radio items, for example the text `"or"`.'
       },
       checked: {
         type: 'boolean',
         required: false,
-        description: 'If true, radio will be checked.'
+        description: 'Whether the radio should be checked when the page loads. Takes precedence over the top-level `value` option.'
       },
       conditional: {
         type: 'object',
         required: false,
-        description:
-          'Provide additional content to reveal when the radio is checked.',
+        description: 'Provide additional content to reveal when the radio is checked.',
         params: {
           html: {
             type: 'string',
-            description: 'The HTML to reveal when the radio is checked.',
-            required: true
+            required: true,
+            description: 'The HTML to reveal when the radio is checked.'
           }
         }
       },
       disabled: {
         type: 'boolean',
         required: false,
-        description: 'If true, radio will be disabled.'
+        description: 'If `true`, radio will be disabled.'
       },
       attributes: {
         type: 'object',
         required: false,
-        description:
-          'HTML attributes (for example data attributes) to add to the radio input tag.'
+        description: 'HTML attributes (for example data attributes) to add to the radio input tag.'
       }
     }
+  },
+  value: {
+    type: 'string',
+    required: false,
+    description: 'The value for the radio which should be checked when the page loads. Use this as an alternative to setting the `checked` option on each individual item.'
   },
   classes: {
     type: 'string',
@@ -195,8 +189,7 @@ export const params = {
   attributes: {
     type: 'object',
     required: false,
-    description:
-      'HTML attributes (for example data attributes) to add to the radio input tag.'
+    description: 'HTML attributes (for example data attributes) to add to the radio input tag.'
   }
 }
 
