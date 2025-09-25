1282546b388e82b3278d55b3ee9b6f0f35cf671c
anandamaryon1 <159916913+anandamaryon1@users.noreply.github.com>
2025-09-25 11:44:00 +0100

    error message - align macro options descriptions with GOV - retains span, not p, for now

--- packages/nhsuk-frontend/src/nhsuk/components/error-message/macro-options.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/error-message/macro-options.mjs
@@ -11,36 +11,32 @@ export const params = {
   text: {
     type: 'string',
     required: true,
-    description:
-      'If `html` is set, this is not required. Text to use within the error message. If `html` is provided, the `text` argument will be ignored.'
+    description: 'If `html` is set, this is not required. Text to use within the error message. If `html` is provided, the `text` option will be ignored.'
   },
   html: {
     type: 'string',
     required: true,
-    description:
-      'If `text` is set, this is not required. HTML to use within the error message. If `html` is provided, the `text` argument will be ignored.'
+    description: 'If `text` is set, this is not required. HTML to use within the error message. If `html` is provided, the `text` option will be ignored.'
   },
   id: {
     type: 'string',
     required: false,
-    description: 'Id attribute to add to the error message span tag.'
+    description: 'ID attribute to add to the error message `<span>` tag.'
   },
   classes: {
     type: 'string',
     required: false,
-    description: 'Classes to add to the error message span tag.'
+    description: 'Classes to add to the error message `<span>` tag.'
   },
   attributes: {
     type: 'object',
     required: false,
-    description:
-      'HTML attributes (for example data attributes) to add to the error message span tag.'
+    description: 'HTML attributes (for example data attributes) to add to the error message `<span>` tag.'
   },
   visuallyHiddenText: {
     type: 'string',
     required: false,
-    description:
-      'A visually hidden prefix used before the error message. Defaults to `"Error"`.'
+    description: 'A visually hidden prefix used before the error message. Defaults to `"Error"`.'
   }
 }
 
