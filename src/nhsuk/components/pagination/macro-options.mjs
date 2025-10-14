a98637497ad9d658d9a4e9a7c14017f638876741
Colin Rotherham <work@colinr.com>
2025-10-14 08:56:51 +0100

    Add pagination `previous` and `next` Nunjucks options

--- src/nhsuk/components/pagination/macro-options.mjs
+++ src/nhsuk/components/pagination/macro-options.mjs
@@ -13,25 +13,77 @@ export const params = {
     required: false,
     description: 'The ID of the pagination container.'
   },
-  previousUrl: {
-    type: 'string',
-    required: true,
-    description: 'The value of the previous link href attribute.'
+  previous: {
+    type: 'object',
+    required: false,
+    description: 'A link to the previous page, if there is a previous page.',
+    params: {
+      text: {
+        type: 'string',
+        required: false,
+        description:
+          'The text content of the link to the previous page. Defaults to `"Previous page"`, with \'page\' being visually hidden. If `html` is provided, the `text` option will be ignored.'
+      },
+      html: {
+        type: 'string',
+        required: false,
+        description:
+          'The HTML content of the link to the previous page. Defaults to `"Previous page"`, with \'page\' being visually hidden. If `html` is provided, the `text` option will be ignored.'
+      },
+      labelText: {
+        type: 'string',
+        required: false,
+        description:
+          'The optional label that goes underneath the link to the previous page, providing further context for the user about where the link goes.'
+      },
+      href: {
+        type: 'string',
+        required: true,
+        description: "The previous page's URL."
+      },
+      attributes: {
+        type: 'object',
+        required: false,
+        description:
+          'The HTML attributes (for example, data attributes) you want to add to the anchor.'
+      }
+    }
   },
-  previousPage: {
-    type: 'string',
-    required: true,
-    description: 'The text of the previous link.'
-  },
-  nextUrl: {
-    type: 'string',
-    required: true,
-    description: 'The value of the next link href attribute.'
-  },
-  nextPage: {
-    type: 'string',
-    required: true,
-    description: 'The text of the next link.'
+  next: {
+    type: 'object',
+    required: false,
+    description: 'A link to the next page, if there is a next page.',
+    params: {
+      text: {
+        type: 'string',
+        required: false,
+        description:
+          'The text content of the link to the next page. Defaults to `"Next page"`, with \'page\' being visually hidden. If `html` is provided, the `text` option will be ignored.'
+      },
+      html: {
+        type: 'string',
+        required: false,
+        description:
+          'The HTML content of the link to the next page. Defaults to `"Next page"`, with \'page\' being visually hidden. If `html` is provided, the `text` option will be ignored.'
+      },
+      labelText: {
+        type: 'string',
+        required: false,
+        description:
+          'The optional label that goes underneath the link to the next page, providing further context for the user about where the link goes.'
+      },
+      href: {
+        type: 'string',
+        required: true,
+        description: "The next page's URL."
+      },
+      attributes: {
+        type: 'object',
+        required: false,
+        description:
+          'The HTML attributes (for example, data attributes) you want to add to the anchor.'
+      }
+    }
   },
   landmarkLabel: {
     type: 'string',
@@ -42,13 +94,13 @@ export const params = {
   classes: {
     type: 'string',
     required: false,
-    description: 'Classes to add to the pagination container.'
+    description: 'The classes you want to add to the pagination `nav` parent.'
   },
   attributes: {
     type: 'object',
     required: false,
     description:
-      'HTML attributes (for example data attributes) to add to the pagination container.'
+      'The HTML attributes (for example, data attributes) you want to add to the pagination `nav` parent.'
   }
 }
 
@@ -58,15 +110,44 @@ export const params = {
  * @satisfies {{ [example: string]: MacroExample }}
  */
 export const examples = {
-  default: {
+  'default': {
+    context: {
+      previous: {
+        labelText: 'Treatments',
+        href: '/section/treatments'
+      },
+      next: {
+        labelText: 'Symptoms',
+        href: '/section/symptoms'
+      }
+    },
+    screenshot: {
+      viewports: ['mobile', 'tablet', 'desktop', 'large-desktop']
+    }
+  },
+  'with deprecated parameters': {
     context: {
       previousUrl: '/section/treatments',
       previousPage: 'Treatments',
       nextUrl: '/section/symptoms',
       nextPage: 'Symptoms'
     },
-    screenshot: {
-      viewports: ['mobile', 'tablet', 'desktop', 'large-desktop']
+    options: {
+      hidden: true
+    }
+  },
+  'with translations': {
+    context: {
+      previous: {
+        text: 'Blaenorol',
+        labelText: 'Driniaethau',
+        href: '/section/driniaethau'
+      },
+      next: {
+        text: 'Nesaf',
+        labelText: 'Symptomau',
+        href: '/section/symptomau'
+      }
     }
   }
 }
