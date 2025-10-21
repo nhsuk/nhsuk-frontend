d520f78cfa1d2ccd5ff7a5417a6d97ed326a6f42
Colin Rotherham <work@colinr.com>
2025-10-21 09:34:07 +0100

    Consolidate Rollup builds using multiple inputs

--- tasks/scripts.mjs
+++ tasks/scripts.mjs
@@ -1,70 +1,58 @@
 import { join } from 'node:path'
 
 import * as config from '@nhsuk/frontend-config'
-import { getListing } from '@nhsuk/frontend-lib/files.mjs'
 import { scripts, task } from '@nhsuk/frontend-tasks'
 import gulp from 'gulp'
 
 export const compile = gulp.series(
-  /**
-   * Transform NHS.UK frontend scripts
-   */
   task.name('scripts:transform', async () => {
-    const modulePaths = getListing('nhsuk/components/**/*.mjs', {
-      cwd: join(config.paths.pkg, 'src'),
-      ignore: ['**/macro-options.mjs', '**/*.test.{cjs,js,mjs}']
-    })
+    /**
+     * Transform NHS.UK frontend modules into:
+     *
+     * - ECMAScript (ES) modules for Node.js or bundler `import`
+     *   (External dependencies resolved via `node_modules`)
+     */
+    await scripts.compile(['nhsuk/nhsuk.mjs', 'nhsuk/index.mjs'], {
+      srcPath: join(config.paths.pkg, 'src'),
+      destPath: join(config.paths.pkg, 'dist'),
 
-    // Create Rollup bundle(s)
-    for (const modulePath of [
-      ...modulePaths,
+      // Customise input
+      input: {
+        treeshake: false
+      },
 
-      // Build entry scripts last to restore modules
-      // removed from components due to tree-shaking
-      'nhsuk/common/index.mjs',
-      'nhsuk/nhsuk.mjs',
-      'nhsuk/index.mjs'
-    ]) {
-      await Promise.all([
-        /**
-         * Rollup output for each module:
-         *
-         * - ECMAScript (ES) modules for Node.js or bundler `import`
-         *   (External dependencies resolved via `node_modules`)
-         */
-        scripts.compile(modulePath, {
-          srcPath: join(config.paths.pkg, 'src'),
-          destPath: join(config.paths.pkg, 'dist'),
+      // Customise output
+      output: {
+        entryFileNames: '[name].mjs',
+        format: 'esm',
+        preserveModules: true,
+        preserveModulesRoot: 'src'
+      }
+    })()
 
-          // Customise output
-          output: {
-            entryFileNames: '[name].mjs',
-            format: 'esm',
-            preserveModules: true,
-            preserveModulesRoot: 'src'
-          }
-        })(),
+    /**
+     * Transform NHS.UK frontend modules into:
+     *
+     * - CommonJS (CJS) modules for Node.js or bundler `require()`
+     *   (External dependencies resolved via `node_modules`)
+     */
+    await scripts.compile(['nhsuk/nhsuk.mjs', 'nhsuk/index.mjs'], {
+      srcPath: join(config.paths.pkg, 'src'),
+      destPath: join(config.paths.pkg, 'dist'),
 
-        /**
-         * Rollup output for each module:
-         *
-         * - CommonJS (CJS) modules for Node.js or bundler `import`
-         *   (External dependencies resolved via `node_modules`)
-         */
-        scripts.compile(modulePath, {
-          srcPath: join(config.paths.pkg, 'src'),
-          destPath: join(config.paths.pkg, 'dist'),
+      // Customise input
+      input: {
+        treeshake: false
+      },
 
-          // Customise output
-          output: {
-            entryFileNames: '[name].js',
-            format: 'cjs',
-            preserveModules: true,
-            preserveModulesRoot: 'src'
-          }
-        })()
-      ])
-    }
+      // Customise output
+      output: {
+        entryFileNames: '[name].js',
+        format: 'cjs',
+        preserveModules: true,
+        preserveModulesRoot: 'src'
+      }
+    })()
   }),
 
   /**
