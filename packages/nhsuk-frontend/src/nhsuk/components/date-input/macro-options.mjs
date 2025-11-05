31d9ee70e92dc60f4ef6269f817528b267e9e460
Ed Horsford <mail@edwardhorsford.com>
2025-11-05 13:59:14 +0000

    Fix broken links

--- packages/nhsuk-frontend/src/nhsuk/components/date-input/macro-options.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/date-input/macro-options.mjs
@@ -58,13 +58,13 @@ export const params = {
         type: 'string',
         required: false,
         description:
-          'Attribute to meet [WCAG success criterion 1.3.5: Identify input purpose](https://www.w3.org/WAI/WCAG22/Understanding/identify-input-purposeandard](https://html.spec.whatwg.org/multipage/form-control-infrastructure used.'
+          'Attribute to meet [WCAG success criterion 1.3.5: Identify input purpose](https://www.w3.org/WAI/WCAG22/Understanding/identify-input-purpose). See the [HTML Standard](https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#attr-input-autocomplete) for more information.'
       },
       pattern: {
         type: 'string',
         required: false,
         description:
-          'Attribute to [provide a regular expression pattern](https://html.spec.whatharacter combinations for the input value.'
+          'Attribute to [provide a regular expression pattern](https://html.spec.whatwg.org/multipage/input.html#attr-input-pattern) to validate character combinations for the input value.'
       },
       classes: {
         type: 'string',
