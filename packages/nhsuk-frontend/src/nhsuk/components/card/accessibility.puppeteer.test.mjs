0c09fe93ce352580e0e9d2f7195e0a67cf26bd36
Colin Rotherham <work@colinr.com>
2025-11-07 15:26:50 +0000

    Split out accessibility tests per example

--- packages/nhsuk-frontend/src/nhsuk/components/card/accessibility.puppeteer.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/card/accessibility.puppeteer.test.mjs
@@ -6,13 +6,14 @@ describe('Card', () => {
   /** @type {Page} */
   let page
 
-  describe('Component examples', () => {
-    it('passes accessibility tests', async () => {
-      for (const example in examples) {
-        page = await goToComponent(browser, 'card', { example })
-        await expect(axe(page)).resolves.toHaveNoViolations()
-      }
-    }, 120000)
+  describe.each(Object.keys(examples))('%s', (example) => {
+    beforeAll(async () => {
+      page = await goToComponent(browser, 'card', { example })
+    })
+
+    it('passes accessibility tests', () => {
+      return expect(axe(page)).resolves.toHaveNoViolations()
+    })
   })
 })
 
