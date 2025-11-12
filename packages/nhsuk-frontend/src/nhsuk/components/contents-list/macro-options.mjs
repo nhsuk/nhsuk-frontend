f2244081b5a7731a4790df3f3b8b5fc12286c4ae
Colin Rotherham <work@colinr.com>
2025-11-12 17:26:25 +0000

    Add contents list `item.html`, `item.classes` and `item.attributes` Nunjucks options

--- packages/nhsuk-frontend/src/nhsuk/components/contents-list/macro-options.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/contents-list/macro-options.mjs
@@ -33,7 +33,25 @@ export const params = {
       text: {
         type: 'string',
         required: true,
-        description: 'Text to use within each content list item label.'
+        description:
+          'If `html` is set, this is not required. Text to use within each content list item label. If `html` is provided, the `text` argument will be ignored.'
+      },
+      html: {
+        type: 'string',
+        required: true,
+        description:
+          'If `text` is set, this is not required. HTML to use within each content list item label. If `html` is provided, the `text` argument will be ignored.'
+      },
+      classes: {
+        type: 'string',
+        required: false,
+        description: 'Classes to add to the content list item label.'
+      },
+      attributes: {
+        type: 'object',
+        required: false,
+        description:
+          'HTML attributes (for example data attributes) to add to the content list item label.'
       }
     }
   },
