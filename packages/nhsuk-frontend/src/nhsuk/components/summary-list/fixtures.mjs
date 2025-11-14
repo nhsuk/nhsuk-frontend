0fffaa2c8d628e8ce35fb4935f5c3908fe8c94cb
Colin Rotherham <work@colinr.com>
2025-11-14 11:37:23 +0000

    Add card to summary list component

--- packages/nhsuk-frontend/src/nhsuk/components/summary-list/fixtures.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/summary-list/fixtures.mjs
@@ -449,6 +449,418 @@ export const examples = {
     screenshot: {
       viewports: ['mobile', 'tablet', 'desktop']
     }
+  },
+  'as a card': {
+    context: {
+      card: {
+        heading: 'Regional Manager',
+        headingSize: 'm'
+      },
+      rows: [
+        {
+          key: {
+            text: 'Name'
+          },
+          value: {
+            text: 'Karen Francis'
+          }
+        },
+        {
+          key: {
+            text: 'Date of birth'
+          },
+          value: {
+            text: '15 March 1984'
+          }
+        }
+      ]
+    }
+  },
+  'as a card with multiple actions': {
+    context: {
+      card: {
+        heading: 'Regional Manager',
+        headingSize: 'm',
+        actions: {
+          items: [
+            {
+              text: 'Delete',
+              href: '#'
+            },
+            {
+              text: 'Withdraw',
+              href: '#'
+            }
+          ]
+        }
+      },
+      rows: [
+        {
+          key: {
+            text: 'Name'
+          },
+          value: {
+            text: 'Karen Francis'
+          }
+        },
+        {
+          key: {
+            text: 'Date of birth'
+          },
+          value: {
+            text: '15 March 1984'
+          },
+          actions: {
+            items: [
+              {
+                href: '#',
+                text: 'Change',
+                visuallyHiddenText: 'date of birth'
+              }
+            ]
+          }
+        },
+        {
+          key: {
+            text: 'Contact information'
+          },
+          value: {
+            html: outdent`
+              73 Roman Rd<br>
+              Leeds<br>
+              LS2 5ZN
+            `
+          },
+          actions: {
+            items: [
+              {
+                href: '#',
+                text: 'Change',
+                visuallyHiddenText: 'contact information'
+              }
+            ]
+          }
+        },
+        {
+          key: {
+            text: 'Contact details'
+          },
+          value: {
+            html: outdent`
+              <p>07700 900362</p>
+              <p>karen.francis@example.com</p>
+            `
+          },
+          actions: {
+            items: [
+              {
+                href: '#',
+                text: 'Add',
+                visuallyHiddenText: 'new contact details'
+              },
+              {
+                href: '#',
+                text: 'Change',
+                visuallyHiddenText: 'contact details'
+              }
+            ]
+          }
+        },
+        {
+          key: {
+            text: 'Medicines'
+          },
+          value: {
+            html: outdent`
+              <p>Isotretinoin capsules (Roaccutane)</p>
+              <p>Isotretinoin gel (Isotrex)</p>
+              <p>Pepto-Bismol (bismuth subsalicylate)</p>
+            `
+          },
+          actions: {
+            items: [
+              {
+                href: '#',
+                text: 'Add',
+                visuallyHiddenText: 'new medicine'
+              },
+              {
+                href: '#',
+                text: 'Change',
+                visuallyHiddenText: 'medicines'
+              }
+            ]
+          }
+        }
+      ]
+    },
+    options: {
+      width: 'full'
+    }
+  },
+  'as a card with actions': {
+    context: {
+      card: {
+        heading: 'Regional Manager',
+        headingSize: 'm',
+        actions: {
+          items: [
+            {
+              text: 'Delete',
+              href: '#/delete'
+            },
+            {
+              text: 'Withdraw',
+              href: '#/withdraw'
+            }
+          ]
+        }
+      },
+      rows: [
+        {
+          key: {
+            text: 'Name'
+          },
+          value: {
+            text: 'Karen Francis'
+          }
+        },
+        {
+          classes: 'nhsuk-summary-list__row--no-border',
+          key: {
+            text: 'Date of birth'
+          },
+          value: {
+            text: '15 March 1984'
+          }
+        }
+      ]
+    }
+  },
+  'as a card (secondary) with actions': {
+    context: {
+      card: {
+        heading: 'Regional Manager',
+        headingSize: 'm',
+        secondary: true,
+        actions: {
+          items: [
+            {
+              text: 'Delete',
+              href: '#/delete'
+            },
+            {
+              text: 'Withdraw',
+              href: '#/withdraw'
+            }
+          ]
+        }
+      },
+      rows: [
+        {
+          key: {
+            text: 'Name'
+          },
+          value: {
+            text: 'Karen Francis'
+          }
+        },
+        {
+          classes: 'nhsuk-summary-list__row--no-border',
+          key: {
+            text: 'Date of birth'
+          },
+          value: {
+            text: '15 March 1984'
+          }
+        }
+      ]
+    }
+  },
+  'as a card (feature) with actions': {
+    context: {
+      card: {
+        heading: 'Regional Manager',
+        headingSize: 'm',
+        feature: true,
+        actions: {
+          items: [
+            {
+              text: 'Delete',
+              href: '#/delete'
+            },
+            {
+              text: 'Withdraw',
+              href: '#/withdraw'
+            }
+          ]
+        }
+      },
+      rows: [
+        {
+          key: {
+            text: 'Name'
+          },
+          value: {
+            text: 'Karen Francis'
+          }
+        },
+        {
+          classes: 'nhsuk-summary-list__row--no-border',
+          key: {
+            text: 'Date of birth'
+          },
+          value: {
+            text: '15 March 1984'
+          }
+        }
+      ]
+    }
+  },
+  'as a card (clickable) without actions': {
+    context: {
+      card: {
+        heading: 'Regional Manager',
+        headingSize: 'm',
+        href: '#/card-clickable',
+        clickable: true
+      },
+      rows: [
+        {
+          key: {
+            text: 'Name'
+          },
+          value: {
+            text: 'Karen Francis'
+          }
+        },
+        {
+          classes: 'nhsuk-summary-list__row--no-border',
+          key: {
+            text: 'Date of birth'
+          },
+          value: {
+            text: '15 March 1984'
+          }
+        }
+      ]
+    }
+  },
+  'as a card (type non-urgent) with actions': {
+    context: {
+      card: {
+        heading: 'Regional Manager',
+        type: 'non-urgent',
+        actions: {
+          items: [
+            {
+              text: 'Delete',
+              href: '#/delete'
+            },
+            {
+              text: 'Withdraw',
+              href: '#/withdraw'
+            }
+          ]
+        }
+      },
+      rows: [
+        {
+          key: {
+            text: 'Name'
+          },
+          value: {
+            text: 'Karen Francis'
+          }
+        },
+        {
+          classes: 'nhsuk-summary-list__row--no-border',
+          key: {
+            text: 'Date of birth'
+          },
+          value: {
+            text: '15 March 1984'
+          }
+        }
+      ]
+    }
+  },
+  'as a card (type urgent) with actions': {
+    context: {
+      card: {
+        heading: 'Regional Manager',
+        type: 'urgent',
+        actions: {
+          items: [
+            {
+              text: 'Delete',
+              href: '#/delete'
+            },
+            {
+              text: 'Withdraw',
+              href: '#/withdraw'
+            }
+          ]
+        }
+      },
+      rows: [
+        {
+          key: {
+            text: 'Name'
+          },
+          value: {
+            text: 'Karen Francis'
+          }
+        },
+        {
+          classes: 'nhsuk-summary-list__row--no-border',
+          key: {
+            text: 'Date of birth'
+          },
+          value: {
+            text: '15 March 1984'
+          }
+        }
+      ]
+    }
+  },
+  'as a card (type emergency) with actions': {
+    context: {
+      card: {
+        heading: 'Regional Manager',
+        type: 'emergency',
+        actions: {
+          items: [
+            {
+              text: 'Delete',
+              href: '#'
+            },
+            {
+              text: 'Withdraw',
+              href: '#'
+            }
+          ]
+        }
+      },
+      rows: [
+        {
+          key: {
+            text: 'Name'
+          },
+          value: {
+            text: 'Karen Francis'
+          }
+        },
+        {
+          key: {
+            text: 'Date of birth'
+          },
+          value: {
+            text: '15 March 1984'
+          }
+        }
+      ]
+    }
   }
 }
 
