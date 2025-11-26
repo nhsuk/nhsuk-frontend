8a56017a48f624008dd3cbf2669cbf6112522509
Colin Rotherham <work@colinr.com>
2025-11-26 17:10:50 +0000

    Update accessibility tests to share pages

--- packages/nhsuk-frontend/src/nhsuk/components/date-input/accessibility.puppeteer.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/date-input/accessibility.puppeteer.test.mjs
@@ -1,4 +1,8 @@
-import { axe, goToComponent } from '@nhsuk/frontend-helpers/puppeteer.mjs'
+import {
+  axe,
+  getPage,
+  goToComponent
+} from '@nhsuk/frontend-helpers/puppeteer.mjs'
 
 import { examples } from './fixtures.mjs'
 
@@ -6,12 +10,13 @@ describe('Date input', () => {
   /** @type {Page} */
   let page
 
-  describe.each(Object.keys(examples))('%s', (example) => {
-    beforeAll(async () => {
-      page = await goToComponent(browser, 'date-input', { example })
-    })
+  beforeAll(async () => {
+    page = await getPage(browser)
+  })
 
-    it('passes accessibility tests', () => {
+  describe.each(Object.keys(examples))('%s', (example) => {
+    it('passes accessibility tests', async () => {
+      await goToComponent(page, 'date-input', { example })
       return expect(axe(page)).resolves.toHaveNoViolations()
     })
   })
