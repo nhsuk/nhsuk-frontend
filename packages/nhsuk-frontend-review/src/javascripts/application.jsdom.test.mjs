d80b3d07073c60b8685422835ce889acfa103158
Colin Rotherham <work@colinr.com>
2025-12-11 11:15:53 +0000

    Configure Babel to preserve dynamic import

--- packages/nhsuk-frontend-review/src/javascripts/application.jsdom.test.mjs
+++ packages/nhsuk-frontend-review/src/javascripts/application.jsdom.test.mjs
@@ -3,8 +3,8 @@ import { initAll } from 'nhsuk-frontend'
 jest.mock('nhsuk-frontend')
 
 describe('Review app', () => {
-  it('should init NHS.UK frontend without auto-focus', async () => {
-    await import('./application.mjs')
+  it('should init NHS.UK frontend without auto-focus', () => {
+    jest.requireActual('./application.mjs')
 
     expect(initAll).toHaveBeenCalledWith({
       errorSummary: { disableAutoFocus: true },
