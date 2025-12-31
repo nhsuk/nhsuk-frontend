6e8d3dccc2053f739e77f510db58f35bcf35c6ba
Colin Rotherham <work@colinr.com>
2025-12-31 09:54:24 +0000

    Update unit tests

--- packages/nhsuk-frontend/src/nhsuk/core/core.unit.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/core/core.unit.test.mjs
@@ -52,6 +52,7 @@ describe('Core', () => {
         --nhsuk-success-colour: #007f3b;
         --nhsuk-border-colour: #d8dde0;
         --nhsuk-border-hover-colour: #aeb7bd;
+        --nhsuk-border-text-colour: #768692;
         --nhsuk-secondary-border-colour: rgba(255, 255, 255, 0.2);
         --nhsuk-input-border-colour: #4c6272;
         --nhsuk-hover-colour: #aeb7bd;
