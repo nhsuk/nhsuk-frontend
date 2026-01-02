26e81c0b32887a52961e3170f6213965da002c7f
Colin Rotherham <work@colinr.com>
2026-01-02 21:25:28 +0000

    Uplift GOV.UK Frontend accordion component

---
+++ packages/nhsuk-frontend/src/nhsuk/components/accordion/accessibility.puppeteer.test.mjs
@@ -0,0 +1,25 @@
+import {
+  axe,
+  getOptions,
+  goToComponent
+} from '@nhsuk/frontend-helpers/puppeteer.mjs'
+
+import { examples } from './fixtures.mjs'
+
+describe('Accordion', () => {
+  it('listing page passes accessibility tests', async () => {
+    await goToComponent(page, 'accordion')
+    return expect(axe(page)).resolves.toHaveNoViolations()
+  }, 20000)
+
+  describe.each(Object.entries(examples))('%s', (name, example) => {
+    it.each(getOptions(name, example))(
+      '$title passes accessibility tests',
+      async (options) => {
+        await goToComponent(page, 'accordion', options)
+        return expect(axe(page)).resolves.toHaveNoViolations()
+      },
+      20000
+    )
+  })
+})
