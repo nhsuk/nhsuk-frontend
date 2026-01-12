12e7f9b1b3c3eae3b69716ab2909f9ad85057ba0
Frankie Roberto <frankie@frankieroberto.com>
2026-01-12 15:55:46 +0000

    Add a modifier class to tables with sortable columns
    This lets us add a small amount of padding to the table headers

--- packages/nhsuk-frontend/src/nhsuk/components/tables/table.jsdom.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/tables/table.jsdom.test.mjs
@@ -102,6 +102,11 @@ describe('Table', () => {
     })
   })
 
+  it('should add a class to the table element', () => {
+    initTables()
+    expect($root.classList).toContain('nhsuk-table--with-sortable-columns')
+  })
+
   describe('Heading buttons', () => {
     beforeEach(() => {
       initTables()
