b3ba35be72b1cc0599db5baa01b0666f3ad04fc2
Colin Rotherham <work@colinr.com>
2026-01-12 15:52:12 +0000

    Support cards with image captions etc

--- packages/nhsuk-frontend/src/nhsuk/components/card/fixtures.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/card/fixtures.mjs
@@ -714,6 +714,21 @@ export const examples = {
       viewports: ['mobile', 'tablet', 'desktop']
     }
   },
+  'feature with A to Z content': {
+    context: {
+      feature: true,
+      heading: 'A',
+      headingId: 'a',
+      headingSize: 'm',
+      descriptionHtml: outdent`
+        <ul class="nhsuk-list nhsuk-list--border">
+          <li><a href="#/conditions/abdominal-aortic-aneurysm/">AAA, see Abdominal aortic aneurysm</a></li>
+          <li><a href="#/conditions/abdominal-aortic-aneurysm/">Abdominal aortic aneurysm</a></li>
+          <li><a href="#/conditions/abscess/">Abscess</a></li>
+        </ul>
+      `
+    }
+  },
   'feature with summary list': {
     context: {
       feature: true,
@@ -838,6 +853,47 @@ export const examples = {
     },
     screenshot: {
       viewports: ['mobile', 'tablet', 'desktop']
+    }
+  },
+  'with image and caption': {
+    context: {
+      image: {
+        html: components.render('images', {
+          context: {
+            src: 'https://assets.nhs.uk/prod/images/A_0218_exercise-main_FKW1X7.width-690.jpg',
+            caption: {
+              classes: 'nhsuk-u-secondary-text-colour',
+              text: 'No specific amount of time is recommended, but a typical training session could take less than 20 minutes.'
+            }
+          }
+        })
+      },
+      href: '#',
+      clickable: true,
+      heading: 'Exercise',
+      headingSize: 'm',
+      description:
+        'Programmes, workouts and tips to get you moving and improve your fitness and wellbeing'
+    }
+  },
+  'with image and custom HTML': {
+    context: {
+      image: {
+        src: 'https://service-manual.nhs.uk/assets/blog-prototype-kit.png'
+      },
+      href: 'https://digital.nhs.uk/blog/design-matters/2025/why-we-are-reinvesting-in-the-nhs-prototype-kit',
+      clickable: true,
+      heading: 'Why we are reinvesting in the NHS prototype kit',
+      headingSize: 'm',
+      headingHtml: outdent`
+        <p class="nhsuk-body-s nhsuk-u-secondary-text-colour nhsuk-u-margin-bottom-0"><span class="nhsuk-u-visually-hidden">Published on: </span>21 July 2025</p>
+        <p class="nhsuk-body-s nhsuk-u-font-weight-bold">NHS England Design Matters blog</p>
+      `,
+      description:
+        'Frankie Roberto and Mike Gallagher explain why we revived the NHS prototype kit, the benefits of prototyping in code and how digital teams in the NHS can get started using it.'
+    },
+    options: {
+      width: 'one-half'
     }
   },
   'top task': {
