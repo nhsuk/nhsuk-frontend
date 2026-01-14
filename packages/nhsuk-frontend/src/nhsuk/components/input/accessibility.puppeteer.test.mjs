5fbc2548a1307e69a14abba60041a28f67ea5351
Colin Rotherham <work@colinr.com>
2026-01-14 09:03:38 +0000

    Run accessibility tests on component listing pages

--- packages/nhsuk-frontend/src/nhsuk/components/input/accessibility.puppeteer.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/input/accessibility.puppeteer.test.mjs
@@ -7,6 +7,11 @@ import {
 import { examples } from './fixtures.mjs'
 
 describe('Input', () => {
+  it('Listing page passes accessibility tests', async () => {
+    await goToComponent(page, 'input')
+    return expect(axe(page)).resolves.toHaveNoViolations()
+  })
+
   describe.each(Object.entries(examples))('%s', (name, example) => {
     it.each(getOptions(name, example))(
       '$title passes accessibility tests',
--- packages/nhsuk-frontend/src/nhsuk/components/summary-list/accessibility.puppeteer.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/summary-list/accessibility.puppeteer.test.mjs
@@ -7,6 +7,11 @@ import {
 import { examples } from './fixtures.mjs'
 
 describe('Summary list', () => {
+  it('Listing page passes accessibility tests', async () => {
+    await goToComponent(page, 'summary-list')
+    return expect(axe(page)).resolves.toHaveNoViolations()
+  })
+
   describe.each(Object.entries(examples))('%s', (name, example) => {
     it.each(getOptions(name, example))(
       '$title passes accessibility tests',
