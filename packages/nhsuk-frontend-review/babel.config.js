20597adca48bcfb1584570226f2d08560ae430eb
Colin Rotherham <work@colinr.com>
2026-01-14 14:40:32 +0000

    Add Babel to review app

---
+++ packages/nhsuk-frontend-review/babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  browserslistEnv: 'javascripts',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        // Apply bug fixes to avoid transforms
+        bugfixes: true,
+
+        // Apply smaller "loose" transforms for browsers
+        loose: true
+      }
+    ]
+  ],
+  env: {
+    test: {
+      browserslistEnv: 'node'
+    }
+  }
+}
