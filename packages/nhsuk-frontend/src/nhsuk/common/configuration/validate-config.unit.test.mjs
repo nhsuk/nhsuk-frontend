f014365069c2a244fb420cfb8fa1c7f2fe03a4e8
Colin Rotherham <work@colinr.com>
2026-02-25 19:55:31 +0000

    Add code coverage for `validateConfig()` with 'allOf'

--- packages/nhsuk-frontend/src/nhsuk/common/configuration/validate-config.unit.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/common/configuration/validate-config.unit.test.mjs
@@ -1,8 +1,85 @@
 import { validateConfig } from './validate-config.mjs'
 
-import { MockConfigurableComponentAnyOf } from '#lib/fixtures/configuration/mock-component.mjs'
+import {
+  MockConfigurableComponentAllOf,
+  MockConfigurableComponentAnyOf
+} from '#lib/fixtures/configuration/mock-component.mjs'
 
 describe('validateConfig', () => {
+  describe("schema 'allOf' validation", () => {
+    it.each([
+      {
+        config: {},
+        expected: [
+          '"example1" and "example2" must be provided',
+          '"example3" must be provided',
+          '"example4" must be provided'
+        ]
+      },
+      {
+        config: {
+          example1: 'example1'
+        },
+        expected: [
+          '"example1" and "example2" must be provided',
+          '"example3" must be provided',
+          '"example4" must be provided'
+        ]
+      },
+      {
+        config: {
+          example2: 'example2'
+        },
+        expected: [
+          '"example1" and "example2" must be provided',
+          '"example3" must be provided',
+          '"example4" must be provided'
+        ]
+      },
+      {
+        config: {
+          example3: 100
+        },
+        expected: [
+          '"example1" and "example2" must be provided',
+          '"example4" must be provided'
+        ]
+      },
+      {
+        config: {
+          example4: true
+        },
+        expected: [
+          '"example1" and "example2" must be provided',
+          '"example3" must be provided'
+        ]
+      },
+      {
+        config: {
+          example3: 100,
+          example4: true
+        },
+        expected: ['"example1" and "example2" must be provided']
+      },
+      {
+        config: {
+          example1: 'example1',
+          example2: 'example2',
+          example3: 100,
+          example4: true
+        },
+        expected: []
+      }
+    ])('returns errors unless all schema pass', ({ config, expected }) => {
+      const errors = validateConfig(
+        MockConfigurableComponentAllOf.schema,
+        config
+      )
+
+      expect(errors).toEqual(expected)
+    })
+  })
+
   describe("schema 'anyOf' validation", () => {
     it.each([
       {
