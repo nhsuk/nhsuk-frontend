8030d663ffd5a90c6317aa4a32481cd891486a19
Colin Rotherham <work@colinr.com>
2026-03-06 17:55:28 +0000

    Add new Nunjucks global `getComponentMacro()`

--- src/nhsuk/lib/nunjucks/globals/index.mjs
+++ src/nhsuk/lib/nunjucks/globals/index.mjs
@@ -2,4 +2,5 @@
  * Nunjucks globals
  */
 export { getComponentHTML } from './get-component-html.mjs'
+export { getComponentMacro } from './get-component-macro.mjs'
 export { uniqueHTML } from './unique-html.mjs'
