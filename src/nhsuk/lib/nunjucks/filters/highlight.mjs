670a8b866ab3605b0b409b47a79a381f5bcd6e76
Colin Rotherham <work@colinr.com>
2026-03-09 11:57:23 +0000

    Automatically make safe when highlighting

--- src/nhsuk/lib/nunjucks/filters/highlight.mjs
+++ src/nhsuk/lib/nunjucks/filters/highlight.mjs
@@ -5,11 +5,20 @@ import { unindent } from './unindent.mjs'
 /**
  * Format code with syntax highlighting
  *
+ * @this {{ env: Environment }}
  * @param {string} code - Code in plain text
  * @param {string | string[]} [language] - Code programming language(s)
- * @returns {string} Code with syntax highlighting
+ * @returns {runtime.SafeString} Code with syntax highlighting
  */
 export function highlight(code, language = []) {
+  const safe = this.env.getFilter('safe')
+
   const languages = language ? [language].flat() : undefined
-  return highlighter.highlightAuto(unindent(code), languages).value
+  const { value } = highlighter.highlightAuto(unindent(code), languages)
+
+  return safe(value)
 }
+
+/**
+ * @import { Environment, runtime } from 'nunjucks'
+ */
