80daf415191cd5d8ea976e9c506b3087d35b5429
Colin Rotherham <work@colinr.com>
2026-03-11 17:15:28 +0000

    Add new Nunjucks global `getComponentHTML()`

---
+++ packages/nhsuk-frontend/src/nhsuk/lib/nunjucks/globals/get-component-html.mjs
@@ -0,0 +1,18 @@
+import { components } from '#lib'
+
+/**
+ * Component HTML code (formatted)
+ *
+ * @this {{ env: Environment }}
+ * @param {string} component - Component name
+ * @param {MacroRenderOptions} [options] - Nunjucks macro render options
+ * @returns {string} HTML rendered by the component
+ */
+export function getComponentHTML(component, options = {}) {
+  return components.render(component, { env: this.env, ...options })
+}
+
+/**
+ * @import { MacroRenderOptions } from '#lib'
+ * @import { Environment } from 'nunjucks'
+ */
--- packages/nhsuk-frontend/src/nhsuk/lib/nunjucks/globals/index.mjs
+++ packages/nhsuk-frontend/src/nhsuk/lib/nunjucks/globals/index.mjs
@@ -1,4 +1,5 @@
 /**
  * Nunjucks globals
  */
+export { getComponentHTML } from './get-component-html.mjs'
 export { uniqueHTML } from './unique-html.mjs'
