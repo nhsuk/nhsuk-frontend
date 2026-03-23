7d55acd88da31f988332dad74b0e2718ce3eda4c
Colin Rotherham <work@colinr.com>
2026-03-23 16:24:22 +0000

    Add release info to macro options

--- packages/nhsuk-frontend/src/nhsuk/components/action-link/macro-options.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/action-link/macro-options.mjs
@@ -9,46 +9,54 @@ const options = {
   id: {
     type: 'string',
     required: false,
-    description: 'The ID of the action link.'
+    description: 'The ID of the action link.',
+    released: '10.0.0'
   },
   text: {
     type: 'string',
     required: true,
     description:
-      'If `html` is set, this is not required. Text to use within the action link. If `html` is provided, the `text` option will be ignored.'
+      'If `html` is set, this is not required. Text to use within the action link. If `html` is provided, the `text` option will be ignored.',
+    released: '1.0.0'
   },
   html: {
     type: 'string',
     required: true,
     description:
-      'If `text` is set, this is not required. HTML to use within the action link. If `html` is provided, the `text` option will be ignored.'
+      'If `text` is set, this is not required. HTML to use within the action link. If `html` is provided, the `text` option will be ignored.',
+    released: '10.2.0'
   },
   href: {
     type: 'string',
     required: true,
-    description: 'The value of the link `href` attribute.'
+    description: 'The value of the link `href` attribute.',
+    released: '1.0.0'
   },
   openInNewWindow: {
     type: 'boolean',
     required: false,
-    description: 'If set to `true`, then the link will open in a new window.'
+    description: 'If set to `true`, then the link will open in a new window.',
+    released: '1.0.0'
   },
   variant: {
     type: 'string',
     required: false,
     description:
-      'Optional variant of action link. You can use only `"reverse"` or empty values with this option.'
+      'Optional variant of action link. You can use only `"reverse"` or empty values with this option.',
+    released: '10.4.0'
   },
   classes: {
     type: 'string',
     required: false,
-    description: 'Classes to add to the anchor tag.'
+    description: 'Classes to add to the anchor tag.',
+    released: '1.0.0'
   },
   attributes: {
     type: 'object',
     required: false,
     description:
-      'HTML attributes (for example data attributes) to add to the anchor tag.'
+      'HTML attributes (for example data attributes) to add to the anchor tag.',
+    released: '1.0.0'
   }
 }
 
