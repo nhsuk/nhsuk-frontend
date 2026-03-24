f8f9ae9beafa71866465996b80c246e02d991110
Colin Rotherham <work@colinr.com>
2026-03-24 08:08:19 +0000

    Add date input translated example

--- packages/nhsuk-frontend/src/nhsuk/components/date-input/fixtures.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/date-input/fixtures.mjs
@@ -20,6 +20,36 @@ const fixtures = {
     },
     screenshot: true
   },
+  'with translations': {
+    context: {
+      fieldset: {
+        legend: {
+          text: 'Beth yw eich dyddiad geni?',
+          size: 'l',
+          isPageHeading: true
+        }
+      },
+      hint: {
+        text: 'Er enghraifft, 31 3 1980'
+      },
+      id: 'example',
+      day: {
+        label: {
+          text: 'Dydd'
+        }
+      },
+      month: {
+        label: {
+          text: 'Mis'
+        }
+      },
+      year: {
+        label: {
+          text: 'Blwyddyn'
+        }
+      }
+    }
+  },
   'with values': {
     context: {
       fieldset: {
