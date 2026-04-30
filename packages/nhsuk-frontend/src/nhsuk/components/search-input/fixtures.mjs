e8fe980f38cc68de196560f8b0385222ada6d0fe
Colin Rotherham <work@colinr.com>
2026-04-30 21:58:28 +0100

    Add search input component

---
+++ packages/nhsuk-frontend/src/nhsuk/components/search-input/fixtures.mjs
@@ -0,0 +1,137 @@
+/**
+ * Nunjucks macro option examples
+ *
+ * @satisfies {{ [example: string]: MacroExample }}
+ */
+const fixtures = {
+  'default': {
+    context: {
+      label: {
+        text: 'Search patients by NHS number',
+        classes: 'nhsuk-u-visually-hidden'
+      },
+      placeholder: 'NHS number',
+      name: 'example',
+      inputmode: 'numeric',
+      width: 10
+    }
+  },
+  'large': {
+    context: {
+      label: {
+        text: 'Search patients by NHS number',
+        classes: 'nhsuk-u-visually-hidden'
+      },
+      placeholder: 'NHS number',
+      name: 'example',
+      width: 20,
+      large: true,
+      inputmode: 'numeric'
+    }
+  },
+  'large with brand button': {
+    context: {
+      label: {
+        text: 'Search patients by NHS number',
+        classes: 'nhsuk-u-visually-hidden'
+      },
+      button: {
+        variant: 'brand'
+      },
+      placeholder: 'NHS number',
+      name: 'example',
+      width: 20,
+      large: true,
+      inputmode: 'numeric'
+    }
+  },
+  'with hint': {
+    context: {
+      label: {
+        text: 'Search patients by NHS number',
+        classes: 'nhsuk-u-visually-hidden'
+      },
+      hint: {
+        text: 'Your NHS number is a 10 digit number that you find on any letter the NHS has sent you, for example, 485 777 3456'
+      },
+      placeholder: 'NHS number',
+      id: 'with-hint',
+      name: 'example',
+      width: 10,
+      inputmode: 'numeric'
+    }
+  },
+  'with brand button': {
+    context: {
+      label: {
+        text: 'Search patients by NHS number',
+        classes: 'nhsuk-u-visually-hidden'
+      },
+      button: {
+        variant: 'brand'
+      },
+      placeholder: 'NHS number',
+      name: 'example',
+      width: 10,
+      inputmode: 'numeric'
+    }
+  },
+  'with secondary button': {
+    context: {
+      label: {
+        text: 'Search patients by NHS number',
+        classes: 'nhsuk-u-visually-hidden'
+      },
+      button: {
+        variant: 'secondary'
+      },
+      placeholder: 'NHS number',
+      name: 'example',
+      width: 10,
+      inputmode: 'numeric'
+    }
+  },
+  'with secondary button, solid background': {
+    context: {
+      label: {
+        text: 'Search patients by NHS number',
+        classes: 'nhsuk-u-visually-hidden'
+      },
+      button: {
+        variant: 'secondary-solid'
+      },
+      placeholder: 'NHS number',
+      name: 'example',
+      width: 10,
+      inputmode: 'numeric'
+    },
+    options: {
+      layout: 'background-grey'
+    }
+  },
+  'without button': {
+    context: {
+      label: {
+        text: 'Search patients by NHS number',
+        classes: 'nhsuk-u-visually-hidden'
+      },
+      button: false,
+      placeholder: 'NHS number',
+      name: 'example',
+      width: 10,
+      inputmode: 'numeric'
+    }
+  }
+}
+
+/**
+ * Nunjucks macro option examples
+ * (with typed keys)
+ *
+ * @type {Record<keyof typeof fixtures, MacroExample>}
+ */
+export const examples = fixtures
+
+/**
+ * @import { MacroExample } from '#lib'
+ */
