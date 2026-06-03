7baa48d24338df18f6ea6acbd1d35e732faea46b
Colin Rotherham <work@colinr.com>
2026-06-03 13:21:01 +0100

    Add pagination screenshot tests

--- src/nhsuk/components/pagination/fixtures.mjs
+++ src/nhsuk/components/pagination/fixtures.mjs
@@ -108,6 +108,8 @@ const fixtures = {
       width: 'full'
     },
     screenshot: {
+      states: ['focus', 'hover', 'active'],
+      selector: '.nhsuk-pagination__item--current a',
       viewports: ['mobile', 'tablet', 'desktop', 'large-desktop']
     }
   },
