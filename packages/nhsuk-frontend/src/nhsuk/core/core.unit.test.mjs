3a55996a2bedfdc69804adff3c31ef91dbee09c8
anandamaryon1 <159916913+anandamaryon1@users.noreply.github.com>
2026-07-02 15:26:06 +0100

    Add reverse hover colour

--- packages/nhsuk-frontend/src/nhsuk/core/core.unit.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/core/core.unit.test.mjs
@@ -56,6 +56,7 @@ describe('Core', () => {
         --nhsuk-reverse-border-colour: #337ec6;
         --nhsuk-input-border-colour: #4c6272;
         --nhsuk-hover-colour: #aeb7bd;
+        --nhsuk-reverse-hover-colour: #004b93;
         --nhsuk-input-background-colour: white;
         --nhsuk-code-colour: #d5281b;
         --nhsuk-link-colour: #005eb8;
