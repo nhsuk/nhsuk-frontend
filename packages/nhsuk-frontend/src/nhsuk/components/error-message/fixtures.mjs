f1480033d15fb88599d8896c657fd203c11dafad
Colin Rotherham <work@colinr.com>
2026-07-09 08:46:51 +0100

    Add error message component missing `caller` option

--- packages/nhsuk-frontend/src/nhsuk/components/error-message/fixtures.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/error-message/fixtures.mjs
@@ -4,12 +4,37 @@
  * @satisfies {{ [example: string]: MacroExample }}
  */
 const fixtures = {
-  default: {
+  'default': {
     context: {
       text: 'Enter your full name'
     },
     screenshot: {
       viewports: ['tablet']
+    }
+  },
+  'with text escaping': {
+    context: {
+      text: 'Postcode must not include & and <'
+    }
+  },
+  'with HTML': {
+    context: {
+      html: 'Postcode must not include &amp; and &lt;'
+    }
+  },
+  'with HTML via call block': {
+    callBlock: 'Postcode must not include &amp; and &lt;'
+  },
+  'with translations': {
+    context: {
+      text: 'Rhowch eich enw llawn',
+      visuallyHiddenText: 'Gwall'
+    }
+  },
+  'without visually hidden text': {
+    context: {
+      text: 'There is an error on line 42',
+      visuallyHiddenText: ''
     }
   }
 }
