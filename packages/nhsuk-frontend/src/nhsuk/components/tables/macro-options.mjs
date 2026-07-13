ec7468248dbfe6136abe002b1314c9762806ae50
Colin Rotherham <work@colinr.com>
2026-07-13 10:32:21 +0100

    Add missing macro option guidance for table cell `attributes`

--- packages/nhsuk-frontend/src/nhsuk/components/tables/macro-options.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/tables/macro-options.mjs
@@ -56,6 +56,13 @@ const options = {
         required: false,
         description: 'Specify how many rows a cell extends.',
         released: '1.0.0'
+      },
+      attributes: {
+        type: 'object',
+        required: false,
+        description:
+          'HTML attributes (for example data attributes) to add to the table cell.',
+        released: '9.4.0'
       }
     }
   },
@@ -103,6 +110,13 @@ const options = {
         required: false,
         description: 'Specify how many rows a cell extends.',
         released: '1.0.0'
+      },
+      attributes: {
+        type: 'object',
+        required: false,
+        description:
+          'HTML attributes (for example data attributes) to add to the table head cell.',
+        released: '9.4.0'
       },
       sortable: {
         type: 'boolean',
