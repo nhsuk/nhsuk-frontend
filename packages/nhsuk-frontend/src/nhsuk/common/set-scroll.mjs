445212326f91e5af9ca386a11bf6c42342496045
Colin Rotherham <work@colinr.com>
2026-07-22 21:33:13 +0100

    Add common utility function `isInView()`

--- packages/nhsuk-frontend/src/nhsuk/common/set-scroll.mjs
+++ packages/nhsuk-frontend/src/nhsuk/common/set-scroll.mjs
@@ -9,6 +9,42 @@ export function setScroll($target, options = {}) {
 }
 
 /**
+ * Check if target is in view
+ *
+ * @template {Element | DOMRect} TargetType
+ * @param {TargetType | undefined} $target - HTML element or DOMRect to check
+ * @param {{ offset?: number, side?: 'all' | 'top' } & SetScrollOptions} [options] - View options
+ * @returns {$target is TargetType} Whether the target is in view
+ */
+export function isInView($target, options = {}) {
+  if (!$target) {
+    return false
+  }
+
+  const bounds = !($target instanceof DOMRect)
+    ? $target.getBoundingClientRect()
+    : $target
+
+  const { offset = 0, side = 'all', threshold = 50 } = options
+  const viewHeight = (window.innerHeight * threshold) / 100
+
+  let { top, bottom } = bounds
+
+  top += offset
+  bottom += offset
+
+  const isTopWithinView = top >= 0 && top < viewHeight
+  const isBottomWithinView = bottom >= 0 && bottom <= viewHeight
+
+  if (side === 'top') {
+    return isTopWithinView
+  }
+
+  return isTopWithinView && isBottomWithinView
+}
+
+/**
  * @typedef {object} SetScrollOptions
  * @property {ScrollBehavior} [behavior] - Browser scroll behavior, e.g. 'smooth'
+ * @property {number} [threshold=50] - The percentage value of the viewport height considered within view
  */
