1f1cbf035c35e04cb9196826221c1493ba5e4bfe
Colin Rotherham <work@colinr.com>
2026-07-22 21:34:02 +0100

    Update `setScroll()` to support multiple `scrollTo` targets
    This allows HTML elements to try scrolling to, should the target element stay in view
    
    For example, so the error summary can attempt to scroll to a legend or label based on whether it keeps the target element (the input) in view

--- packages/nhsuk-frontend/src/nhsuk/common/set-scroll.mjs
+++ packages/nhsuk-frontend/src/nhsuk/common/set-scroll.mjs
@@ -1,10 +1,73 @@
 /**
  * Scroll to element
  *
+ * If the element is in the bottom half of the screen we scroll it into view,
+ * avoiding situations where the input either ends up off the screen or is
+ * obscured by a software keyboard
+ *
  * @param {HTMLElement} $target - HTML element to scroll to
  * @param {SetScrollOptions} [options] - Scroll options
  */
 export function setScroll($target, options = {}) {
+  const { scrollTo = [], threshold } = options
+
+  // Add target element to list of (non-empty) targets
+  const $targets = (Array.isArray(scrollTo) ? scrollTo : [scrollTo])
+    .filter(($element) => !!$element)
+    .concat([$target])
+
+  // Cache the bounding box for each scroll target
+  const candidates = new Map(
+    $targets.map(($element, index) => [
+      $element,
+      {
+        $elements: $targets.slice(index),
+        bounds: $element.getBoundingClientRect()
+      }
+    ])
+  )
+
+  /**
+   * @param {Element[]} $elements
+   * @param {{ offset?: number }} [options]
+   */
+  function getResults($elements, { offset } = {}) {
+    return $elements.filter(($candidate) => {
+      const bounds = candidates.get($candidate)?.bounds
+      const side = $candidate === $target ? 'all' : 'top'
+
+      return !isInView(bounds, { offset, side, threshold })
+    })
+  }
+
+  const $targetsOutOfView = getResults($targets)
+
+  for (const [$element, candidate] of candidates) {
+    const { $elements, bounds } = candidate
+
+    // Check for elements out of view
+    const $outOfView = $elements.filter(($candidate) =>
+      $targetsOutOfView.includes($candidate)
+    )
+
+    // Skip scroll when all elements in view
+    if (!$outOfView.length) {
+      return
+    }
+
+    // Check again, but scroll (virtually) to the element
+    const $outOfViewOffset = getResults($outOfView, {
+      offset: $element === $target ? 0 : -bounds.top
+    })
+
+    // All in view now? Scroll to the element
+    if (!$outOfViewOffset.length) {
+      $element.scrollIntoView(options)
+      return
+    }
+  }
+
+  // Scroll to the default target
   $target.scrollIntoView(options)
 }
 
@@ -46,5 +109,6 @@ export function isInView($target, options = {}) {
 /**
  * @typedef {object} SetScrollOptions
  * @property {ScrollBehavior} [behavior] - Browser scroll behavior, e.g. 'smooth'
+ * @property {(Element | null | undefined)[] | Element | null | undefined} [scrollTo] - Additional HTML elements to try scrolling to, should the target element stay in view
  * @property {number} [threshold=50] - The percentage value of the viewport height considered within view
  */
