cbed5f87fc8de9f982074160e5a00f00727dfa6c
Colin Rotherham <work@colinr.com>
2026-07-27 17:06:08 +0100

    Use mock component in `checkSupport()` tests

--- packages/nhsuk-frontend/src/nhsuk/component.jsdom.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/component.jsdom.test.mjs
@@ -1,4 +1,5 @@
-import { Component } from './component.mjs'
+import { MockComponent } from '#lib/fixtures/configuration/mock-component.mjs'
+
 import { SupportError } from './errors/index.mjs'
 
 describe('Component', () => {
@@ -8,35 +9,36 @@ describe('Component', () => {
   })
 
   describe('checkSupport()', () => {
-    describe('default implementation', () => {
-      class ServiceComponent extends Component {
-        static moduleName = 'app-service-component'
-      }
+    let /** @type {HTMLElement} */ $root
 
+    beforeEach(() => {
+      $root = document.createElement('div')
+      $root.setAttribute('data-module', MockComponent.moduleName)
+
+      document.body.appendChild($root)
+    })
+
+    describe('default implementation', () => {
       it('Makes initialisation throw if NHS.UK frontend is not supported', () => {
         document.body.classList.remove('nhsuk-frontend-supported')
-        expect(() => new ServiceComponent(document.body)).toThrow(SupportError)
+        expect(() => new MockComponent($root)).toThrow(SupportError)
       })
 
       it('Allows initialisation if NHS.UK frontend is supported', () => {
-        expect(() => new ServiceComponent(document.body)).not.toThrow()
+        expect(() => new MockComponent($root)).not.toThrow()
       })
     })
 
     describe('when overriden', () => {
       it('Allows child classes to define their own condition for support', () => {
-        class ServiceComponent extends Component {
-          static moduleName = 'app-service-component'
-
+        class ServiceComponent extends MockComponent {
           static checkSupport() {
             throw new Error('Custom error')
           }
         }
 
         // Use the message rather than the class as `SupportError` extends `Error`
-        expect(() => new ServiceComponent(document.body)).toThrow(
-          'Custom error'
-        )
+        expect(() => new ServiceComponent($root)).toThrow('Custom error')
       })
     })
   })
