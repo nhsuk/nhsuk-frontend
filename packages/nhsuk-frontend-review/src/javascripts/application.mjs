69a89ce0ccaf80acaf4edf126a8a6acc86184c67
Colin Rotherham <work@colinr.com>
2026-07-27 16:35:58 +0100

    Make sure review app logs all errors before throwing

--- packages/nhsuk-frontend-review/src/javascripts/application.mjs
+++ packages/nhsuk-frontend-review/src/javascripts/application.mjs
@@ -1,23 +1,40 @@
 import { initAll } from 'nhsuk-frontend'
 
+/**
+ * Review app errors
+ *
+ * @type {{ error: unknown, context: ErrorContext<CompatibleClass> }[]}
+ */
+const errors = []
+
 initAll({
   errorSummary: { disableAutoFocus: true },
   notificationBanner: { disableAutoFocus: true },
   onError(error, context) {
-    const $element = context.element
-
-    // Locate component example
-    const $example = $element?.closest('.app-wrapper')
-    const $heading = $example?.querySelector('h2')
-
-    console.error(
-      $heading
-        ? `Error in NHS.UK frontend: '${$heading.textContent}'`
-        : 'Error in NHS.UK frontend',
-      error,
-      context
-    )
-
-    throw error
+    errors.push({ error, context })
   }
 })
+
+for (const { error, context } of errors) {
+  const $element = context.element
+
+  // Locate component example
+  const $example = $element?.closest('.app-wrapper')
+  const $heading = $example?.querySelector('h2')
+
+  console.error(
+    $heading
+      ? `Error in NHS.UK frontend: '${$heading.textContent}'`
+      : 'Error in NHS.UK frontend',
+    error,
+    context
+  )
+}
+
+if (errors.length > 0) {
+  throw new Error('Errors in NHS.UK frontend review app')
+}
+
+/**
+ * @import { CompatibleClass, ErrorContext } from 'nhsuk-frontend'
+ */
