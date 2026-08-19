0f0eaa283649394c8483a4773ab4c24f23c89a90
Colin Rotherham <work@colinr.com>
2026-08-19 14:03:48 +0100

    Support table headers and cells as strings

--- packages/nhsuk-frontend/src/nhsuk/components/tables/fixtures.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/tables/fixtures.mjs
@@ -464,6 +464,32 @@ const fixtures = {
       ]
     }
   },
+  "with string items": {
+    context: {
+      caption: {
+        text: "Impetigo can look similar to other skin conditions",
+        size: "m"
+      },
+      firstCellIsHeader: true,
+      head: ["Skin symptoms", "Possible cause"],
+      rows: [
+        ["Blisters on lips or around the mouth", "Cold sores"],
+        ["Itchy, dry, cracked, sore", "Eczema"],
+        ["Itchy blisters", "Shingles, chickenpox"]
+      ]
+    }
+  },
+  "with empty items and string items": {
+    context: {
+      caption: {
+        text: "Vaccinations given",
+        size: "m"
+      },
+      firstCellIsHeader: true,
+      head: ["Date", "Vaccine", false],
+      rows: [["10 July 2024", "RSV", false], false]
+    }
+  },
   "with missing data": {
     context: {
       caption: {
@@ -2891,6 +2917,33 @@ const fixtures = {
             sortValue: "1975-04-18"
           }
         ]
+      ]
+    }
+  },
+  "sortable with string items": {
+    context: {
+      caption: "Appointments",
+      firstCellIsHeader: true,
+      head: [
+        {
+          text: "Time",
+          sort: "ascending"
+        },
+        {
+          text: "Name",
+          sort: true
+        },
+        {
+          text: "Date of birth",
+          classes: "nhsuk-u-nowrap"
+        }
+      ],
+      rows: [
+        ["11:00", "Laura Stone", "4 January 1986"],
+        ["11:30", "Emma Katie-Brown", "7 February 1976"],
+        ["13:10", "David Chen", "19 March 1981"],
+        ["13:40", "Michael Thompson", "6 December 1964"],
+        ["14:20", "Juan Martinez", "18 April 1975"]
       ]
     }
   }
