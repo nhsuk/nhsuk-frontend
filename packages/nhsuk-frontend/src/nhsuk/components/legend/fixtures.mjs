c80b8b0cdf25d44eb201a3a9a12ec39ad6282f83
Colin Rotherham <work@colinr.com>
2026-08-19 13:09:10 +0100

    Add examples for legend `headingLevel`

--- packages/nhsuk-frontend/src/nhsuk/components/legend/fixtures.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/legend/fixtures.mjs
@@ -110,9 +110,30 @@ const fixtures = {
     },
     callBlock: "What is your address?"
   },
+  "with heading level 1": {
+    context: {
+      text: "What is your address?",
+      size: "l",
+      headingLevel: 1
+    }
+  },
+  "with heading level 2": {
+    context: {
+      text: "What is your address?",
+      size: "m",
+      headingLevel: 2
+    }
+  },
   "without page heading": {
     context: {
       text: "What is your address?"
+    }
+  },
+  "without page heading overriding heading level": {
+    context: {
+      text: "What is your address?",
+      isPageHeading: false,
+      headingLevel: 2
     }
   }
 }
