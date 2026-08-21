abccb6c518edc2935933a1a3dadb36bd04101ac5
Colin Rotherham <work@colinr.com>
2026-08-21 10:52:47 +0100

    Update `caller` macro options documentation

--- packages/nhsuk-frontend/src/nhsuk/components/tag/macro-options.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/tag/macro-options.mjs
@@ -30,7 +30,7 @@ const options = {
     type: 'nunjucks-block',
     required: false,
     description:
-      'Not strictly a parameter but [Nunjucks code convention](https://mozilla.github.io/nunjucks/templating.html#call). Using a `call` block enables you to call a macro with all the text inside the tag. This is helpful if you want to pass a lot of content into a macro. To use it, you will need to wrap the entire tag component in a `call` block.',
+      'Not strictly an option but supports the [`call` block](https://mozilla.github.io/nunjucks/templating.html#call) as an alternative to the `html` option. To use it, you will need to wrap the entire tag component in a `call` block.',
     released: '10.6.0'
   },
   classes: {
