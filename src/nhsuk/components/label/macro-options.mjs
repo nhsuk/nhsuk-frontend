3edc9d2bff1aa6695dd2732bc74af2955590d091
Colin Rotherham <work@colinr.com>
2026-08-24 16:28:25 +0100

    Deprecate label and legend `isPageHeading` for `heading` option

--- src/nhsuk/components/label/macro-options.mjs
+++ src/nhsuk/components/label/macro-options.mjs
@@ -53,11 +53,62 @@ const options = {
       'The value of the `for` attribute, the ID of the input the label is associated with.',
     released: '1.0.0'
   },
+  heading: {
+    type: 'object',
+    required: false,
+    description: 'Whether the label also acts as a heading.',
+    released: '10.6.1',
+    isComponent: true,
+    params: {
+      text: {
+        type: 'string',
+        required: false,
+        description:
+          'If `html` is set, this is not required. Text to use within the label as a heading. If `html` is provided, the `text` option will be ignored.',
+        released: '10.6.1'
+      },
+      html: {
+        type: 'string',
+        required: false,
+        description:
+          'If `text` is set, this is not required. HTML to use within the label as a heading. If `html` is provided, the `text` option will be ignored.',
+        released: '10.6.1'
+      },
+      visuallyHiddenText: {
+        type: 'string',
+        required: false,
+        description:
+          'A visually hidden suffix added to the label as a heading.',
+        released: '10.6.1'
+      },
+      caption: {
+        type: 'object',
+        required: false,
+        description: 'Optional caption for the label as a heading.',
+        released: '10.6.1',
+        isComponent: true
+      },
+      size: {
+        type: 'string',
+        required: false,
+        description:
+          'Size of the label as a heading – `"s"`, `"m"`, `"l"` or `"xl"`.',
+        released: '10.6.1'
+      },
+      level: {
+        type: 'integer',
+        required: false,
+        description: 'Optional label heading level. Defaults to `1`.',
+        released: '10.6.1'
+      }
+    }
+  },
   isPageHeading: {
     type: 'boolean',
     required: false,
-    description: 'Whether the label also acts as the heading for the page.',
-    released: '1.0.0'
+    description: 'Replaced by the `heading` option.',
+    released: '1.0.0',
+    deprecated: '10.6.1'
   },
   size: {
     type: 'string',
