8b2cc30e5e6d6478839faab42b2f0162cf984c6d
Colin Rotherham <work@colinr.com>
2026-09-17 21:59:44 +0100

    123

--- packages/nhsuk-frontend/src/nhsuk/components/action-link/fixtures.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/action-link/fixtures.mjs
@@ -48,6 +48,99 @@ const fixtures = {
       selector: ".nhsuk-action-link"
     }
   },
+  "slot": {
+    context: {
+      text: "Find your nearest A&E",
+      href: "#/find"
+    },
+    variants: [
+      {
+        description: 'with "before"',
+        context: {
+          slots: {
+            before: {
+              html: "<samp>Before</samp>"
+            }
+          }
+        }
+      },
+      {
+        description: 'with "before" and "start"',
+        context: {
+          slots: {
+            before: {
+              html: "<samp>Before</samp>"
+            },
+            start: {
+              html: "<samp>Start–</samp>"
+            }
+          }
+        }
+      },
+      {
+        description: 'with "before", "start", "end" and "after"',
+        context: {
+          slots: {
+            before: {
+              html: "<samp>Before</samp>"
+            },
+            start: {
+              html: "<samp>Start–</samp>"
+            },
+            end: {
+              html: "<samp>–End</samp>"
+            },
+            after: {
+              html: "<samp>After</samp>"
+            }
+          }
+        }
+      },
+      {
+        description: 'with "start"',
+        context: {
+          slots: {
+            start: {
+              html: "<samp>Start–</samp>"
+            }
+          }
+        }
+      },
+      {
+        description: 'with "end"',
+        context: {
+          slots: {
+            end: {
+              html: "<samp>–End</samp>"
+            }
+          }
+        }
+      },
+      {
+        description: 'with "end" and "after"',
+        context: {
+          slots: {
+            end: {
+              html: "<samp>–End</samp>"
+            },
+            after: {
+              html: "<samp>After</samp>"
+            }
+          }
+        }
+      },
+      {
+        description: 'with "after"',
+        context: {
+          slots: {
+            after: {
+              html: "<samp>After</samp>"
+            }
+          }
+        }
+      }
+    ]
+  },
   "with HTML": {
     context: {
       html: outdent`
