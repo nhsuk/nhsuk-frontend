1eb36f686cd3278ab021f8c2f0df6744e36540e8
Colin Rotherham <work@colinr.com>
2026-09-24 16:57:50 +0100

    Simplfy Nunjucks attribute macro tests

--- packages/nhsuk-frontend/src/nhsuk/macros/attributes.unit.test.mjs
+++ packages/nhsuk-frontend/src/nhsuk/macros/attributes.unit.test.mjs
@@ -2,92 +2,79 @@ import { outdent } from 'outdent'
 
 import { nunjucks } from '#lib'
 
-describe('attributes.njk', () => {
+describe('Macro: Attributes', () => {
+  /**
+   * @param {MacroRenderOptions['context']} [context]
+   */
+  function renderMacro(context) {
+    return nunjucks.renderMacro(
+      'nhsukAttributes',
+      'nhsuk/macros/attributes.njk',
+      { context }
+    )
+  }
+
   describe('nhsukAttributes', () => {
     it('renders a string attribute', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            'data-attribute': 'value'
-          }
-        }
-      )
+      const attributes = renderMacro({
+        'data-attribute': 'value'
+      })
 
       // Note the starting space so we ensure it doesn't stick to possible other previous attributes
       expect(attributes).toBe(' data-attribute="value"')
     })
 
     it('renders a string attribute with item options', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            'data-attribute': {
-              type: 'string',
-              value: 'value'
-            }
-          }
+      const attributes = renderMacro({
+        'data-attribute': {
+          type: 'string',
+          value: 'value'
         }
-      )
+      })
 
       // Note the starting space so we ensure it doesn't stick to possible other previous attributes
       expect(attributes).toBe(' data-attribute="value"')
     })
 
     it('renders an array attribute with item options', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            'data-attributes': {
-              type: 'array',
-              value: ['value1', 'value2']
-            }
-          }
+      const attributes = renderMacro({
+        'data-attributes': {
+          type: 'array',
+          value: ['value1', 'value2']
         }
-      )
+      })
 
       // Note the starting space so we ensure it doesn't stick to possible other previous attributes
       expect(attributes).toBe(' data-attributes=\'["value1","value2"]\'')
     })
 
     it('renders array attributes values with escaping', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            'data-quotes-double': {
-              type: 'array',
-              value: ['value "1"']
-            },
-            'data-quotes-single': {
-              type: 'array',
-              value: ["value '2'"]
-            },
-            'data-quotes-mixed': {
-              type: 'array',
-              value: ['value "\'3\'"']
-            },
-            'data-html-elements': {
-              type: 'array',
-              value: ['value <strong>4</strong>']
-            },
-            'data-html-entities': {
-              type: 'array',
-              value: ['value & 5']
-            },
-            'data-html-entities-double': {
-              type: 'array',
-              value: ['value &amp; 6']
-            }
-          }
+      const attributes = renderMacro({
+        'data-quotes-double': {
+          type: 'array',
+          value: ['value "1"']
+        },
+        'data-quotes-single': {
+          type: 'array',
+          value: ["value '2'"]
+        },
+        'data-quotes-mixed': {
+          type: 'array',
+          value: ['value "\'3\'"']
+        },
+        'data-html-elements': {
+          type: 'array',
+          value: ['value <strong>4</strong>']
+        },
+        'data-html-entities': {
+          type: 'array',
+          value: ['value & 5']
+        },
+        'data-html-entities-double': {
+          type: 'array',
+          value: ['value &amp; 6']
         }
-      )
+      })
 
       // Note that single quotes are only used when rendering arrays that contain double quotes
       expect(attributes).toBe(
@@ -103,32 +90,26 @@ describe('attributes.njk', () => {
     })
 
     it('renders multiple attributes', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            'data-attribute': 'value',
-            'data-second-attribute': 'second-value',
-            'data-third-attribute': {
-              type: 'string',
-              value: 'third-value'
-            },
-            'data-fourth-attribute': {
-              type: 'array',
-              value: ['fourth-value']
-            },
-            'data-fifth-attribute': {
-              type: 'array',
-              value: [true]
-            },
-            'data-sixth-attribute': {
-              type: 'array',
-              value: []
-            }
-          }
+      const attributes = renderMacro({
+        'data-attribute': 'value',
+        'data-second-attribute': 'second-value',
+        'data-third-attribute': {
+          type: 'string',
+          value: 'third-value'
+        },
+        'data-fourth-attribute': {
+          type: 'array',
+          value: ['fourth-value']
+        },
+        'data-fifth-attribute': {
+          type: 'array',
+          value: [true]
+        },
+        'data-sixth-attribute': {
+          type: 'array',
+          value: []
         }
-      )
+      })
 
       // Note that single quotes are only used when rendering arrays that contain double quotes
       expect(attributes).toBe(
@@ -144,17 +125,11 @@ describe('attributes.njk', () => {
     })
 
     it('renders attribute values as strings by default', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            'viewBox': '0 0 15 13',
-            'focusable': false,
-            'aria-hidden': true
-          }
-        }
-      )
+      const attributes = renderMacro({
+        'viewBox': '0 0 15 13',
+        'focusable': false,
+        'aria-hidden': true
+      })
 
       // Note that `aria-hidden` and `focusable` are not converted to boolean attributes
       expect(attributes).toBe(
@@ -163,21 +138,15 @@ describe('attributes.njk', () => {
     })
 
     it('renders attribute values as strings when `optional` not set', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            'viewBox': '0 0 15 13',
-            'focusable': {
-              value: false
-            },
-            'aria-hidden': {
-              value: true
-            }
-          }
+      const attributes = renderMacro({
+        'viewBox': '0 0 15 13',
+        'focusable': {
+          value: false
+        },
+        'aria-hidden': {
+          value: true
         }
-      )
+      })
 
       // Note that `aria-hidden` and `focusable` are not converted to boolean attributes
       expect(attributes).toBe(
@@ -186,40 +155,34 @@ describe('attributes.njk', () => {
     })
 
     it('renders attribute values as strings when `optional: false`', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            // Treat null and undefined values as not provided
-            'example-empty-1': {
-              value: undefined,
-              optional: false
-            },
-            'example-empty-2': {
-              value: null,
-              optional: false
-            },
+      const attributes = renderMacro({
+        // Treat null and undefined values as not provided
+        'example-empty-1': {
+          value: undefined,
+          optional: false
+        },
+        'example-empty-2': {
+          value: null,
+          optional: false
+        },
 
-            // But watch out for intentionally falsy values
-            // https://github.com/alphagov/nhsuk-frontend/issues/4669
-            'example-falsy-1': {
-              value: '',
-              optional: false
-            },
-            'example-falsy-2': {
-              value: 0,
-              optional: false
-            },
+        // But watch out for intentionally falsy values
+        // https://github.com/alphagov/nhsuk-frontend/issues/4669
+        'example-falsy-1': {
+          value: '',
+          optional: false
+        },
+        'example-falsy-2': {
+          value: 0,
+          optional: false
+        },
 
-            // Except false, always stringify unless optional
-            'example-falsy-3': {
-              value: false,
-              optional: false
-            }
-          }
+        // Except false, always stringify unless optional
+        'example-falsy-3': {
+          value: false,
+          optional: false
         }
-      )
+      })
 
       // Note that all non-optional values are rendered to strings by Nunjucks,
       // even true/false which only become boolean attributes when optional
@@ -229,130 +192,93 @@ describe('attributes.njk', () => {
     })
 
     it('skip attribute when falsy with `optional: true` and null, undefined or false', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            // Treat null and undefined values as not provided
-            'example-empty-1': {
-              value: undefined,
-              optional: true
-            },
-            'example-empty-2': {
-              value: null,
-              optional: true
-            },
+      const attributes = renderMacro({
+        // Treat null and undefined values as not provided
+        'example-empty-1': {
+          value: undefined,
+          optional: true
+        },
+        'example-empty-2': {
+          value: null,
+          optional: true
+        },
 
-            // But watch out for intentionally falsy values
-            // https://github.com/alphagov/nhsuk-frontend/issues/4669
-            'example-falsy-1': {
-              value: '',
-              optional: true
-            },
-            'example-falsy-2': {
-              value: 0,
-              optional: true
-            },
+        // But watch out for intentionally falsy values
+        // https://github.com/alphagov/nhsuk-frontend/issues/4669
+        'example-falsy-1': {
+          value: '',
+          optional: true
+        },
+        'example-falsy-2': {
+          value: 0,
+          optional: true
+        },
 
-            // Except false, we remove `false` boolean attributes
-            'example-falsy-3': {
-              value: false,
-              optional: true
-            }
-          }
+        // Except false, we remove `false` boolean attributes
+        'example-falsy-3': {
+          value: false,
+          optional: true
         }
-      )
+      })
 
       // Note that null, undefined and false are skipped, intentionally falsy values are preserved
       expect(attributes).toBe(' example-falsy-1="" example-falsy-2="0"')
     })
 
     it('renders attribute when (string) `"true"` with `optional: true` as strings`', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            type: 'radio',
-            checked: {
-              value: 'true',
-              optional: true
-            }
-          }
+      const attributes = renderMacro({
+        type: 'radio',
+        checked: {
+          value: 'true',
+          optional: true
         }
-      )
+      })
 
       // Note that `checked` defaults to a string value unless strictly a boolean
       expect(attributes).toBe(' type="radio" checked="true"')
     })
 
     it('renders attribute when (string) `"false"` with `optional: true` as strings', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            type: 'radio',
-            checked: {
-              value: 'false',
-              optional: true
-            }
-          }
+      const attributes = renderMacro({
+        type: 'radio',
+        checked: {
+          value: 'false',
+          optional: true
         }
-      )
+      })
 
       // Note that `checked` defaults to a string value unless strictly a boolean
       expect(attributes).toBe(' type="radio" checked="false"')
     })
 
     it('renders attribute when (boolean) `true` with `optional: true` as boolean attribute', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            type: 'radio',
-            checked: {
-              value: true,
-              optional: true
-            }
-          }
+      const attributes = renderMacro({
+        type: 'radio',
+        checked: {
+          value: true,
+          optional: true
         }
-      )
+      })
 
       // Note that `checked` has no value is, e.g. `<input type="radio" checked>`
       expect(attributes).toBe(' type="radio" checked')
     })
 
     it('skip attribute when (boolean) `false` with `optional: true` as boolean attribute', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {
-            type: 'radio',
-            checked: {
-              value: false,
-              optional: true
-            }
-          }
+      const attributes = renderMacro({
+        type: 'radio',
+        checked: {
+          value: false,
+          optional: true
         }
-      )
+      })
 
       // Note that `checked` is removed when false
       expect(attributes).toBe(' type="radio"')
     })
 
     it('outputs attributes if already stringified', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: ' data-attribute="Testing &amp; more"'
-        }
-      )
-
+      const attributes = renderMacro(' data-attribute="Testing &amp; more"')
       expect(attributes).toBe(' data-attribute="Testing &amp; more"')
     })
 
@@ -368,14 +294,7 @@ describe('attributes.njk', () => {
     })
 
     it('outputs nothing if there are no attributes', () => {
-      const attributes = nunjucks.renderMacro(
-        'nhsukAttributes',
-        'nhsuk/macros/attributes.njk',
-        {
-          context: {}
-        }
-      )
-
+      const attributes = renderMacro({})
       expect(attributes).toBe('')
     })
 
@@ -430,3 +349,7 @@ describe('attributes.njk', () => {
     })
   })
 })
+
+/**
+ * @import { MacroRenderOptions } from '#lib'
+ */
